3022ff81afd4a20159d27165540f28615e832292
mstorti <mstorti>
2001-05-30 03:45:40 +0000

    ppp

--- tools/petscload.pl
+++ tools/petscload.pl
@@ -1,5 +1,30 @@
 #!/usr/bin/perl
-#__INSERT_LICENSE__
+#  [-begin-license-]
+#  This  file  belongs  to  the  PETSc  -  FEM  package,  a  library  and
+#  application  suite oriented  to  the Finite  Element  Method based  on
+#  PETSc.   Copyright  (C)  1999-2001,  Mario  Alberto  Storti,  Norberto
+#  Marcelo Nigro, Centro Internacional de Metodos Numericos en Ingenieria
+#  (CIMEC-Argentina),  Universidad Nacional del  Litoral (UNL-Argentina),
+#  Consejo   Nacional   de   Investigaciones   Cientificas   y   Tecnicas
+#  (UNL-Argentina).
+#  
+#  This program is  free software; you can redistribute  it and/or modify
+#  it under the  terms of the GNU General Public  License as published by
+#  the Free Software Foundation; either  version 2 of the License, or (at
+#  your option) any later version.
+#  
+#  This program  is distributed in the  hope that it will  be useful, but
+#  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+#  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+#  General Public License for more details.
+#  
+#  You  should have received  a copy  of the  GNU General  Public License
+#  along  with  this  program;  if   not,  write  to  the  Free  Software
+#  Foundation, Inc.,  59 Temple Place, Suite 330,  Boston, MA 02111-1307,
+#  USA.
+#  [-end-license-]
+
+
 
 $in_data=0;
 $name="";
