6a4a6c1f784ee9a5f708f462b2f72d1e7259333a
mstorti <mstorti>
2001-07-01 15:19:30 +0000

    Added license

--- test/wallkechan/cutoff.m
+++ test/wallkechan/cutoff.m
@@ -2,7 +2,7 @@
 ##    Sets components of `v' to `tol' if `v < tol',
 ##    else let unchanged. 
 
-# $Id: cutoff.m,v 1.1 2001/07/01 15:14:26 mstorti Exp $
+# $Id: cutoff.m,v 1.2 2001/07/01 15:19:30 mstorti Exp $
 #__INSERT_LICENSE__
 
 function v = cutoff (v,tol)
