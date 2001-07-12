611afe23095e0ddaa42feb900001a8cf9bf46503
mstorti <mstorti>
2001-07-12 17:45:38 +0000

    *** empty log message ***

---
+++ src/lusubd.cpp
@@ -0,0 +1,13 @@
+//__INSERT_LICENSE__
+//$Id: lusubd.cpp,v 1.1 2001/07/12 17:45:38 mstorti Exp $
+
+#include "fem.h"
+#include <vector>
+#include <set>
+#include "utils.h"
+#include "getprop.h"
+#include "elemset.h"
+#include "idmap.h"
+#include "dofmap.h"
+#include "arglist.h"
+#include "readmesh.h"
