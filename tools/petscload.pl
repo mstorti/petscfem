b14cf801ad2a2633416d519487c003d5c5c25dbb
mstorti <mstorti>
2001-07-18 13:55:59 +0000

    Now can load repeated names (results in x, x_0, x_1, ....)

--- tools/petscload.pl
+++ tools/petscload.pl
@@ -5,11 +5,35 @@ $in_data=0;
 $name="";
 $basename="tmp_petsc_data";
 @namelist=();
+%names=();
+
+sub check_name {
+#    print "checking $name\n";
+    if ( $names {$name} ) {
+#  	my $nameo = $name;
+#  	if ($name =~ /^(.*)_(\d*)$/) {
+#  	    $name = "$1_".($2+1);
+#  	} else {
+#  	    $name = "${name}_0";
+#  	}
+	my $new_name,$c=0;
+	while (1) {
+	    $new_name = "${name}_$c";
+#	    print "trying $new_name\n";
+	    last unless $names{$new_name};
+	    $c++;
+	}
+	print "repeated name, replacing $name -> $new_name\n";
+	$name = $new_name;
+    }
+    $names{$name}=1;
+}
 
 while(<>) {
     if (/^(\S*) = \[/) {
 	$in_data=1;
 	$name=$1;
+	check_name();
 	open TMP,">$basename._";
     } elsif (/\]/) {
 	$in_data=0;
@@ -20,7 +44,9 @@ while(<>) {
 	print TMP;
     } elsif (/\s*(\S*)\s*=\s*spconvert/) {
 	$nameo=$name;
+	$names{$nameo} = 0;
 	$name=$1;
+	check_name();
 	pop @namelist;
 	push @namelist,$name;
 	rename "$basename.$nameo","$basename.$name";
