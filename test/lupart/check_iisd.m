381b8186d90428e7deca78abadfe109732152336
mstorti <mstorti>
2001-07-24 01:19:04 +0000

    Added a test for IISD solver

---
+++ test/lupart/check_iisd.m
@@ -0,0 +1,15 @@
+u_lu=aload("save.state.lu.tmp");
+u_iisd1=aload("save.state.iisd.np1.tmp");
+u_iisd2=aload("save.state.iisd.np2.tmp");
+u_iisd_cgs=aload("save.state.iisd.cgs.np2.tmp");
+
+tol = 1e-8;
+
+err = merr(u_iisd1-u_lu);
+printf("IISD on 1 processors OK ? > %d, [error: %g]\n",err<tol,err);
+
+err = merr(u_iisd2-u_lu);
+printf("IISD on 2 processors OK ? > %d, [error: %g]\n",err<tol,err);
+
+err = merr(u_iisd_cgs-u_lu);
+printf("IISD on 2 processors with CGS OK ? > %d, [error: %g]\n",err<tol,err);
