aa9210aacbd10f30e789dd7462cfe6707f4a63f8
mstorti <mstorti>
2001-08-02 01:52:33 +0000

    Moved code to src/distmat.cpp

--- test/distmap/distmat.cpp
+++ test/distmap/distmat.cpp
@@ -1,177 +1,24 @@
 /*__INSERT_LICENSE__*/
-// $Id: distmat.cpp,v 1.3 2001/08/01 20:05:33 mstorti Exp $
+// $Id: distmat.cpp,v 1.4 2001/08/02 01:52:33 mstorti Exp $
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
 
-#include "../../src/distmap.h"
+#include <utils.h>
+#include <maximizr.h>
+#include <buffpack.h>
+#include <distmap.h>
+#include <distmat.h>
+
 #include <petsc.h>
 
-int SIZE, MYRANK, M;
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-// Test for the distributed map class
-// A distributed map<int,int,double> class
-class Row : public  map<int,double> {
-  int size_of_pack() const;
-  void pack(char *&buff) const;
-};
-
-typedef DistMap<int,Row> DistMat;
-typedef map<int,Row> BasMap;
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-int DistMat::
-size_of_pack(const DistMat::iterator iter) const {
-  int n = iter->second.size();
-  // size + row number + size*(int+double)
-  return (n+2)*sizeof(int)+n*sizeof(double);
-}
-
-namespace BufferPack {
-
-  template<class T>
-  void pack(const T &t,char *& buff) {
-    memcpy(buff,&t,sizeof(T));
-    buff += sizeof(T);
-  }
-  
-  template<class T>
-  void unpack(T &t,const char *& buff) {
-    memcpy(&t,buff,sizeof(T));
-    buff += sizeof(T);
-  }
-  
-}
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-void DistMat::
-pack(const int &k,const Row &row,char *&buff) const {
-  int n = row.size();
-  Row::const_iterator iter;
-  BufferPack::pack(n,buff);
-  BufferPack::pack(k,buff);
-  for (iter=row.begin(); iter!=row.end(); iter++) {
-    BufferPack::pack(iter->first,buff);
-    BufferPack::pack(iter->second,buff);
-  }
-}
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-void DistMat::
-unpack(int &k,Row &row,const char *&buff) {
-  int n,j,key;
-  double val;
-  BufferPack::unpack(n,buff);
-  BufferPack::unpack(k,buff);
-  for (j=0; j<n; j++) {
-    BufferPack::unpack(key,buff);
-    BufferPack::unpack(val,buff);
-    row[key] += val;
-  }
-}
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-void DistMat::
-combine(const pair<int,Row> &p) {
-  int n,j;
-  map<int,Row>::iterator iter = find(p.first);
-  Row::iterator r;
-  Row::const_iterator q;
-  if (iter == end()) {
-    printf("[%d] inserting row %d\n",myrank,p.first);
-    // insert(p);
-    (*this)[p.first] = p.second;
-  } else {
-    Row &oldr = iter->second;
-    const Row &newr = p.second;
-    for (q = newr.begin(); q!=newr.end(); q++) {
-      r = oldr.find(q->first);
-      if (r == oldr.end()) {
-	r->second += q->second;
-      } else {
-	oldr.insert(*q);
-      }
-    }
-  }
-}
+extern int SIZE, MYRANK, M;
 
 int proc(int l) { return int((l*SIZE)/M);}
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-double maxd(int n,...) {
-  va_list list;
-  va_start(list,n);
-  double max,item;
-  for (int kk=0; kk<n; kk++) {
-    item = va_arg(list,double);
-    max = ( kk==0 ? item : ( max > item ? max : item));
-  }
-  va_end(list);
-  return max;
-}
 
 int
 DistMat::processor(const DistMat::iterator k) const {
   return proc(k->first);
-};
-
-class DistMatrix : public DistMat {
-public:
-  void insert_val(int i,int j,double v);
-  double val(int i,int j);
-};
-
-void DistMatrix::insert_val(int i,int j,double v) {
-  DistMat::iterator I = find(i);
-  Row::iterator J;
-  if (I == end()) {
-    insert(pair<int,Row>(i,Row()));
-    I = find(i);
-  }
-  Row &row = I->second;
-  J = row.find(j);
-  if (J == row.end()) {
-    row.insert(pair<int,double>(j,v));
-  } else {
-    J->second += v;
-  }
-}
-
-double DistMatrix::val(int i,int j) {
-  Row::iterator J;
-  DistMat::iterator I = find(i);
-  if (I == end()) return 0.;
-  Row &row = I->second;
-  J = row.find(j);
-  if (J == row.end()) {
-    return 0.;
-  } else {
-    return J->second;
-  }
-}  
-
-template<class T> 
-class Maximizer {
-private:
-  int ini;
-  T t_;
-  double max_;
-public:
-  void reset() {ini=0;};
-  const T &t;
-  const double &max;
-  Maximizer() : ini(0), max_(0), max(max_), t(t_) {};
-  void scan(T &tt,double val);
-};
-
-template<class T> 
-void Maximizer<T>::scan(T &tt,double val) {
-  if (!ini || val>max_) {
-    t_=tt; 
-    max_=val;
-    ini=1;
-  }
 };
 
 #define MAT(i,j) VEC2(mat,i,j,M)
@@ -258,9 +105,9 @@ int main(int argc,char **argv) {
 	p.first=j;
 	p.second=k;
 	maxerr.scan(p,fabs(e-w));
-	if (w!=0) 
-	  PetscSynchronizedPrintf(PETSC_COMM_WORLD,
-				  "(%d,%d) desired %f, found %f\n",j,k,w,e);
+//  	if (w!=0) 
+//  	  PetscSynchronizedPrintf(PETSC_COMM_WORLD,
+//  				  "(%d,%d) desired %f, found %f\n",j,k,w,e);
       }
     }
   }
