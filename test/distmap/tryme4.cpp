3748f75a74231076a1a493e29ecfd6809ade2c1f
mstorti <mstorti>
2001-08-19 00:18:47 +0000

    Doesn't loose memory. Reuses factorization and reuses matrix, and zeroes components of matrix.

--- test/distmap/tryme4.cpp
+++ test/distmap/tryme4.cpp
@@ -2,52 +2,72 @@
 #include <sles.h>
 
 int main(int argc,char **args) {
-  const int N=100;
-  const int M=10;
-  int ierr,m=3,its;
-  double v, *a, sum;
+  const int N=1000;
+  const int M=100;
+  int ierr,m=3,its, Nit=-1;
+  double v, *a, *a2, sum,sum2,dit;
   SLES sles;
   KSP ksp;
   PC pc;
   Mat A;
-  Vec x,b;
+  Vec x,b,x2,b2;
 
   PetscInitialize(&argc,&args,NULL,NULL);
   ierr = VecCreateSeq(PETSC_COMM_SELF,N,&x);
   ierr = VecCreateSeq(PETSC_COMM_SELF,N,&b);
-  
-  ierr = MatCreateSeqAIJ(PETSC_COMM_SELF,N,N,m,
- 			 PETSC_NULL,&A); 
+  ierr = VecCreateSeq(PETSC_COMM_SELF,N,&b2);
+  ierr = VecCreateSeq(PETSC_COMM_SELF,N,&x2);
 
-  ierr = SLESCreate(PETSC_COMM_SELF,&sles); CHKERRA(ierr); 
-  ierr = SLESSetOperators(sles,A,
-			  A,SAME_NONZERO_PATTERN); CHKERRA(ierr); 
-  ierr = SLESGetKSP(sles,&ksp); CHKERRA(ierr); 
-  ierr = SLESGetPC(sles,&pc); CHKERRA(ierr); 
+  dit=0.;
+  while (1) {
+    ierr = MatCreateSeqAIJ(PETSC_COMM_SELF,N,N,m,
+			   PETSC_NULL,&A); 
+    ierr = MatZeroEntries(A); CHKERRQ(ierr);
 
-  ierr = KSPSetType(ksp,KSPPREONLY); CHKERRA(ierr); 
-  ierr = PCSetType(pc,PCLU); CHKERRA(ierr); 
-//    double pc_lu_fill=2.;
-//    ierr = PCLUSetFill(pc,pc_lu_fill); CHKERRA(ierr); 
-  ierr = PCLUSetUseInPlace(pc); CHKERRA(ierr);
+    ierr = SLESCreate(PETSC_COMM_SELF,&sles); CHKERRA(ierr); 
+    ierr = SLESSetOperators(sles,A,
+			    A,SAME_NONZERO_PATTERN); CHKERRA(ierr); 
+    ierr = SLESGetKSP(sles,&ksp); CHKERRA(ierr); 
+    ierr = SLESGetPC(sles,&pc); CHKERRA(ierr); 
 
-  for (int j=0; j<N; j++) {
-    v=1.;
-    VecSetValue(b,j,v,ADD_VALUES);
-    v=1.;
-    MatSetValue(A,j,j,v,ADD_VALUES);
-    v=.1;
-    MatSetValue(A,j,(j+M)%N,v,ADD_VALUES);
-    MatSetValue(A,j,(j+N-M)%N,v,ADD_VALUES);
+    ierr = KSPSetType(ksp,KSPPREONLY); CHKERRA(ierr); 
+    ierr = PCSetType(pc,PCLU); CHKERRA(ierr); 
+    //    double pc_lu_fill=2.;
+    //    ierr = PCLUSetFill(pc,pc_lu_fill); CHKERRA(ierr); 
+    ierr = PCLUSetUseInPlace(pc); CHKERRA(ierr);
+    
+    for (int j=0; j<N; j++) {
+      v=dit;
+      VecSetValue(b,j,v,ADD_VALUES);
+      v=2.*dit;
+      VecSetValue(b2,j,v,ADD_VALUES);
+      v=1.;
+      MatSetValue(A,j,j,v,ADD_VALUES);
+      v=.1;
+      MatSetValue(A,j,(j+M)%N,v,ADD_VALUES);
+      MatSetValue(A,j,(j+N-M)%N,v,ADD_VALUES);
+    }
+    ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
+    ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
+
+    ierr = SLESSolve(sles,b,x,&its); CHKERRA(ierr); 
+    ierr = SLESSolve(sles,b2,x2,&its); CHKERRA(ierr); 
+    ierr = VecGetArray(x,&a); CHKERRQ(ierr); 
+    ierr = VecGetArray(x2,&a2); CHKERRQ(ierr); 
+    sum = 0.;
+    sum2 = 0.;
+    for (int j=0; j<N; j++) {
+      sum = sum += a[j];
+      sum2 = sum2 += a2[j];
+    }
+    printf("sum(x) = %f (expected %f)\n",sum,dit*double(N)/1.2);
+    printf("sum(x2) = %f (expected %f)\n",sum2,2*dit*double(N)/1.2);
+    ierr = VecRestoreArray(x,&a);
+    ierr = VecRestoreArray(x2,&a2);
+    ierr = SLESDestroy(sles); CHKERRA(ierr); 
+    ierr = MatDestroy(A); CHKERRA(ierr); 
+    dit +=1.;
+    if(Nit>0 && dit > double(Nit)) break;
   }
-  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
-  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY); CHKERRA(ierr);
-
-  ierr = SLESSolve(sles,b,x,&its);
-  ierr = VecGetArray(x,&a); CHKERRQ(ierr); 
-  sum = 0.;
-  for (int j=0; j<N; j++) sum = sum += a[j];
-  printf("sum(x) = %f (expected %f)\n",sum,double(N)/1.2);
-
   PetscFinalize();
 }
