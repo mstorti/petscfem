206b3acf58c558e1cb4dd642bebb88fce132f32c
mstorti <mstorti>
2001-11-23 00:39:22 +0000

    Adding test for `Graph' class

---
+++ test/graph/tryme.cpp
@@ -0,0 +1,11 @@
+/*__INSERT_LICENSE__*/
+// $Id: tryme.cpp,v 1.1 2001/11/23 00:39:22 mstorti Exp $
+
+#include <src/utils>
+#include <src/graph>
+
+#define M 1000
+int main(int argc, char **args) {
+
+
+}
