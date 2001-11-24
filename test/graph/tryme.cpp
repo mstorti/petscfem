fc80ef325da91bfcf8ad777defea6e635a0c30d7
mstorti <mstorti>
2001-11-24 01:05:27 +0000

    Checked that `Graph.clear()' doesn't loose memory.

--- test/graph/tryme.cpp
+++ test/graph/tryme.cpp
@@ -1,5 +1,5 @@
 /*__INSERT_LICENSE__*/
-// $Id: tryme.cpp,v 1.5 2001/11/24 01:01:52 mstorti Exp $
+// $Id: tryme.cpp,v 1.6 2001/11/24 01:05:27 mstorti Exp $
 
 #include <src/utils.h>
 #include <src/graph.h>
@@ -28,14 +28,14 @@ void TGraph::set_ngbrs(int elem,vector<int> &ngbrs_v) {
 
 int main(int argc, char **args) {
   const int N=200;
+  TGraph GG;
+  Graph &G = GG;
   int k=0;
   while (1) {
     printf("partitioning graph %d\n",k++);
-    TGraph *GG = new TGraph;
-    Graph &G = *GG;
-    GG->N = N;
+    GG.N = N;
     G.part(N,N,2);
-    delete GG;
+    G.clear();
 //      for (int j=0; j<N; j++) 
 //        printf("vrtx %d in proc %d\n",j,G.vrtx_part(j));
   }
