6189f79f525aa70e977a6ed90846b89fd434b015
mstorti <mstorti>
2001-12-02 20:46:12 +0000

    Test class for `adaptor_pg' class.

---
+++ applications/ns/qharm.cpp
@@ -0,0 +1,50 @@
+//__INSERT_LICENSE__
+//$Id: qharm.cpp,v 1.1 2001/12/02 20:46:12 mstorti Exp $
+
+#include <src/fem.h>
+#include <src/utils.h>
+#include <src/readmesh.h>
+#include <src/getprop.h>
+#include <src/fastmat2.h>
+
+#include "nsi_tet.h"
+#include "adaptor.h"
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+class qharm : public adaptor_pg {
+public:
+  double conductivity,rho_Cp;
+  void elemset_init();
+  void pg_connector(const FastMat2 &xpg,
+		    const FastMat2 &state_old_pg,
+		    const FastMat2 &grad_state_old_pg,
+		    const FastMat2 &state_new_pg,
+		    const FastMat2 &grad_state_new_pg,
+		    FastMat2 &res_pg,FastMat2 &mat_pg);
+};
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void qharm::elemset_init() {
+  assert(ndof==1);
+  conductivity=1.;
+  rho_Cp=1.;
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void qharm::pg_connector(const FastMat2 &xpg,
+			 const FastMat2 &state_old_pg,
+			 const FastMat2 &grad_state_old_pg,
+			 const FastMat2 &state_new_pg,
+			 const FastMat2 &grad_state_new_pg,
+			 FastMat2 &res_pg,FastMat2 &mat_pg) {
+  res_pg
+    .prod(dshapex,grad_state_new_pg,-1,1,-1,2)
+    .scale(-conductivity);
+  mat_pg
+    .ir(2,1)			// Set `dof' indices
+    .ir(4,1)
+    .prod(dshapex,dshapex,-1,1,-1,2) // add prod.
+    .scale(-conductivity)	// scale by conductivity
+    .rs();
+  
+}
