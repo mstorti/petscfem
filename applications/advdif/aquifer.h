839170fdbaa6573e6e5149ea38bf7ed4a3761d28
mstorti <mstorti>
2002-01-14 10:37:05 +0000

    This is the `advdif' class (in fact it's only diffusive) for the subsurface part. Right now it only contains the freatic aquifer. Later, aquitards and cofined aquifers may be added.

---
+++ applications/advdif/aquifer.h
@@ -0,0 +1,88 @@
+// -*- mode: C++ -*- 
+/*__INSERT_LICENSE__*/
+// $Id: aquifer.h,v 1.1 2002/01/14 10:37:05 mstorti Exp $
+#ifndef AQUIFER_H
+#define AQUIFER_H
+
+#include "advective.h"
+
+class aquifer_ff : public NewAdvDifFF {
+ public:
+  /** This is called before any other in a loop and may help in
+      optimization 
+      @param ret_options (input/output) this is used by the flux
+      function writer for returning some options. Currently the only
+      option used is #SCALAR_TAU#. This options tells the elemset
+      whether the flux function returns a scalar or matrix
+      #tau_supg#. 
+  */ 
+  void start_chunk(int &ret_options);
+
+  /** This is called before entering the Gauss points loop and may
+      help in optimization. 
+      @param element (input) an iterator on the elemlist. 
+  */ 
+  void element_hook(ElementIterator &element);
+
+  /** @name Advective jacobians related */
+  //@{
+  /** Computes the product #(A_grad_N)_(p,mu,nu) = A_(i,mu,nu) (grad_N)_(i,p)#
+      @param A_grad_N (output, size #nel# x #nd# x #nd#) 
+      @param grad_N (input, size #nel# x #ndof#)
+  */ 
+  void comp_A_grad_N(FastMat2 & A_grad_N,FastMat2 & grad_N);
+
+  /** Computes the product #(A_jac_n)_(mu,nu) = A_(i,mu,nu) normal_i#
+      @param A_jac_n (output, size #ndof# x #ndof#) 
+      @param normal (input, size #ndim#)
+  */ 
+  void comp_A_jac_n(FastMat2 &A_jac_n, FastMat2 &normal);
+
+  /** Computes fluxes, upwind parameters etc...
+      fixme:= more doc here ...
+  */ 
+  void compute_flux(COMPUTE_FLUX_ARGS);
+  //@}
+
+  /** @name Diffusive jacobians related */
+  //@{
+  /** Computes the product #(grad_N_D_grad_N)_(p,mu,q,nu) 
+      = D_(i,j,mu,nu) (grad_N)_(i,p) (grad_N)_(j,q)#
+      @param grad_N_D_grad_N (output) size #nel# x #ndof# x #nel# x #ndof# 
+      @param grad_N (input) size #nel# x #ndof#
+  */ 
+  void comp_grad_N_D_grad_N(FastMat2 &grad_N_D_grad_N,
+			    FastMat2 & dshapex,double w);
+  //@}
+
+  /** @name Reactive jacobians related */
+  //@{
+  /** Computes the product #(N_N_C)_(p,mu,q,nu) 
+      = w C_(mu,nu) N_p N_q#
+      @param N_N_C (output, size #nel# x #ndof# x #nel# x #ndof# 
+      @param N (input) FEM interpolation function size #nel#
+      @param w (input) a scalar coefficient
+  */ 
+  void comp_N_N_C(FastMat2 &N_N_C,FastMat2 &N,double w);
+
+  /** Computes the product #(N_P_C)_(mu,q,nu) 
+      = w (P_supg)_(mu,lambda) C_(lambda,nu) N_q #
+      @param N_P_C (output) size  #ndof# x #nel# x #ndof# 
+      @param P_supg (input) SUPG perturbation function size #ndof# x #ndof#
+      @param N (input) FEM interpolation function size #nel#
+      @param w (input) a scalar coefficient
+  */ 
+  void comp_N_P_C(FastMat2 &N_P_C, FastMat2 &P_supg,
+		  FastMat2 &N,double w);
+  //@}
+
+  aquifer_ff(const NewAdvDif *e) : NewAdvDifFF(e) {}
+};
+
+class aquifer : public NewAdvDif {
+ public:
+  aquifer() :  NewAdvDif(new aquifer_ff(this)) {};
+  // ~aquifer() { delete adv_diff_ff; }
+};
+
+#endif
