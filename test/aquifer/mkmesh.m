f3d55a528dc241541e16487098e7ca6b76aa8ca5
mstorti <mstorti>
2002-01-15 21:35:40 +0000

    Test for the aquifer elemset.

---
+++ test/aquifer/mkmesh.m
@@ -0,0 +1,25 @@
+source("data.m.tmp");
+
+w=zhomo([0 Lx 0 Ly],Nx+1,Ny+1);
+[xnod,icone] = pfcm2fem(w);
+icone = icone(:,[1 4 3 2]);
+
+asave("aqui.nod.tmp",xnod);
+asave("aqui.con.tmp",icone);
+
+x=xnod(:,1);
+y=xnod(:,2);
+
+tol=1e-5;
+
+fid = fopen("aqui.fixa.tmp","w");
+x0 = (1:Ny+1)';
+for k=x0'
+  fprintf(fid,"%d 1 0.\n",k);
+endfor
+
+xL = Nx*(Ny+1)+(1:Ny+1)';
+for k=xL'
+  fprintf(fid,"%d 1 1.\n",k);
+endfor
+fclose(fid);
