d6a4abdf2fe2b094d67fa9663509364192509ad6
mstorti <mstorti>
2002-02-02 15:18:57 +0000

    Verified that stream object gives the same solution for linear and circular paths.

--- test/aquifer/mkmeshs.m
+++ test/aquifer/mkmeshs.m
@@ -1,7 +1,13 @@
 source("data.m.tmp");
 
 x=[(0:Nx)'/Nx*Lx zeros(Nx+1,1)];
-ico=[(1:Nx)' (2:Nx+1)'];
+
+if 0 # || circular_path
+  phi = x(:,1)/Lx*pi;
+  x = Lx/pi*[cos(phi) sin(phi) 0*phi];
+endif
+
+ico = [(1:Nx)' (2:Nx+1)'];
 
 asave("stream.nod.tmp",x);
 asave("stream.con.tmp",ico);
