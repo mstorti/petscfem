f8352306b2e0a5067073ee9a8f30cd48bbd4b74d
mstorti <mstorti>
2002-02-05 00:57:29 +0000

    Adapted `stream' test for `eta' passed as `H' variable.

--- test/aquifer/mkmeshs.m
+++ test/aquifer/mkmeshs.m
@@ -1,12 +1,14 @@
 source("data.m.tmp");
 
 x=[(0:Nx)'/Nx*Lx zeros(Nx+1,1)];
+s = x(:,1);                     # arc length
 
-if 0 # || circular_path
+if circular_path
   phi = x(:,1)/Lx*pi;
-  x = Lx/pi*[cos(phi) sin(phi) 0*phi];
+  x = Lx/pi*[cos(phi) sin(phi)];
 endif
 
+x=[x -slope*s];
 ico = [(1:Nx)' (2:Nx+1)'];
 
 ini=ones(rows(x),1);
