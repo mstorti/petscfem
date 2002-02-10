6a31cd7d54de4b27bb2557f1c8baf6b50efc3a82
mstorti <mstorti>
2002-02-10 19:53:45 +0000

    Secondary extension function file

---
+++ test/aquifer/fun2.cpp
@@ -0,0 +1,43 @@
+//__INSERT_LICENSE__
+//$Id: fun2.cpp,v 1.1 2002/02/10 19:53:45 mstorti Exp $
+
+#include <math.h>
+
+#include <src/fem.h>
+#include <src/getprop.h>
+#include <src/ampli.h>
+
+struct  MyFunData {
+  double f0, f1, t0, t1, slope;
+};
+
+extern "C" void init_fun(TextHashTable *thash,void *&fun_data) {
+  int ierr;
+  TGETOPTDEF(thash,double,t0,0.);
+  TGETOPTDEF(thash,double,t1,1.);
+  TGETOPTDEF(thash,double,f0,0.);
+  TGETOPTDEF(thash,double,f1,1.);
+  printf("in init: t0 %f,t1 %f,f0 %f,f1 %f\n",t0,t1,f0,f1);
+
+  MyFunData *d = new MyFunData;
+  fun_data = d;
+  d->f0 = f0;
+  d->f1 = f1;
+  d->t0 = t0;
+  d->t1 = t1;
+  d->slope = (f1-f0)/(t1-t0);
+}
+
+extern "C" double eval_fun(double t,void *fun_data) {
+  MyFunData *d = (MyFunData *) fun_data;
+  // printf("in eval_fun: t0 %f,t1 %f,f0 %f,f1 %f\n",d->t0,d->t1,d->f0,d->f1);
+  if (t < d->t0) return d->f0;
+  else if (t > d->t1) return d->f1;
+  else return d->f0 + d->slope *(t - d->t0);
+}
+
+extern "C" void clear(void *fun_data) {
+  MyFunData *d = (MyFunData *) fun_data;
+  delete d;
+  fun_data=NULL;
+}
