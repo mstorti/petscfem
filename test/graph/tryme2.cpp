db2b49428a850ca99b126ecaee418469ab5b2c6a
mstorti <mstorti>
2002-07-17 22:45:30 +0000

    Assign correctly the map. Probably leaks memory.

--- test/graph/tryme2.cpp
+++ test/graph/tryme2.cpp
@@ -1,8 +1,18 @@
 /*__INSERT_LICENSE__*/
-// $Id: tryme2.cpp,v 1.2 2002/07/17 22:33:54 mstorti Exp $
+// $Id: tryme2.cpp,v 1.3 2002/07/17 22:45:30 mstorti Exp $
 
-#include <cstdio>
 #include <glib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+double drand() {  
+  return ((double)(rand()))/((double)(RAND_MAX));
+}
+
+int irand(int imin,int imax) {
+  return int(rint(drand()*double(imax-imin+1)-0.5))+imin;
+}
 
 int int_compare(const void *aa,const void *bb) {
   const int *a = (const int *)aa;
@@ -12,8 +22,12 @@ int int_compare(const void *aa,const void *bb) {
 
 int main() {
   GTree* tree = g_tree_new(int_compare);
-  for (int j=0; j<10; j++) { 
-    g_tree_insert(tree,new int(j),new int(j*j)); 
+  for (int j=0; j<100; j++) { 
+    int k,l;
+    k = irand(1,10);
+    l = irand(1,10);
+    printf("%d -> %d\n",k,l);
+    g_tree_insert(tree,new int(k),new int(l)); 
   }
   for (int k=-10; k<20; k++) {
     int *v = (int *)g_tree_lookup(tree,&k);
@@ -24,4 +38,3 @@ int main() {
     }
   }
 }
-
