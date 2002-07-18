2a447c9936749b85b1b698b661920489aa3a1c8c
mstorti <mstorti>
2002-07-18 21:43:37 +0000

    *** empty log message ***

--- test/sparse/tryme3.cpp
+++ test/sparse/tryme3.cpp
@@ -1,5 +1,5 @@
 /*__INSERT_LICENSE__*/
-// $Id: tryme3.cpp,v 1.3 2002/07/18 20:01:44 mstorti Exp $
+// $Id: tryme3.cpp,v 1.4 2002/07/18 21:43:37 mstorti Exp $
 
 #include <cmath>
 #include <map>
@@ -11,6 +11,15 @@
 
 TextHashTable GLOBAL_OPTIONS;
 
+#define ALLOC malloc_alloc
+
+typedef set< int, less<int>, ALLOC > SET;
+
+class MAP : public map< int, SET, less<int>, ALLOC > {
+public:
+  void add(int i,int j) { (*this)[i].insert(j); }
+};
+
 int main(int argc, char **argv) {
   PetscInitialize(&argc,&argv,NULL,NULL);
 
@@ -19,11 +28,12 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(PETSC_COMM_WORLD,&MY_RANK);
 
   int M=1000000;
-  StoreGraph g(M,&seq_partitioner);
-  for (int j=0; j<M; j++) { g.add(irand(1,M*M),irand(1,M*M)); }
-  g.clear();
+  // StoreGraph g(M,&seq_partitioner);
+  MAP g;
+  for (int j=0; j<M; j++) { g.add(irand(1,M),irand(1,M)); }
   double *a = new double[3*M];
   for (int j=0; j<3*M; j++) a[j] = double(j);
+  g.clear();
   delete[] a;
 
   PetscFinalize();
