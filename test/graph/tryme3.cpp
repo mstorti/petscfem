a69e7cfa5c18d942eb284e217307861ce954fdf9
mstorti <mstorti>
2002-07-24 02:14:30 +0000

    *** empty log message ***

--- test/graph/tryme3.cpp
+++ test/graph/tryme3.cpp
@@ -1,7 +1,9 @@
 /*__INSERT_LICENSE__*/
-// $Id: tryme3.cpp,v 1.5 2002/07/24 01:19:58 mstorti Exp $
+// $Id: tryme3.cpp,v 1.6 2002/07/24 02:14:30 mstorti Exp $
+#define _GNU_SOURCE
 
 #include <src/utils.h>
+#include <src/debug.h>
 #include <src/linkgraph.h>
 
 int MY_RANK,SIZE;
@@ -30,21 +32,100 @@ typedef
 DistCont<LinkGraph,LinkGraphRow,Part>  LinkGraphDis;
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+/// Size of packed row (plus header)
+int LinkGraphDis::size_of_pack(Row const & row) const {
+  int n = row.size();
+  // size + row number + size*(int+double)
+  return (n+2)*sizeof(int);
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+/// Pack the row
+void LinkGraphDis::pack(const Row & row,char *&buff) const {
+  int n=row.size();
+  BUFFER_PACK<int>(row.row,buff);
+  BUFFER_PACK<int>(n,buff);
+  Row::iterator q;
+  for (q=row.begin(); q!=row.end(); q++)
+    BUFFER_PACK<int>(*q,buff);
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void LinkGraphDis::unpack(Row & row,const char *&buff)  {
+  int n,k;
+  BUFFER_UNPACK<int>(row.row,buff);
+  BUFFER_UNPACK<int>(n,buff);
+  for (int j=0; j<n; j++) {
+    BUFFER_UNPACK<int>(k,buff);
+    row.insert(k);
+  }
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+/// combine a row in the container
+void LinkGraphDis::combine(const Row &row) {
+  int j=row.row;
+  Row::iterator q;
+  for (q=row.begin(); q!=row.end(); q++) list_insert(j,*q);
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void graph_print(LinkGraphDis &graph, char *s=NULL) {
+  if (s) printf("%s\n",s);
+  for (LinkGraph::iterator k=graph.begin(); k!=graph.end(); k++) {
+    row_print(*k);
+    printf("size of row: %d\n",k.size());
+  }
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 int main(int argc, char **args) {
   MPI_Init(&argc,&args);
   MPI_Comm_size (MPI_COMM_WORLD, &SIZE);
   MPI_Comm_rank (MPI_COMM_WORLD, &MY_RANK);
 
-  LinkGraphDis graph(&part,MPI_COMM_WORLD);
+#if 0
+  Debug debug;
+  debug.activate();
+  Debug::init();
+  debug.trace("start? ");
+#endif
+
+  LinkGraphDis graph(&part,MPI_COMM_WORLD), graph2(&part,MPI_COMM_WORLD);
+  
   graph.init(M);
+  graph2.init(M);
   for (int j=0; j<M; j++) {
     graph.add(j,modulo(j+1,M));
     graph.add(j,modulo(j-1,M));
   }
+
+  printf("-------------\nBefore copying: \n");
+  graph_print(graph,"graph: ");
+  graph_print(graph2,"graph2: ");
+
+  for (LinkGraph::iterator q=graph.begin();
+       q!=graph.end(); q++) {
+    LinkGraphRow row = *q;
+    graph2.combine(row);
+    graph.erase(q);
+  }
+
+  printf("-------------\nAfter copying: \n");
+  graph_print(graph,"graph: ");
+  graph_print(graph2,"graph2: ");
+
+#if 0
   graph.scatter();
   LinkGraphDis::iterator k;
-  for (k=graph.begin(); k!=graph.end(); k++) {
-    row_print(*k);
-    printf("size of row: %d\n",k.size());
+  for (int p=0; p<SIZE; p++) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (p==MY_RANK) {
+      for (k=graph.begin(); k!=graph.end(); k++) {
+	row_print(*k);
+	printf("size of row: %d\n",k.size());
+      }
+    }
   }
+#endif
 }
