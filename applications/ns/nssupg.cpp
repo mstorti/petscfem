c6bcd30e44876e5bb291ecb3b6edc01ac2152fde
mstorti <mstorti>
2002-07-30 20:00:50 +0000

    `nssupg' compiles

--- applications/ns/nssupg.cpp
+++ applications/ns/nssupg.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: nssupg.cpp,v 1.1 2002/07/30 16:13:27 mstorti Exp $
+//$Id: nssupg.cpp,v 1.2 2002/07/30 20:00:50 mstorti Exp $
 
 #include <src/fem.h>
 #include <src/utils.h>
@@ -36,8 +36,12 @@ int ns_sup_g::assemble(arg_data_list &arg_data_v,Nodedata *nodedata,
   // so that for $f=0$ we recover the slip boundary
   // condition $w=0$. 
   TGETOPTDEF(thash,double,fs_eq_factor,1.);
+  //o Scales a diffusion term on the surface that
+  // tends to smooth the free surface equation
+  TGETOPTDEF(thash,double,free_surface_visco,0.);
+
   assert(nel % 2==0); // one layer of nodes is for \eta the other for w
-  nel2 = nel/2;
+  int nel2 = nel/2;
 
   GET_JOBINFO_FLAG(comp_mat);
   GET_JOBINFO_FLAG(comp_mat_res);
@@ -93,10 +97,14 @@ int ns_sup_g::assemble(arg_data_list &arg_data_v,Nodedata *nodedata,
     veccontr(2,nel,ndof),
     eta(1,nel2), eta_new(1,nel2), 
     w(1,nel2), w_new(1,nel2), w_star(1,nel2), 
-    matlocf(4,nel,ndof,nel,ndof),xloc(2,nel2,ndim);
+    res, matlocf(4,nel,ndof,nel,ndof),xloc(2,nel2,ndim),
+    res_pg, res_pgg, n, tmp, tmp1, grad_eta, tmp2, mass_mat, lap_mat,
+    tmp3, tmp4, eta_star, Jaco;
 
-  double w,w_new,w_star,eta,eta_new,eta_star,res;
-
+  // Unpack nodedata
+  int nu=nodedata->nu;
+  //o Number of Gauss points.
+  TGETOPTNDEF(thash,int,npg,none);
   //o Type of element geometry to define Gauss Point data
   TGETOPTDEF_S(thash,string,geometry,cartesian2d);
   GPdata gp_data(geometry.c_str(),ndim,nel,npg,GP_FASTMAT2);
@@ -125,9 +133,10 @@ int ns_sup_g::assemble(arg_data_list &arg_data_v,Nodedata *nodedata,
     } else if (comp_mat_res || comp_res) {
 
       // Load local node coordinates in local vector
+#define NODEDATA(j,k) VEC2(nodedata->nodedata,j,k,nu)
 #define ICONE(j,k) (icone[nel*(j)+(k)]) 
-      for (kloc=0; kloc<nel2; kloc++) {
-	node = ICONE(k,kloc);
+      for (int kloc=0; kloc<nel2; kloc++) {
+	int node = ICONE(k,kloc);
 	xloc.ir(1,kloc+1).set(&NODEDATA(node-1,0));
       }
       xloc.rs();
@@ -135,11 +144,12 @@ int ns_sup_g::assemble(arg_data_list &arg_data_v,Nodedata *nodedata,
       locstate2.set(&(LOCST2(ielh,0,0)));
 
       // Surface elevations and normal velocities at t^{n+1}
+      // `_new' values
       locstate.is(1,nel2+1,nel).ir(2,1);
-      eta.set(locstate);
+      eta_new.set(locstate);
       locstate.rs();
       locstate.is(1,1,nel2).ir(2,normal_dir);
-      w.set(locstate);
+      w_new.set(locstate);
       locstate.rs();
 
       // Surface elevations and normal velocities at t^{n}
@@ -150,20 +160,55 @@ int ns_sup_g::assemble(arg_data_list &arg_data_v,Nodedata *nodedata,
       w.set(locstate2);
       locstate2.rs();
 
+      res.set(0.);
+      mass_mat.set(0.);
+      matlocf.set(0.);
+      veccontr.set(0.);
 
-      locstate.is(1,nel2+1,nel).ir(2,1);
-      eta_new = locstate.get(2,1);
-      // Normal velocities at t^n, t^{n+1}, t^*
-      w = locstate2.get(1,normal_dir);
-      w_new = locstate.get(1,normal_dir);
-      w_star = alpha * w_new + (1.-alpha) * w;
-      // Residual of equation (eta^{n+1}-eta^n)/Dt - w = 0
-      res = -(eta_new-eta)*rec_Dt*fs_eq_factor + w_star;
-      veccontr.setel(res,2,1);
-      // alpha's here are not clear. 
-      matlocf.setel(rec_Dt/alpha*fs_eq_factor,2,1,2,1)
-	.setel(-alpha,2,1,1,ndim);
-      veccontr.export_vals(&(RETVAL(ielh,0,0)));
+      // Normal velocities at t^{*}
+      w_star.set(w_new).scale(alpha).axpy(w,1-alpha);
+      // elevation at t^*
+      eta_star.set(eta_new).scale(alpha).axpy(eta,1-alpha);
+      // Residual of free surface equation at t^* (nodal values).
+      tmp.set(eta_new).rest(eta).scale(-rec_Dt*fs_eq_factor).add(w_star);
+
+#define DSHAPEXI (*gp_data.FM2_dshapexi[ipg])
+#define SHAPE    (*gp_data.FM2_shape[ipg])
+#define WPG      (gp_data.wpg[ipg])
+
+      veccontr.is(1,nel2+1,nel).ir(2,1);
+      // loop over Gauss points
+      for (int ipg=0; ipg<npg; ipg++) {
+	
+	Jaco.prod(DSHAPEXI,xloc,1,-1,-1,2);
+	double detJaco = mydetsur(Jaco,n);
+	double wpgdet = detJaco*WPG;
+
+	// Residual of equation (eta^{n+1}-eta^n)/Dt - w = 0
+	res_pg.prod(tmp,SHAPE,-1,-1);
+	res_pgg.set(SHAPE).scale(res_pg.get()*wpgdet);
+	
+	grad_eta.prod(DSHAPEXI,eta,1,-1,-1);
+	tmp2.prod(DSHAPEXI,grad_eta,-1,1,-1);
+	res_pgg.axpy(tmp2,-free_surface_visco*wpgdet);
+	veccontr.add(res_pgg);
+
+	// Jacobian term
+	tmp3.prod(SHAPE,SHAPE,1,2);
+	mass_mat.axpy(tmp3,wpgdet);
+	tmp4.prod(DSHAPEXI,DSHAPEXI,-1,1,-1,2);
+	lap_mat.axpy(tmp4,wpgdet);
+      }
+
+      veccontr.rs().export_vals(&(RETVAL(ielh,0,0)));
+
+      matlocf.is(1,nel2+1,nel).ir(1,1).is(3,nel2+1,nel).ir(4,1)
+	.set(mass_mat).scale(rec_Dt/alpha*fs_eq_factor) // temporal term
+	.axpy(lap_mat,free_surface_visco) // surface diffusion term
+	.rs();
+      matlocf.is(1,nel2+1,nel).ir(1,1).is(3,1,nel2).ir(4,normal_dir)
+	.set(mass_mat).scale(-alpha).rs();
+      
       if (update_jacobian) matlocf.export_vals(&(RETVALMAT(ielh,0,0,0,0)));
 
     }
