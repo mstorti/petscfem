d45aa41ef6c168148c1a6a6fbec6fc691b0889e7
mstorti <mstorti>
2002-09-11 00:27:49 +0000

    Added printing build date, time and compiler to NS module.

--- applications/ns/compdate.cpp
+++ applications/ns/compdate.cpp
@@ -1,17 +1,25 @@
 //__INSERT_LICENSE__
-//$Id: compdate.cpp,v 1.1 2002/09/10 23:47:53 mstorti Exp $
+//$Id: compdate.cpp,v 1.2 2002/09/11 00:27:49 mstorti Exp $
 #include <cstdio>
 #include <petsc.h>
 
+#ifndef PETSCFEM_HOSTNAME
+#define PETSCFEM_HOSTNAME "<unknown host>"
+#endif
+
 void print_petscfem_link_date() {
   PetscPrintf(PETSC_COMM_WORLD,
-	      "NS-module built on " __DATE__ ", " __TIME__ ", with "
+	      "-----------------------------------------------------------------\n"
+	      "Navier-Stokes module built on " __DATE__ ", " __TIME__ ", with "
 #ifdef __GNUC__
-	      "GCC %d.%d.%d\n",__GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__
+	      "GCC %d.%d.%d\n"
 #else
 	      "<unknown compiler>\n"
 #endif
+	      "on host: " PETSCFEM_HOSTNAME "\n"
+	      "-----------------------------------------------------------------\n",
+#ifdef __GNUC__
+	      __GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__
+#endif
 	      );
 }
-
-
