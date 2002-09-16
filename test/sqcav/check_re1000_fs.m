171176b65ba31370a89e4be2caaff70c662da8e1
mstorti <mstorti>
2002-09-16 16:05:34 +0000

    Back to beta-3.03 to run sqcav tests.

--- test/sqcav/check_re1000_fs.m
+++
@@ -1,9 +0,0 @@
-source("data.m.tmp");
-
-u=aload("sqcav.fractional_step.tmp");
-load -force u_re1000_fs.ref
-
-tol=1e-8;
-erro = merr(u(ny,1)-uu);
-printf("Fractional step error < tol OK ? %d (error = %g, tol = %g)\n", \
-       erro<tol,erro,tol);
