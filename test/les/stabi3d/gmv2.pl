f112bfa676bcbd59a0c1e6f5967be5d22ae4dc9f
mstorti <mstorti>
2002-11-11 03:14:20 +0000

    Working on DX visualization

--- test/les/stabi3d/gmv2.pl
+++ test/les/stabi3d/gmv2.pl
@@ -5,9 +5,9 @@ $nod = "$case.nod.tmp";	# nodes
 $con = "$case.con.tmp";	# connectivities
 $gmv = "$case.gmv.tmp";	# MV input file
 
-$rslt = "stabi.state.tmp"; # petscfem result file
+# $rslt = "stabi.state.tmp"; # petscfem result file
 # $rslt = "stabi.ini.tmp"; # petscfem result file
-# $rslt = "outvector0.out"; # petscfem result file
+$rslt = "outvector0.out.tmp"; # petscfem result file
 $rec = 'last';
 
 # $fields = 'scalar';
