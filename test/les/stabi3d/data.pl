50e9d7ddc724628b8a1f7b3347ab71c0aa7ffd84
mstorti <mstorti>
2002-11-22 12:07:34 +0000

    *** empty log message ***

--- test/les/stabi3d/data.pl
+++ test/les/stabi3d/data.pl
@@ -1,10 +1,10 @@
 # -*- mode: shell-script -*-
 
-$hratio=4;			# refinement ratio center/wall
+$hratio=2;			# refinement ratio center/wall
 $Rel=6000;			# Reynolds number
 $Lx=2;				# Length in the longitudinal direction
 $Ly=1;				# Plate separation
 $Lz=2;				# Length in transversal (periodic)  direction
-$Nx=40;				# number of elements in x direction
-$Ny=30;				# number of elements in y direction
-$Nz=60;				# number of elements in z direction
+$Nx=30;				# number of elements in x direction
+$Ny=40;				# number of elements in y direction
+$Nz=30;				# number of elements in z direction
