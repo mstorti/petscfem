3ff4b35454f5fa725ea9cb7cd2f4bbc2b7504a16
mstorti <mstorti>
2002-11-24 09:52:54 +0000

    Running LES example

--- test/les/stabi3d/data.pl
+++ test/les/stabi3d/data.pl
@@ -1,9 +1,9 @@
 # -*- mode: shell-script -*-
 
-$hratio=1;			# refinement ratio center/wall
-$Rel=3000;			# Reynolds number
-$Lx=$Lz=0.5;			# Length in the longitudinal and transverse direction
+$hratio=3;			# refinement ratio center/wall
+$Rel=100000;			# Reynolds number
+$Lx=$Lz=2;			# Length in the longitudinal and transverse direction
 $Ly=1;				# Plate separation
-$Nz=$Nx=10;			# number of elements in
+$Nz=$Nx=16;			# number of elements in
 				# longitudinal and transverse direction
 $Ny=40;				# number of elements in y direction
