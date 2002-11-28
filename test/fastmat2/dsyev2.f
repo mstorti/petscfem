bd87cf0c6d2565f12c5946c68e1ab59034068404
mstorti <mstorti>
2002-11-28 02:06:32 +0000

    *** empty log message ***

--- test/fastmat2/dsyev2.f
+++ test/fastmat2/dsyev2.f
@@ -1,5 +1,6 @@
       subroutine   dsyev2(n, a, w, work, lwork, info );
       implicit real*8 (a-h,o-z)
-      call dsyev('V','U',n, a,lda, w, work, lwork, info);
+      write (*,*) n,lwork
+      call dsyev('V','U',n, a,n, w, work, lwork, info);
       return
       end
