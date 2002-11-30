14cfbfcaf9086b89ae90e6a0fee29c7ea3237795
mstorti <mstorti>
2002-11-30 20:24:41 +0000

    Runs strong test for deforming 10x10 2D mesh.

--- test/mmove/mkstep.m
+++ test/mmove/mkstep.m
@@ -2,20 +2,25 @@ source("data.m.tmp");
 w = zhomo([0 1 0 1],N+1,N+1);
 [xnod,icone] = pfcm2fem(w);
 
-## icone = icone(:,[1 4 3 2]);
-icone=[icone(:,[1 3 2]);
-       icone(:,[1 3 4])];
+if strcmp(shape,"quad")
+  icone = icone(:,[1 4 3 2]);
+else
+  icone=[icone(:,[1 3 2]);
+	 icone(:,[1 3 4])];
+endif
 
 asave("step.nod.tmp",xnod);
 asave("step.con.tmp",icone);
 
 fid = fopen("step.fixa.tmp","w");
 for k=1:N+1
-  dd = disp;
-  if k<=N/2
-    dd = 0;
-  elseif k==N/2+1
-    dd=disp/2; 
+  y = xnod(k,2);
+  if y<.3
+    dd = 0.;
+  elseif y>.7
+    dd = disp;
+  else
+    dd = disp*(y-.3)/.4;
   endif
   fprintf(fid,"%d %d %f\n",k,1,dd);
   fprintf(fid,"%d %d %f\n",k,2,0.);
