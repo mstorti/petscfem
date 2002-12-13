49b105edf03c91a9da0bed281fa94e833be17cb3
mstorti <mstorti>
2002-12-13 17:17:25 +0000

    *** empty log message ***

--- test/advec/advec.m
+++ test/advec/advec.m
@@ -9,10 +9,29 @@ icone=[icone(:,[1 4 3]);
 asave("advec.nod.tmp",xnod);
 asave("advec.con.tmp",icone);
 
+if !exist("ydisc"); ydisc = .5; endif
+
+yydisc = ydisc*ux/sqrt(ux^2+uy^2);
+
 fid = fopen("advec.fixa.tmp","w");
 for k=1:M+1
-  y = xnod(k,2);
-  phi = tanh((y-0.5)/delta);
-  fprintf(fid,"%d %d %f\n",k,1,phi);
+  node = k;
+  ## Coordinate orthogonal to velocity
+  yy = xnod(node,:)*[-uy ux]'/sqrt(ux^2+uy^2);
+  phi = tanh((yy-yydisc)/delta);
+  fprintf(fid,"%d %d %f\n",node,1,phi);
 endfor
+fclose(fid);
+
+uy>=0 || error("not uy<0 allowed");
+fid = fopen("advec.fixa-y0.tmp","w");
+if uy>0
+  for k=2:N+1
+    node = (M+1)*(k-1)+1;
+    ## Coordinate orthogonal to velocity
+    yy = xnod(node,:)*[-uy ux]'/sqrt(ux^2+uy^2);
+    phi = tanh((yy-yydisc)/delta);
+    fprintf(fid,"%d %d %f\n",node,1,phi);
+  endfor
+endif  
 fclose(fid);
--- test/advec/proc.m
+++ test/advec/proc.m
@@ -1,4 +1,7 @@
 source("data.m.tmp");
 
 phi=aload("save.state.tmp");
+x=aload("advec.nod.tmp");
+y=x(1:M+1,2);
 phi = reshape(phi,M+1,N+1);
+plot(y,phi);
