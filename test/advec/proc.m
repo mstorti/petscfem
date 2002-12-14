9a8c7bf9dde2d14b574c9adcb93458ac32c97e2f
mstorti <mstorti>
2002-12-14 14:04:34 +0000

    Runs cone advection example.

--- test/advec/proc.m
+++ test/advec/proc.m
@@ -1,8 +1,10 @@
 source("data.m.tmp");
 
-phi=aload("save.state.tmp");
 x=aload("advec.nod.tmp");
-y=x(1:M+1,2);
-## phi = reshape(phi,M+1,N+1);
-## plot(y,phi);
+phi_ini=aload("advec.ini.tmp");
+[xc0,sx0,sy0]=bellpar(x,phi_ini);
+phi=aload("save.state.tmp");
+[xc,sx,sy]=bellpar(x,phi);
 
+xrate = (sx/sx0-1)/(xc(1)-xc0(1))
+yrate = (sy/sy0-1)/(xc(1)-xc0(1))
