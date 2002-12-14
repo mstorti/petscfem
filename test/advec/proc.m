90ee39a70d6f3508b4b25984897d181add95d936
mstorti <mstorti>
2002-12-14 12:20:43 +0000

    Runs advection of Gaussian example.

--- test/advec/proc.m
+++ test/advec/proc.m
@@ -3,5 +3,6 @@ source("data.m.tmp");
 phi=aload("save.state.tmp");
 x=aload("advec.nod.tmp");
 y=x(1:M+1,2);
-phi = reshape(phi,M+1,N+1);
-plot(y,phi);
+## phi = reshape(phi,M+1,N+1);
+## plot(y,phi);
+
