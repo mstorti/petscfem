e9a09f649e52c182c368e0a4fcb41971e3a77499
mstorti <mstorti>
2002-12-19 18:51:25 +0000

    *** empty log message ***

--- test/plate/mkplate.m
+++ test/plate/mkplate.m
@@ -1,8 +1,15 @@
 source("data.m.tmp");
 
-w=zhomo([0 Ly 0 Lx],Ny+1,Nx+1);
+w=zhomo([0 Ly 0 Lx],Ny+1,Nx+1,[1 0 yratio 1 0 1]);
 [xnod,icone] = pfcm2fem(w);
 xnod = xnod(:,[2 1]);
+
+xnod(:,1) = xnod(:,1)-xini;
+x = xnod(:,1);
+y = xnod(:,2);
+
+dy = 4*t/Lplate^2* (x>0. & x<=1.).*x.*(Lplate-x).*(1-y/Ly);
+xnod = [x y+dy];
 
 asave("plate.nod.tmp",xnod);
 asave("plate.con.tmp",icone);
@@ -15,8 +22,12 @@ asave("plate.ini.tmp",ini);
 fid = fopen("plate.fixa.tmp","w");
 for k=1:Nx+1
   ## On the plate
-  fprintf(fid,"%d %d %f\n",k,1,0.);
   fprintf(fid,"%d %d %f\n",k,2,0.);
+  ## Plate extends only from 0 to 1
+  xx = xnod(k,1);
+  if xx>=0 && xx <=1
+    fprintf(fid,"%d %d %f\n",k,1,0.);
+  endif
 endfor
 
 for k=2:Nx+1
--- test/plate/proc.m
+++ test/plate/proc.m
@@ -1,7 +1,25 @@
 source("data.m.tmp");
 
 U=aload("save.state.tmp");
+xnod=aload("plate.nod.tmp");
+icone=aload("plate.con.tmp");
+icone=[icone(:,[1 2 3]);
+	icone(:,[3 4 1])];
+nnod = length(xnod);
+y=xnod(1:Nx+1:nnod,2);
 u=reshape(U(:,1),Nx+1,Ny+1)';
 v=reshape(U(:,2),Nx+1,Ny+1)';
 p=reshape(U(:,3),Nx+1,Ny+1)';
 
+if 0
+  plot(y,u);
+  pause
+  plot(y,v);
+  pause
+  plot(y,p);
+  pause
+endif
+
+W = curl(xnod,icone,U(:,1:2));
+w = reshape(W,Nx+1,Ny+1)';
+plot(y,w);
