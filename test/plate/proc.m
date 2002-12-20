9f514db1d172e9c7305b75413c29bda7380b4cb9
mstorti <mstorti>
2002-12-20 03:14:13 +0000

    *** empty log message ***

--- test/plate/proc.m
+++ test/plate/proc.m
@@ -7,6 +7,7 @@ icone=[icone(:,[1 2 3]);
 	icone(:,[3 4 1])];
 nnod = length(xnod);
 y=xnod(1:Nx+1:nnod,2);
+x=xnod(1:Nx+1,1);
 u=reshape(U(:,1),Nx+1,Ny+1)';
 v=reshape(U(:,2),Nx+1,Ny+1)';
 p=reshape(U(:,3),Nx+1,Ny+1)';
