519c8f2b7871259c798bb799d4214ccba7690f8c
mstorti <mstorti>
2002-12-25 22:10:31 +0000

    *** empty log message ***

---
+++ test/nsgenload/constraint.m
@@ -0,0 +1,30 @@
+## This is a simple example showing how the constraints
+## are treated inside PETSc-FEM
+
+nedof = 10;
+tol = 1e-10;
+
+Q=eye(nedof);
+
+cons = [-1.   2     .1    4       0  9;
+	0.1   2     -1    4       0 10;
+	0.1   2     -1.1  4       0 10];
+
+for k=1:rows(cons)
+  consj = cons(k,:);
+  row = zeros(1,nedof);
+  for l=1:columns(consj)/2
+    coef = consj(1); edof = consj(2); consj(1:2)=[];
+    row = row + coef*Q(edof,:);
+  endfor
+  if (max(abs(row))<tol);  continue; endif
+  [bid,edof_elim] = max(abs(row));
+  coef = row(edof_elim);
+  row(edof_elim) = 0;
+  row = row/coef;
+  col = Q(:,edof_elim);
+  Q(:,edof_elim) = zeros(nedof,1);
+  Q = Q - col*row;
+endfor
+
+Q
