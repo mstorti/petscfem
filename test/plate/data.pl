78bc765068192a6a5eedc7fb3630d4423b71dc61
mstorti <mstorti>
2003-01-12 16:00:10 +0000

    *** empty log message ***

--- test/plate/data.pl
+++ test/plate/data.pl
@@ -1,6 +1,33 @@
-$Re = 1000;
-$Co = 2;
-$re_start = 0;
+$Re = 1000;			# Reynolds number
+$Co = 2;			# Courant number
+$re_start = 0;			# restart previous run? 
+
+$ev = sub { print $_[0]; };
+
+$omega=0.25;			# filter param (time scale)
+$xif=1;				# filter param (damping)
+/`/;
+$filter1 = <<EOM;
+# Filter
+n_coef 3
+a_coef &$ev(1./$omega**2+$xif/$omega+0.5) \
+       &$ev(-2/$omega**2)                 \
+       &$ev(1./$omega**2-$xif/$omega+0.5)
+b_coef &$ev(0.5+$xif/$omega) \
+       0.                   \
+       &$ev(0.5-$xif/$omega)
+EOM
+
+$omega=0.;			# filter param (time scale)
+$filter2 = <<EOM;
+# Filter
+n_coef 2
+a_coef 1 &$ev(1-$omega)
+b_coef $omega 0
+EOM
+/`/;
+
+$filter = $filter2;
 
 if (1) {   ## large mesh
     $Rint=1;
