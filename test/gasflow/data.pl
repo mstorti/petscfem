942616ca273b38295b772e199d96014b83ee42f1
mstorti <mstorti>
2003-01-16 03:26:53 +0000

    Working on NSC

--- test/gasflow/data.pl
+++ test/gasflow/data.pl
@@ -4,11 +4,11 @@ $slip = 1;
 ## GEOMETRY
 $L = 8;				# semi-length of nozzle
 $Rin = 1;			# radius at nozzle inlet/outle
-$Rn = 0.7;			# radius at throttle (minimum radius)
+$Rn = 0.5;			# radius at throttle (minimum radius)
 $nw = 3;			# throttle half-width. The shape is a `cosh' and
 				# at this distance from the center it recovers 90% of the
 				# inlet width.
-$Nr = 10;			# number of elements in radius direction
+$Nr = 5;			# number of elements in radius direction
 $Nx = 40;			# number of elements in x direction
 $r_ratio = 8;			# hr(axis)/hr(wall)
 $x_ratio = 16;			# hx(inlet) / hx(throttle)
@@ -26,7 +26,7 @@ $g_body  =  0;
 $gravity = -9.8;
 
 # BOUNDARY CONDITIONS
-$Mach_ref   = 0.3;
+$Mach_ref   = 0.5;
 $rho_ini    = 1.15;
 $p_ini      = 1.0e5; 
 $p_ref      = 1.0e5;
