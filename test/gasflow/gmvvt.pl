9e07beff65b07fa48882d3c8ae17f59cc43c4b16
mstorti <mstorti>
2003-01-22 14:44:29 +0000

    Runs vtube example with no cold outlet in geronimo.

--- test/gasflow/gmvvt.pl
+++ test/gasflow/gmvvt.pl
@@ -5,11 +5,11 @@ $nod = "$case.nod.tmp";	# nodes
 $con = "$case.con.tmp";	# connectivities
 $gmv = "$case.gmv.tmp";	# GMV input file
 if (1) {
-    $rslt = "$case.state.good"; # petscfem result file
+    $rslt = "$case.state.tmp"; # petscfem result file
 #    $rslt = "$case.ini.tmp"; # petscfem result file
     $rec = 0;
     $fields = 'nsc';
 }
 
 require "./data.pl";
-require "$ENV{'HOME'}/PETSC/petscfem/tools/gmv.pl";
+require "/u/mstorti/PETSC/petscfem.mario/tools/gmv.pl";
