d04731a53a010b6abd6e51c26f1a1a2e48e5543f
mstorti <mstorti>
2003-01-26 15:06:36 +0000

    *** empty log message ***

--- test/gasflow/gasflow.cpp
+++ test/gasflow/gasflow.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: gasflow.cpp,v 1.1 2003/01/25 21:25:49 mstorti Exp $
+//$Id: gasflow.cpp,v 1.2 2003/01/26 15:06:36 mstorti Exp $
 #define _GNU_SOURCE
 
 extern int MY_RANK,SIZE;
@@ -9,6 +9,7 @@ extern int MY_RANK,SIZE;
 #include <cassert>
 #include <cstdlib>
 #include <vector>
+#include <map>
 
 #include <src/vecmacros.h>
 #include <src/fstack.h>
@@ -19,19 +20,60 @@ extern int MY_RANK,SIZE;
 #include <src/hook.h>
 #include <src/dlhook.h>
 
+class gasflow_hook;
+typedef map<string,gasflow_hook *> gasflow_hook_table_t;
+gasflow_hook_table_t  gasflow_hook_table;
+
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 class gasflow_hook {
+private:
+  double flow_resistance,char_time;
+  string outlet_name;
 public:
+  double pressure;
   void init(Mesh &mesh,Dofmap &dofmap,
-	    TextHashTableFilter *options,const char *name) {
-    printf("gasflow_hook: init\n"); }
-  void time_step_pre(double time,int step) {
-    printf("gasflow_hook: time_step_pre\n"); }
+	    TextHashTableFilter *o,const char *name) { 
+    TextHashTable *options = (TextHashTable *)TextHashTable::find(name);
+    int ierr;
+    TGETOPTDEF_ND(options,double,flow_resistance,0.);
+    TGETOPTDEF_ND(options,double,char_time,0.);
+    assert(flow_resistance>0.);
+    assert(char_time>0.);
+    TGETOPTDEF(options,double,initial_pressure,0.);
+    assert(initial_pressure>0.);
+    pressure = initial_pressure;
+    outlet_name = string(name);
+    printf("registering gasflow_hook: %s\n",outlet_name.c_str());
+    assert(gasflow_hook_table.find(outlet_name)
+	   ==gasflow_hook_table.end());
+    gasflow_hook_table[outlet_name] = this;
+  }
+  void time_step_pre(double time,int step) { }
   void time_step_post(double time,int step,
-		      const vector<double> &gather_values) {
-    printf("gasflow_hook: time_step_post\n"); }
-  void close() {
-    printf("gasflow_hook: close\n"); }
+		      const vector<double> &gather_values) { 
+  }
+  void close() { }
 };
 
 DL_GENERIC_HOOK(gasflow_hook);
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+class flow_controller : public DLGenericTmpl {
+private:
+  gasflow_hook *my_gasflow_hook;
+public:
+  flow_controller() : my_gasflow_hook(NULL) {}
+  void init(TextHashTable *thash) { 
+    int ierr;
+    TGETOPTDEF_S(thash,string,outlet_name,<none>);
+    printf("Looking for hook %s\n",outlet_name.c_str());
+    gasflow_hook_table_t::iterator q = 
+      gasflow_hook_table.find(outlet_name);
+    assert(q!=gasflow_hook_table.end());
+    my_gasflow_hook = q->second;
+  }
+  double eval(double) { return my_gasflow_hook->pressure;  }
+};
+
+DEFINE_EXTENDED_AMPLITUDE_FUNCTION2(flow_controller);
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
