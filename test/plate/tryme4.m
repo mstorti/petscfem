13f9038f18a6015bd671f7f20300674c7f454c25
mstorti <mstorti>
2003-03-04 21:19:57 +0000

    *** empty log message ***

--- test/plate/tryme4.m
+++ test/plate/tryme4.m
@@ -1,36 +1,39 @@
-X=[0 0;
-   0 1;
-   0 2;
-   0 3;
-   1 0;
-   1 1;
-   1 2;
-   1 3;
-   2 0;
-   2 1;
-   2 2;
-   2 3];
+## Copyright (C) 2002 Mario A. Storti
+##
+## This file is part of Octave.
+##__INSERT_LICENSE__
+## $Id: tryme4.m,v 1.2 2003/03/04 21:19:57 mstorti Exp $
+
+## Tries to determine if from a set of M points we can find
+## the N<M points such that the stencil of these N points have
+## the best cloud quality. 
+
+## Author: Mario Storti
+## Keywords: cloud, least squares
+m=2;
+w = zhomo([0 1 0 1],2*m+1,2*m+1);
+[X,icone] = pfcm2fem(w);
+X = X*2*m;
 
 x=X(:,1); y=X(:,2);
 N=rows(X);
 
 A=[ones(N,1) x x.*x  y y.*x y.*x.*x y.*y y.*y.*x y.*y.*x.*x];
 A = leftscal(1./l2(A),A);
-nodosA = (1:N)';
+nA = (1:N)';
+indx = 2*(2*m+1)+m+1;
+AA=[];
+nAA=[];
 
-AA=A(1,:);
-nodosAA=1;
-A(1,:)=[];
-nodosA(1)=[];
-
-for k=1:N-1
-  H = A*AA';
-  [bid,indx] = min(max(H'));
-
+while 1
   AA=[AA;
       A(indx,:)];
   A(indx,:)=[];
-  nodosAA=[nodosAA;
-	   nodosA(indx)];
-  nodosA(indx)=[];
-endfor
+  nAA=[nAA;
+       nA(indx)];
+  nA(indx)=[];
+
+  if rows(A)==0; break; endif
+  H = A*AA';
+  [bid,indx] = min(max(H'));
+endwhile
