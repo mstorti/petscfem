213fa7ea0ea260e0841fe18a60f893bd80ce84d8
mstorti <mstorti>
2003-03-14 14:27:42 +0000

    *** empty log message ***

--- test/plate/project.m
+++ test/plate/project.m
@@ -1,3 +1,18 @@
+## Copyright (C) 2002 Mario A. Storti
+##
+## This file is part of Octave.
+##__INSERT_LICENSE__
+## $Id: project.m,v 1.2 2003/03/14 14:32:41 mstorti Exp $
+
+## usage: xpro = project (xi,x1,x2,fun);
+## Finds a point on the curve defined by `fun(x)=0' in the interval
+## delimited by `x1' and `x2', at a distance from both controlled by
+## `xi'. More precisely, if `x0' is the projection of the point on the
+## curve onto the secant from `x1' to `x2', then `xi=|x0-x1|/|x2-x1|'. 
+## In particular, for `xi=0' we obtain `x1' and for `xi=1' we obtain `x2'.
+
+## Author: Mario Storti
+## Keywords: KEYWORDS
 function xpro = project (xi,x1,x2,fun);
 
   xini = x1+xi*(x2-x1);
--- test/plate/projectd.m
+++ test/plate/projectd.m
@@ -1,3 +1,17 @@
+## Copyright (C) 2002 Mario A. Storti
+##
+## This file is part of Octave.
+##__INSERT_LICENSE__
+## $Id: projectd.m,v 1.2 2003/03/14 14:27:42 mstorti Exp $
+
+## usage: xpro = projectd (xini,nor,s,fun);
+## Projects a point `xini' along direccion `nor' on a curve defined
+## by `fun(x)=0'. The search is done by a secant method and `s' is
+## the initial trial distance, so that the to initial points are
+## `xini' and `xini+s*nor'. 
+
+## Author: Mario Storti
+## Keywords: KEYWORDS
 function xpro = projectd (xini,nor,s,fun);
 
   tol = 1e-10;
