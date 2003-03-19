abc7c76cde4353c1fb460cb3fce6dc705c5929bd
mstorti <mstorti>
2003-03-19 19:06:38 +0000

    Working on pseudo-ale

--- test/spiller/alehook.cpp
+++ test/spiller/alehook.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: alehook.cpp,v 1.1 2003/03/19 17:24:52 mstorti Exp $
+//$Id: alehook.cpp,v 1.2 2003/03/19 19:06:41 mstorti Exp $
 #define _GNU_SOURCE
 
 #include <cstdio>
@@ -41,21 +41,22 @@ void ale_hook::time_step_pre(double time,int step) {}
 void ale_hook::time_step_post(double time,int step,
 			      const vector<double> &gather_values) {
 
-  PetscPrintf(PETSC_COMM_WORLD,"ALEHOOK: begin time_step_post ....\n");
-  char * const argv[] = {"/usr/bin/make","spillway_mesh",NULL};
-  int stat = execv(argv[0],argv);
+  PetscPrintf(PETSC_COMM_WORLD,"ALEHOOK: begins time_step_post() ....\n");
+  // char * const argv[] = {"/usr/bin/make","spillway_mesh",NULL};
+  // int stat = execv(argv[0],argv);
+  int stat = system("/usr/bin/make spillway_mesh");
   if (stat==-1) {
-    printf("ALEHOOK: Couldn't \"execv\" octave process...\n");
+    printf("ALEHOOK: Couldn't launch octave process...\n");
     abort();
   }
-  FILE *fid = fopen("spiller.nod.tmp","w");
+  FILE *fid = fopen("spiller.nod.tmp","r");
   int nnod = mesh->nodedata->nnod;
   int nu = mesh->nodedata->nu;
   // double *nodedata = mesh->nodedata->nodedata;
 #define NODEDATA(j,k) VEC2(mesh->nodedata->nodedata,j,k,nu)
   for (int j=0; j<nnod; j++) {
     for (int k=0; k<ndim; k++) {
-      int nread = fscanf(fid,"%f",&NODEDATA(j,k));
+      int nread = fscanf(fid,"%lf",&NODEDATA(j,k));
       assert(nread==1);
     }
     double dummy;
@@ -65,7 +66,7 @@ void ale_hook::time_step_post(double time,int step,
     }
   }
   fclose(fid);
-  PetscPrintf(PETSC_COMM_WORLD,"ALEHOOK: ends time_step_post ....\n");
+  PetscPrintf(PETSC_COMM_WORLD,"ALEHOOK: ends time_step_post() ....\n");
 }
 
 void ale_hook::close() {}
--- test/spiller/spiller.m
+++ test/spiller/spiller.m
@@ -2,18 +2,19 @@
 ##
 ## This file is part of PETSc-FEM.
 ##__INSERT_LICENSE__
-## $Id: spiller.m,v 1.7 2003/03/17 21:54:37 mstorti Exp $
+## $Id: spiller.m,v 1.8 2003/03/19 19:06:41 mstorti Exp $
 
 ## Author: Mario Storti
 ## Keywords: spiller, mesh
 global spiller_data
 
 source("data.m.tmp");
+initia = str2num(getenv("initia"));
+printf("# initia: %d\n",initia);
 
 ## H = bottom height
 ## h = water height
 ## y = H + h position of free surface
-
 
 H2 = H1-C*L1^E;			# Height of spiller w.r.t. flat bottom
 h2 = y2-H2;			# water height at outlet
@@ -33,20 +34,64 @@ spiller_data.H2 = H2;
 ## and almost constant far from the spiller
 
 L=L1+L2;
-xfs = [0      H1;
-       0.1*L1 H1;
-       0.2*L1 H1;
-       L-0.7*L y2;
-       L-0.6*L y2;
-       L-0.4*L y2;
-       L-0.2*L y2;
-       L        y2];
 
-for j=1:3
-  xpro = projectd(xfs(j,:)',[0;1],1,"spiller_eq");
-  xpro(2) = xpro(2) + h1;
-  xfs(j,:) = xpro';
-endfor
+xfs = onedstr([1 0 1],10)*L1;
+xfs(length(xfs))=[];
+xfs = [xfs;
+       L1+onedstr([1 0 5],20)*L2];
+npc = rows(xfs);
+
+if initia
+  xfs1 = [0      H1;
+	  0.2*L1 H1;
+	  0.4*L1 H1;
+	  L-0.7*L y2;
+	  L-0.6*L y2;
+	  L-0.4*L y2;
+	  L-0.2*L y2;
+	  L        y2];
+  
+  for j=1:3
+    xpro = projectd(xfs1(j,:)',[0;1],1,"spiller_eq");
+    xpro(2) = xpro(2) + h1;
+    xfs1(j,:) = xpro';
+  endfor
+  xfs = [xfs spline(xfs1(:,1),xfs1(:,2),xfs)];
+else
+  fs = aload("spiller.nod_fs.tmp");
+  xfs_old = aload("spiller.xfs.tmp");
+  state = aload("spiller.state.tmp");
+  vfs = state(fs,1:2);
+  xnod = aload("spiller.nod.tmp");
+  xfs_fem = xnod(fs,:);
+  clear xnod
+  nfs = length(fs);
+  normal_e = xfs_fem(2:nfs,:)-xfs_fem(1:nfs-1,:);
+  le = l2(normal_e);
+  normal_e = leftscal(1./le,normal_e);
+  normal_e = [normal_e(:,2) -normal_e(:,1)];
+  normal = leftscal(le(1:nfs-2),normal_e(1:nfs-2,:))+leftscal(le(2:nfs-1),normal_e(2:nfs-1,:));
+  normal = leftscal(1./l2(normal),normal);
+  vn = sum((vfs(2:nfs-1,:).*normal)')';
+  vn = [0;vn;0];
+  vn_pc = spline(xfs_fem(:,1),vn,xfs);
+  xfs = xfs_old;
+  normal_pc = zeros(npc,2);
+  normal_pc(1,:) = [0 1];
+  normal_pc(npc,:) = [0 1];
+  epsil = 1e-6*L;
+  for k=2:npc-1
+    xx = xfs(k,1);
+    fp = spline(xfs(:,1),xfs(:,2),xx+epsil);
+    fm = spline(xfs(:,1),xfs(:,2),xx-epsil);
+    dfdx = (fp-fm)/(2*epsil);
+    normal_pc(k,:) = [-dfdx 1]/sqrt(1+dfdx^2);
+  endfor
+  xfs(2:npc-1,:) = xfs(2:npc-1,:) + fs_relax * Dt*leftscal(vn_pc(2:npc-1),normal_pc(2:npc-1,:));
+  save spiller.tmp xfs vn_pc 
+endif
+asave("spiller.xfs.tmp",xfs);
+
 spiller_data.xfs = xfs;
 
 x5 = projectd([L1;H2],[1;3],10,"fs_eq");
@@ -77,9 +122,11 @@ H = [1 4 Ny 1 4 1;
 [xnod,icone,mesh] = mesher(XNOD,ICONE,H,"spiller_mapbou");
 
 asave("spiller.nod.tmp",xnod);
+if ~initia; return; endif
 asave("spiller.con.tmp",icone);
 
 fs = mesher_bound(mesh,[6 5 4]);
+asave("spiller.nod_fs.tmp",fs);
 bottom = mesher_bound(mesh,[1 2 3]);
 inlet = mesher_bound(mesh,[4 1]);
 outlet = mesher_bound(mesh,[3 6]);
@@ -133,6 +180,6 @@ endfor
 fclose(fid);
 
 nnod = rows(xnod);
-uini = [1 0 0];
+uini = [uin 0 0];
 uini = uini(ones(nnod,1),:);
 asave("spiller.ini.tmp",uini);
