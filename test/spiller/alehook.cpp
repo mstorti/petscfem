db2d91858d72279d1227b65e2b25a17bca1c7856
mstorti <mstorti>
2003-03-19 21:27:44 +0000

    Running dam problem wth `ale_hook'

--- test/spiller/alehook.cpp
+++ test/spiller/alehook.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: alehook.cpp,v 1.2 2003/03/19 19:06:41 mstorti Exp $
+//$Id: alehook.cpp,v 1.3 2003/03/19 21:27:44 mstorti Exp $
 #define _GNU_SOURCE
 
 #include <cstdio>
@@ -41,31 +41,36 @@ void ale_hook::time_step_pre(double time,int step) {}
 void ale_hook::time_step_post(double time,int step,
 			      const vector<double> &gather_values) {
 
+  int ierr;
   PetscPrintf(PETSC_COMM_WORLD,"ALEHOOK: begins time_step_post() ....\n");
-  // char * const argv[] = {"/usr/bin/make","spillway_mesh",NULL};
-  // int stat = execv(argv[0],argv);
-  int stat = system("/usr/bin/make spillway_mesh");
-  if (stat==-1) {
-    printf("ALEHOOK: Couldn't launch octave process...\n");
-    abort();
-  }
-  FILE *fid = fopen("spiller.nod.tmp","r");
   int nnod = mesh->nodedata->nnod;
   int nu = mesh->nodedata->nu;
-  // double *nodedata = mesh->nodedata->nodedata;
+  if (!MY_RANK) {
+    // char * const argv[] = {"/usr/bin/make","spillway_mesh",NULL};
+    // int stat = execv(argv[0],argv);
+    int stat = system("/usr/bin/make spillway_mesh");
+    if (stat==-1) {
+      printf("ALEHOOK: Couldn't launch octave process...\n");
+      abort();
+    }
+    FILE *fid = fopen("spiller.nod.tmp","r");
+    // double *nodedata = mesh->nodedata->nodedata;
 #define NODEDATA(j,k) VEC2(mesh->nodedata->nodedata,j,k,nu)
-  for (int j=0; j<nnod; j++) {
-    for (int k=0; k<ndim; k++) {
-      int nread = fscanf(fid,"%lf",&NODEDATA(j,k));
-      assert(nread==1);
+    for (int j=0; j<nnod; j++) {
+      for (int k=0; k<ndim; k++) {
+	int nread = fscanf(fid,"%lf",&NODEDATA(j,k));
+	assert(nread==1);
+      }
+      double dummy;
+      for (int k=ndim; k<nu; k++) {
+	int nread = fscanf(fid,"%f",&dummy);
+	assert(nread==1);
+      }
     }
-    double dummy;
-    for (int k=ndim; k<nu; k++) {
-      int nread = fscanf(fid,"%f",&dummy);
-      assert(nread==1);
-    }
+    fclose(fid);
   }
-  fclose(fid);
+  ierr = MPI_Bcast(mesh->nodedata->nodedata, nnod*nu, MPI_DOUBLE, 0,PETSC_COMM_WORLD);
+  assert(!ierr); 
   PetscPrintf(PETSC_COMM_WORLD,"ALEHOOK: ends time_step_post() ....\n");
 }
 
