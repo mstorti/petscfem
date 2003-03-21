d2249471885458e3720ecbb68e154309b7d6c249
mstorti <mstorti>
2003-03-21 20:27:50 +0000

    *** empty log message ***

--- test/spillway/proc.m
+++ test/spillway/proc.m
@@ -1,10 +1,10 @@
 ###key proc.m
 
-xfs=aload("spillway.xfs.tmp");
-xfsh = [xfsh xfs(:,1)];
-yfsh = [yfsh xfs(:,2)];
-plot(xfsh,yfsh);
-pause
+#  xfs=aload("spillway.xfs.tmp");
+#  xfsh = [xfsh xfs(:,1)];
+#  yfsh = [yfsh xfs(:,2)];
+#  plot(xfsh,yfsh);
+#  pause
 
 system(["perl -ne 'print \"$1\n\" if m/control points: " \
 	"(.+)/;' nohup.log >spillway.fs_conv.tmp"]);
