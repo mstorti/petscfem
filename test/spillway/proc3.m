834088873fbe68de62aab7b1cea98952ffb6a4bf
mstorti <mstorti>
2003-04-01 12:40:41 +0000

    Will start to write the volume integrator.

--- test/spillway/proc3.m
+++ test/spillway/proc3.m
@@ -20,8 +20,15 @@ rem(rows(xfs),nfs)==0 || error("bad number of rows in .fsh file");
 yfs = reshape(xfs(:,2),nfs,rows(xfs)/nfs);
 xfs = reshape(xfs(:,1),nfs,rows(xfs)/nfs);
 
-axis([min(min(xfs)) max(max(xfs)) min(min(yfs)) max(max(yfs))])
+x=xfs(:,1);
+xbini = (Lx-L_bump)/2;		# start of bump
+xbend = xbini+L_bump;		# end of bump
+ybot = choose(x<xbini,0*x,x>xbend,0*x,4*t*(x-xbini).*(xbend-x)/L_bump^2);
+axis([min(min(xfs)) max(max(xfs)) min(min(ybot)) max(max(yfs))])
 for k=1:columns(yfs)
-  plot(xfs(:,k),yfs(:,k));
+  plot(xfs(:,k),yfs(:,k),x,ybot);
   pause(0.2);
 endfor
+
+## Average level history
+ylh = sum(yfs)'/rows(yfs);
