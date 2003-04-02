55bd16f46a7013cd272a30206aa75e0adbb86237
mstorti <mstorti>
2003-04-02 14:11:39 +0000

    Runs wave + seudo-ALE example (spillway.02-Apr-2003.tgz)

--- test/spillway/proc3.m
+++ test/spillway/proc3.m
@@ -1,10 +1,11 @@
 source("data.m.tmp");
 
-if 1
+if 0
 fs = (1:Nx+1)'*(Ny+1);
 vfs=[];
 k=0;
 while 1
+  k
   file = ["RUN1/wave.state_" int2str(k) ".tmp"];
   [info, err, msg] = stat (file);
   if err; break; endif
@@ -24,9 +25,9 @@ x=xfs(:,1);
 xbini = (Lx-L_bump)/2;		# start of bump
 xbend = xbini+L_bump;		# end of bump
 ybot = choose(x<xbini,0*x,x>xbend,0*x,4*t*(x-xbini).*(xbend-x)/L_bump^2);
-axis([min(min(xfs)) max(max(xfs)) min(min(ybot)) max(max(yfs))])
+axis([min(min(xfs)) max(max(xfs)) 0 max(max(yfs))])
 for k=1:columns(yfs)
-  plot(xfs(:,k),yfs(:,k),x,ybot);
+  plot(xfs(:,k),yfs(:,k));
   pause(0.2);
 endfor
 
