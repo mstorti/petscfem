6e95049c602b876639bafbbb7c03d9b69e1cf592
mstorti <mstorti>
2003-06-08 22:46:51 +0000

    *** empty log message ***

--- src/interpola.h
+++ src/interpola.h
@@ -1,12 +1,13 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: interpola.h,v 1.1 2003/06/08 21:42:01 mstorti Exp $
+// $Id: interpola.h,v 1.2 2003/06/08 22:51:03 mstorti Exp $
 #ifndef PETSCFEM_INTERPOLA_H
 #define PETSCFEM_INTERPOLA_H
 
-#include <src/pfobject.h>
+//#include <src/pfobject.h>
 #include <src/dvector.h>
 
+#if 0
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 /** This objects provides contranints that restrict the velocity
     field at a certain set of nodes to be irrotational.  */ 
@@ -24,24 +25,29 @@ public:
   /** Specific read function for this obkect.  */ 
   virtual void read(FileStack *fstack,Mesh *mesh,Dofmap *dofmap);
 };
+#endif
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-class interpolation_e : public NewElemset { 
+/// Interpolates the elemset values for a given set of points. 
+class interpolation : public NewElemset { 
+private:
+  /// The 
+  Nodedata *nodedata;
+  dvector<double> points;
+  int npoints;
 public:
-
-  interpolation_e() {}
-  ~interpolation_e() {}
+  interpolation() {}
+  ~interpolation() {}
   /// The ask function for the elemset. 
-
   int ask(const char *jobinfo,int &skip_elemset);
-
   virtual void initialize();
-
   void new_assemble(arg_data_list &arg_data_v,const Nodedata *nodedata,
 		    const Dofmap *dofmap,const char *jobinfo,
 		    const ElementList &elemlist,
 		    const TimeData *time_data);
-
+  void read(FileStack *fstack);
+  /// 
+  virtual char *jobinfo_stage();
 };
 
 #endif
