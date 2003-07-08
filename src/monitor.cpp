f798b94116d0e97f1597ab9f4a50d8c17b874b7d
mstorti <mstorti>
2003-07-08 12:49:38 +0000

    *** empty log message ***

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -1,39 +1,47 @@
 //__INSERT_LICENSE__
-// $Id: monitor.cpp,v 1.2 2003/07/08 12:34:31 mstorti Exp $
+// $Id: monitor.cpp,v 1.3 2003/07/08 12:49:38 mstorti Exp $
 
 #include <cstdio>
 #include <petsc.h>
 #include <src/monitor.h>
 #include <src/monitor2.h>
 
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void DefaultMonitor::init(MPI_Comm comm_a,TextHashTable *options_a) {
+  comm = comm_a;
+  options = options_a;
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void DefaultMonitor::start() { 
-  PetscPrintf(A->comm, "Hi, monitor start()...\n"); 
+  TGETOPTDEF(options,int,print_internal_loop_conv,1);
+  PetscPrintf(comm, "Hi, monitor start()...\n"); 
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void DefaultMonitor::step(int n,double rnorm) { 
-  if (A->print_internal_loop_conv) {
-    if (n==0) PetscPrintf(A->comm,
+  if (print_internal_loop_conv) {
+    if (n==0) PetscPrintf(comm,
 			  " Begin internal iterations "
 			  "--------------------------------------\n");
-    PetscPrintf(A->comm,
-		"iteration %d KSP Residual_norm = %14.12e \n",n,rnorm);
+    PetscPrintf(comm,"iteration %d KSP "
+		"Residual_norm = %14.12e \n",n,rnorm);
   }
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void DefaultMonitor::stop() { 
-  PetscPrintf(A->comm, "Hi, monitor close()...\n"); }
+  PetscPrintf(comm,"Hi, monitor close()...\n");
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 Monitor *Monitor::factory(TextHashTable *thash) {
-TGETOPTDEF_S(thash,string,monitor_type,default);
-Monitor * monitor=NULL;
-if (monitor_type=="default") {
-monitor = new DefaultMonitor;
-} else {
-monitor->init(thash);
+  TGETOPTDEF_S(thash,string,monitor_type,default);
+  Monitor * monitor=NULL;
+  if (monitor_type=="default") {
+    monitor = new DefaultMonitor;
+  } else
+  monitor->init(thash);
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
