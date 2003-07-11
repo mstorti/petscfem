60a37a604fa72fcd207f49070017edc9f5891c90
mstorti <mstorti>
2003-07-11 20:47:49 +0000

    *** empty log message ***

--- src/monitor.h
+++ src/monitor.h
@@ -1,6 +1,6 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: monitor.h,v 1.2 2003/07/08 22:35:52 mstorti Exp $
+// $Id: monitor.h,v 1.3 2003/07/11 20:47:49 mstorti Exp $
 #ifndef PETSCFEM_MONITOR_H
 #define PETSCFEM_MONITOR_H
 
@@ -9,7 +9,7 @@
 class Monitor {
  public:
   static Monitor *factory(TextHashTable *thash);
-  virtual void init(Mpi_Comm comm,TextHashTable *thash) {}
+  virtual void init(MPI_Comm comm,TextHashTable *thash) {}
   virtual void start() {}
   virtual void step(int n,double rnorm) {}
   virtual void stop() {}
