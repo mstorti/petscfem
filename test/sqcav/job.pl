0bd797b1b049d6d6bca12b332200be8e5646dd5d
mstorti <mstorti>
2003-08-23 22:14:22 +0000

    *** empty log message ***

--- test/sqcav/job.pl
+++ test/sqcav/job.pl
@@ -14,36 +14,43 @@ sub pushf {
 }
 
 # foreach  $N (100,200,400) {
-# foreach  $subpart (0,1,2,4,8,16) {
 # for ($nlay=0; $nlay<=5; $nlay++) {
-{{do { $N=100; $subpart=1; $nlay=2;
-    my @mess = ("nlay",$nlay,"subpart",$subpart,"N",$N,@solving,'total',$total);
-    my $mess = join(' ',@mess)."\n";
-    printf("start $mess");
-    system "make nlay=$nlay N=$N subpart=$subpart > job.out.tmp";
-    open OUT,"job.out.tmp";
-    my ($nlay,$befo);
-    my @solving = ();
-    while (<OUT>) {
-	$nlay = $1 if /use_interface_full_preco_nlay -> (\d*)/;
-	$befo = $1 if /Before solving linear system.*\[\S*\s*(\S*)\]/;
-	$iter = $1 if /iteration (\S*) /;
-	$maxits = $1 if /^\s*maxits .> (\d*)\s*$/;       
-	if (/After solving linear system.*\[\S*\s*(\S*)\]/) { 
-	    $after_sol = $1; 
-	    if ($iter>=$maxits) {
-		print "Seems that didn't converge iter $iter, maxits $maxits\n";
-	    }
-	    push @solving,"t",($after_sol-$befo),"iter",$iter; 
-	}
-	$after_comp = $1 if /After computing profile.*\[\S*\s*(\S*)\]/;
-    }
-    $total = $after_sol - $after_comp;
-    close OUT;
-    pushf "job.out.tmp",3;
-    open STAT,">>isp.stat";
-    print STAT $mess;
-    print "end $mess";
-    close STAT;
+do {{ $N=200; $nlay=1;
+       foreach  $subpart (1,2,4,8,16) {
+       my @mess = ("nlay",$nlay,"subpart",$subpart,"N",$N);
+       my $mess = join(' ',@mess)."\n";
+       printf("start $mess");
+       system "make nlay=$nlay N=$N subpart=$subpart > job.out.tmp";
+       open OUT,"job.out.tmp";
+       my ($nlay,$befo);
+       my @solving = ();
+       my $iter = 0;
+       my $maxmem = 0;
+       while (<OUT>) {
+	   $nlay = $1 if /use_interface_full_preco_nlay -> (\d*)/;
+	   $befo = $1 if /Before solving linear system.*\[\S*\s*(\S*)\]/;
+	   $iter = $1 if /iteration (\S*) /;
+	   $maxits = $1 if /^\s*maxits .> (\d*)\s*$/;       
+	   $maxmem = $1 if /After solving linear system.*\[Memory usage.*max (\S*),/;
+	   $after_sol = $1 if (/After solving linear system.*\[\S*\s*(\S*)\]/);
+	   if (/After solving linear system.*\[Memory usage.*max (\S*),/) { 
+	       if ($iter>=$maxits) {
+		   print "Seems that didn't converge iter $iter, maxits $maxits\n";
+	       }
+	       push @solving,"t",($after_sol-$befo),"iter",
+	       $iter,"maxmem",$maxmem; 
+	   }
+	   $after_comp = $1 if /After computing profile.*\[\S*\s*(\S*)\]/;
+       }
+       $total = $after_sol - $after_comp;
+       close OUT;
+       pushf "job.out.tmp",3;
+
+       push @mess,@solving,'total',$total; 
+       $mess = join(' ',@mess)."\n";
+       open STAT,">>isp.stat";
+       print STAT $mess;
+       print "end $mess";
+       close STAT;
 }}}
 
