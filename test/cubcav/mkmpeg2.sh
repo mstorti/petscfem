3ea76556e7a8b0321f60f591c9aa67abf3ff8729
mstorti <mstorti>
2003-09-20 13:17:31 +0000

    *** empty log message ***

--- test/cubcav/mkmpeg2.sh
+++ test/cubcav/mkmpeg2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$Id: mkmpeg2.sh,v 1.6 2003/09/11 03:03:00 mstorti Exp $
+#$Id: mkmpeg2.sh,v 1.7 2003/09/20 13:17:31 mstorti Exp $
 
 nstep=360
 
@@ -10,15 +10,21 @@ yuv_cat() {
     done
 }
 
-yuv_cat YUV-z0
-yuv_cat YUV-z0.25
-yuv_cat YUV-z0.5
-yuv_cat YUV-y0.05
-yuv_cat YUV-x0.95
-yuv_cat YUV-x0.05
-yuv_cat YUV-box
+# yuv_cat YUV-z0
+# yuv_cat YUV-z0.25
+# yuv_cat YUV-z0.5
+# yuv_cat YUV-y0.05
+# yuv_cat YUV-x0.95
+# yuv_cat YUV-x0.05
+# yuv_cat YUV-box
 
-nstep=600
-yuv_cat YUV-iso
-yuv_cat YUV-iso2
-yuv_cat YUV-iso3
+# nstep=600
+# yuv_cat YUV-iso
+# yuv_cat YUV-iso2
+# yuv_cat YUV-iso3
+
+nstep=720
+yuv_cat YUV-Re1e5-iso
+yuv_cat YUV-Re1e5-z0.01
+yuv_cat YUV-Re1e5-z0.25
+yuv_cat YUV-Re1e5-z05
