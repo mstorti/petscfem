065ce27813eecc6b996d19b0b61e0e795fc60380
mstorti <mstorti>
2003-11-12 13:03:00 +0000

    *** empty log message ***

--- test/gaschem/proc2.m
+++ test/gaschem/proc2.m
@@ -1,9 +1,7 @@
 ###key proc.m
 ##__INSERT_LICENSE__
-## $Id: proc2.m,v 1.3 2003/11/12 12:47:02 mstorti Exp $
+## $Id: proc2.m,v 1.4 2003/11/12 13:03:00 mstorti Exp $
 source("data.m.tmp");
-
-
 
 U=aload_any("pool.state_%d.tmp",0);
 
@@ -12,3 +10,16 @@ CO = reshape(U(:,2),Nx+1,Ny+1);
 CN = reshape(U(:,3),Nx+1,Ny+1);
 COd = reshape(U(:,4),Nx+1,Ny+1);
 CNd = reshape(U(:,5),Nx+1,Ny+1);
+
+if !exist("xO_in")
+  disp("doesn't exist xO_in, set to 0.2\n");
+  xO_in = 0.2;
+endif
+
+## Compute saturation
+xnod = aload("pool.nod.tmp");
+y = xnod(:,2);
+pg = patm+rho_liq*gravity*(max(y)-y);
+pg = reshape(pg,Nx+1,Ny+1);
+
+satO = COd./(KO*pg*xO_in);
