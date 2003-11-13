380a71f05cc2960a49d40502c88ffa4878cfb91d
mstorti <mstorti>
2003-11-13 13:57:59 +0000

    *** empty log message ***

--- test/gaschem/job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for k in `seq 0 0`
-do  doit.pl $k
-    echo before make run
-    /usr/bin/make run > nohup.log
-    cat proctable >> nohup.log
-    mv nohup.log nohup.log.no-proc$k
-done
