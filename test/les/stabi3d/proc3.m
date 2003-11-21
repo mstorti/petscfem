88b273376130e6f443d462a4544434ae79db3dfa
mstorti <mstorti>
2003-11-21 21:56:16 +0000

    *** empty log message ***

--- test/les/stabi3d/proc3.m
+++ test/les/stabi3d/proc3.m
@@ -1,15 +1,34 @@
 ## proc2
+
+dir = "/u/jdelia/PETSC/CHANNEL3D";
+source([dir "/data.m.tmp"]);
+if 1,
+
+  arch = [dir "/channel3d.nod.tmp"];
+  x = load (arch) ;
+
+  arch = [dir "/channel3d.some.tmp"];
+  some = aload (arch);
+  iy = some (1:Ny+1);
+  iz = some;
+  iz (1:Ny+1)=[];
+
+  y = x (iy,2);
+  z = x (iz,3);
+  rem (Ny,2) == 0 || warning("Sould use Ny even!!");
+  iz = [round(Ny/2)+1 Ny+1+(1:Nz)]';
+  clear x
+
+endif
+
 nu = 0.000133;
 
-start = 200;
-last = columns(u);
-nsteps = (last-start+1);
-
+if 0
 nplane = (Nx+1)*(Nz+1);
 nnod = nplane * (Ny+1);
 uav= [];
 uavh = zeros(Ny+1,1);
-for k=0:17
+for k=0:30
   k
   u = aload([dir "/RUN/channel3d.state." int2str(k) ".tmp"]);
   for iy = 1:Ny+1;
@@ -18,3 +37,4 @@ for k=0:17
   uav = [uav uavh];
 endfor
 
+endif
