aaeeb4b0d50803228523058006ffd0e9f8e4eb64
mstorti <mstorti>
2003-12-03 22:14:07 +0000

    *** empty log message ***

--- src/generror.h
+++ src/generror.h
@@ -1,6 +1,6 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: generror.h,v 1.2 2003/12/03 11:52:20 mstorti Exp $
+// $Id: generror.h,v 1.3 2003/12/03 22:14:08 mstorti Exp $
 #ifndef PETSCFEM_GENERROR_H
 #define PETSCFEM_GENERROR_H
 
@@ -9,8 +9,8 @@ class GenericError : public string {
 public:
   GenericError() : string("") { }
   // GenericError(char *s) : string(s) { }
-  GenericError(char *s,va_list l);
-  GenericError(char *s,...);
+  GenericError(char *s,va_list l) {}
+  GenericError(char *s,...) {}
 };
 
 #endif
