865795cae86327776aa26adb31faaf600d5c1299
mstorti <mstorti>
2004-01-09 14:40:39 +0000

    *** empty log message ***

--- test/distmap/syncbuff.cpp
+++ test/distmap/syncbuff.cpp
@@ -1,21 +1,22 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: syncbuff.cpp,v 1.2 2004/01/09 03:08:51 mstorti Exp $
-#include <vector>
+// $Id: syncbuff.cpp,v 1.3 2004/01/09 14:40:39 mstorti Exp $
+#include <list>
 #include <iostream>
 #include <src/distcont.h>
 #include <src/distcont2.h>
+#include <src/debug.h>
 #include <algorithm>
 #include <cassert>
 
-extern int SIZE, MY_RANK;
+int SIZE, MY_RANK;
 
 using namespace std;
 
 class PO  {
 public:
   int k;
-  PO(int j) : k(j) {}
+  PO(int j=0) : k(j) {}
   void print() { cout << this->k << endl ; }
 };
 
@@ -30,34 +31,67 @@ TrivialPartitioner::processor(const PO &po,int &nproc,int *plist) {
   plist[0] = 0;
 }
 
-typedef DistCont<vector<PO>,PO,TrivialPartitioner> Container;
-
-// int Container::size_of_pack(const PO &p) const {
-//   return sizeof(int)+sizeof(double);
-// }
-
 template<typename T>
 class SyncBuffer :   
-  public DistCont<vector<T>,T,
-		  TrivialPartitioner,iter_mode=random_iter_mode> 
-  {
-    typedef typename DistCont<vector<T>,T,
-    TrivialPartitioner,iter_mode=random_iter_mode> cont;
-    TrivialPartitioner part;
-    public:
-    SyncBuffer() : cont(&part) {}
-    void sort() { /* not implemented yet */; assert(0); }
-    void print() { 
-      scatter();
-      if (!MY_RANK) {
-	typename vector<T>::iterator q;
-	for (q=begin(); q!=end(); q++) { q->print(); }
-      }
+  public DistCont<list<T>,T,TrivialPartitioner>   {
+  //    typedef typename DistCont<list<T>,T,TrivialPartitioner> cont;
+  TrivialPartitioner part;
+public:
+  SyncBuffer() : 
+    DistCont<list<T>,T,TrivialPartitioner>(&part) {}
+  void sort() { /* not implemented yet */; assert(0); }
+  void print() { 
+    scatter();
+    if (!MY_RANK) {
+      typename list<T>::iterator q;
+      for (q=begin(); q!=end(); q++) { q->print(); }
     }
-  };
+  }
+};
 
-int main() {
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+int DistCont<list<PO>,PO,TrivialPartitioner>
+::size_of_pack(const PO &po) const {
+  return sizeof(PO);
+}
+
+// Copy the int and double to the buffer. Update pointer *buff
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void DistCont<list<PO>,PO,TrivialPartitioner>
+::pack(const PO &po,char *&buff) const {
+  memcpy(buff,&po.k,sizeof(int));
+  buff += sizeof(int);
+}
+  
+  //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void DistCont<list<PO>,PO,TrivialPartitioner>
+::unpack(PO &po,const char *& buff) {
+  memcpy(&po.k,buff,sizeof(int));
+  buff += sizeof(int);
+}
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+void DistCont<list<PO>,PO,TrivialPartitioner>
+::combine(const PO &po) { push_back(po); };
+ 
+int main(int argc,char **argv) {
+
+  PetscInitialize(&argc,&argv,NULL,NULL);
+  MPI_Comm_size (MPI_COMM_WORLD, &SIZE);
+  MPI_Comm_rank (MPI_COMM_WORLD, &MY_RANK);
+
+  Debug debug;
+  debug.init();
+  debug.activate();
   SyncBuffer<PO> sb;
-  for (int j=0; j<10; j++) sb.push_back(j);
+  if (!MY_RANK) {
+    for (int j=0; j<5; j++) sb.push_back(j);
+  } else {
+    for (int j=5; j<10; j++) sb.push_back(j);
+  }
+  debug.trace("antes de sb.print()");
   sb.print();
+  PetscFinalize();
+  exit(0);
+
 }
