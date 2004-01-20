3954dc25d5d705c6a6d4bfb41f293e50ab15c951
mstorti <mstorti>
2004-01-20 23:38:53 +0000

    *** empty log message ***

--- test/pfmat/pfmat3.cpp
+++ test/pfmat/pfmat3.cpp
@@ -1,39 +1,83 @@
 /*__INSERT_LICENSE__*/
-// $Id: pfmat3.cpp,v 1.2 2004/01/20 22:44:11 mstorti Exp $
+// $Id: pfmat3.cpp,v 1.3 2004/01/20 23:38:53 mstorti Exp $
 
-// Tests for the `PFMat' class
-
-#if 1
-#include <ext/hash_map>
+#include <src/dvector.h>
+#include <src/dvector2.h>
+#include <cassert>
 #include <cstdio>
 
-using namespace __gnu_cxx;
-using namespace std;
-
-int main(int argc,char **args) {
-  hash<int> H;
-  for (int j=0; j<1000; j++) {
-    int h = H(j);
-    printf("%d -> %d\n",j,h);
+class pfmat2 {
+private:
+  class cell {
+    int i,j;
+    double val;
+    friend class pfmat2;
+    int next;
+  };
+  dvector<cell> cells;
+  dvector<int> headers;
+  int N;			// number of buckets
+  int M;			// size of cell vector
+  int top;			// first free cell
+  int hash_fun(int i,int j) {
+    // return ((i+j)*1324) % N;
+    return i % N;
+  };
+  int free() {
+    int c = top;
+    assert(c>=0);
+    top = cells.ref(c).next;
+    return c;
   }
-}
-
-#else
-#include <glib.h>
-#include <cstdio>
-#include <cstring>
-
-int my_int_hash_fun(int j) {
-  char a[5];
-  memcpy(a,&j,sizeof(int));
-  a[4]='\0';
-  return g_str_hash(a);
-}
-
-int main(int argc,char **args) {
-  for (int j=0; j<1000; j++) {
-    int h = my_int_hash_fun(j);
-    printf("%d -> %d\n",j,h);
+public:
+  pfmat2(int N_a,int M_a=0) : N(N_a), M(M_a) {
+    assert(N>0);
+    if (!M) M=N;
+    cells.mono(N);
+    headers.mono(N);
+    top = 0;
+    for (int j=0; j<M-1; j++) 
+      cells.ref(j).next = j+1;
+    cells.ref(M-1).next = -1;
+    for (int j=0; j<M; j++) 
+      headers.ref(j) = -1;
   }
+  void add(int i,int j, double val) {
+    int bucket = hash_fun(i,j);
+    int *cursor = &headers.ref(bucket);
+    while (*cursor>=0) {
+      cell & c = cells.ref(*cursor);
+      if (c.i==i && c.j==j) break;
+      cursor = &c.next;
+    }
+    if (*cursor<0) { 
+      *cursor = free();
+      headers.ref(bucket) = *cursor;
+      cell & c = cells.ref(*cursor);
+      c.i = i;
+      c.j = j;
+      c.val = val;
+      c.next = -1;
+    } else {
+      cell & c = cells.ref(*cursor);
+      c.val += val;
+    }
+  }
+  void print() {
+    for (int j=0; j<N; j++) {
+      int cursor = headers.ref(j);
+      while (cursor>=0) {
+	cell & c = cells.ref(cursor);
+	printf("%d, %d: %f\n",c.i,c.j,c.val);
+	cursor = c.next;
+      }
+    }
+  }
+};
+
+int main() {
+  pfmat2 A(1024);
+  for (int j=0; j<10; j++) 
+    A.add(j,j,double(2*j));
+  A.print();
 }
-#endif
