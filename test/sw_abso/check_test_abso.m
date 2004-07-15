e707a371784d0b2111151b339bd6612713a92360
rodrigop <rodrigop>
2004-07-15 19:08:43 +0000

    *** empty log message ***

--- test/sw_abso/check_test_abso.m
+++ test/sw_abso/check_test_abso.m
@@ -6,7 +6,7 @@ u=aload("canal1d.rslt.tmp");
 u=reshape(u(:,2),201,51);
 
 erro = merr(u(:,10:10:30)-uref);
-tol=1e-10;
+tol=1e-9;
 
 printf("Canal1d/sw_abso. Comparison wrt. \
 reference sol OK? > %d (erro %g, tol %g)\n",erro<tol,erro,tol);
