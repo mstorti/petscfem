278838486a0cb8f0c231410b5d1a0c91b070383d
mstorti <mstorti>
2004-11-11 18:23:08 +0000

    *** empty log message ***

--- test/cubcav/check_plain.m
+++ test/cubcav/check_plain.m
@@ -1,27 +1,23 @@
-#$Id: check_plain.m,v 1.3 2003/12/08 13:34:05 mstorti Exp $
+#$Id: check_plain.m,v 1.4 2004/11/11 18:23:08 mstorti Exp $
 
 u0 = aload("cubcav.state.plain_bupl0.tmp");
 u1 = aload("cubcav.state.plain_bupl1.tmp");
+u2 = aload("cubcav.state.plain_bupl2.tmp");
+
+uref = aload("cubcav.sol");
+indx = uref(:,1);
+uref = uref(:,2:5);
 
 erro = merr(u1-u0);
 tol = 1e-10;
 printf("Cubcav/Block uploading error OK? > %d (error %g, tol %g)\n",
        erro<tol,erro,tol);
 
-if 0
-  ## Creates reference state file
-  ## Run and save inf `cubcav.sol'
-  indx=sort(ceil(rand(30,1)*rows(u)));
-  for k=indx'; 
-    printf("%5d %20.12e %20.12e %20.12e %20.12e\n",k,u0(k,:));
-  endfor
-  return
-endif
+erro2 = merr(u2-u0);
+printf("Cubcav/Block uploading 2 error OK? > %d (error %g, tol %g)\n",
+       erro2<tol,erro2,tol);
 
-uref = aload("cubcav.sol");
-indx = uref(:,1);
-uref = uref(:,2:5);
-
-erro = merr(u0(indx,:)-uref);
+erro3 = merr(u0(indx,:)-uref);
 printf("Cubcav/error wrt.reference solution OK? > %d (error %g, tol %g)\n",
-       erro<tol,erro,tol);
+       erro3<tol,erro3,tol);
+
