de768d69ce017c08f4dd4173533d51b2b7d3154e
mstorti <mstorti>
2004-11-17 20:48:51 +0000

    *** empty log message ***

--- tools/femref/femref.h
+++ tools/femref/femref.h
@@ -1,12 +1,95 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: femref.h,v 1.1 2004/11/16 02:59:56 mstorti Exp $
+// $Id: femref.h,v 1.2 2004/11/17 20:48:51 mstorti Exp $
 #ifndef PETSCFEM_FEMREF_H
 #define PETSCFEM_FEMREF_H
 
-#include <src/dvector.h>
-#include <src/dvector2.h>
+#include <src/generror.h>
 
+typedef unsigned int Node;
+
+class GeomObject {
+  virtual bool equal(GeomObject &)=0;
+  static GeomObject *factory(int nnod,const int *nodes,
+		      const char *type);
+};
+
+class OrientedEdge : public GeomObject {
+private:
+  int n1,n2;
+  OrientedEdge(int n,const int *nodes) {
+    if (n!=2) throw GenericError("OrientedEdge must have 2 nodes");
+    n1 = nodes[0];
+    n2 = nodes[1];
+  };
+public:
+  bool equal(GeomObject &go) {
+    OrientedEdge *edge2 = dynamic_cast<OrientedEdge *>(&go);
+    if (!edge2) return false;
+    return (n1==edge.n1 && n2==edge.n2);
+  };
+};
+
+class Edge : public GeomObject {
+private:
+  int n1,n2;
+  Edge(int n,const int *nodes) {
+    if (n!=2) throw GenericError("Edge must have 2 nodes");
+    n1 = nodes[0];
+    n2 = nodes[1];
+  };
+public:
+  bool equal(GeomObject &go) {
+    Edge *edge2 = dynamic_cast<Edge *>(&go);
+    if (!edge2) return false;
+    return ((n1==edge.n1 && n2==edge.n2) ||
+	    (n1==edge.n2 && n2==edge.n1));
+  };
+};
+
+class OrientedTri : public GeomObject {
+private:
+  int n1,n2,n3;
+  int nodes[3];
+  OrientedTri(int n,const int *nodes_a) {
+    if (n!=3) throw GenericError("OrientedTri must have 3 nodes");
+    for (int j=0; j<3; j++) nodes[j] = nodes_a[j];
+  };
+public:
+  bool equal(GeomObject &go) {
+    OrientedTri *tri2 = dynamic_cast<OrientedTri *>(&go);
+    if (!tri2) return false;
+    n0 = tri2->nodes[0];
+    for (int j=0; j<3; j++) 
+      if (nodes[j] = n0) break;
+    if (j==3) return false;
+    for (k=1; k<3; k++)
+      if (tri2->nodes[k] != nodes[(j+k)%3]) return false;
+    return true;
+  };
+};
+
+class OrientedTri : public GeomObject {
+private:
+  int n1,n2,n3;
+  int nodes[3];
+  OrientedTri(int n,const int *nodes_a) {
+    if (n!=3) throw GenericError("OrientedTri must have 3 nodes");
+    for (int j=0; j<3; j++) nodes[j] = nodes_a[j];
+  };
+public:
+  bool equal(GeomObject &go) {
+    OrientedTri *tri2 = dynamic_cast<OrientedTri *>(&go);
+    if (!tri2) return false;
+    n0 = tri2->nodes[0];
+    for (int j=0; j<3; j++) 
+      if (nodes[j] = n0) break;
+    if (j==3) return false;
+    for (k=1; k<3; k++)
+      if (tri2->nodes[k] != nodes[(j+k)%3]) return false;
+    return true;
+  };
+};
 
 #if 0
 class ObjectTable
@@ -38,6 +121,7 @@ class NodeSet {
 };
 #endif
 
+#if 0
 class Mesh {
  public:
   class Object {};
@@ -46,5 +130,6 @@ class Mesh {
   dvector<double> xnod();
   dvector<int> icone();
 };
+#endif
 
 #endif
