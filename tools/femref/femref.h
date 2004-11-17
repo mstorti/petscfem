09293798cb7530a555c34b5032d4e3153e46af31
mstorti <mstorti>
2004-11-17 23:53:47 +0000

    *** empty log message ***

--- tools/femref/femref.h
+++ tools/femref/femref.h
@@ -1,6 +1,6 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: femref.h,v 1.3 2004/11/17 23:45:41 mstorti Exp $
+// $Id: femref.h,v 1.5 2004/11/17 23:53:47 mstorti Exp $
 #ifndef PETSCFEM_FEMREF_H
 #define PETSCFEM_FEMREF_H
 
@@ -109,45 +109,12 @@ public:
   };
 };
 
-#if 0
-class ObjectTable
-
-class NodeSet {
- public:
-  /// Ctor 
-  NodeSet(int ndim_a) : ndim(ndim_a), max(0) { }
-  /** A node is identified by an index in `node_list'. 
-      All indices between in range #[0,max-1)# that are not in 
-      #invalid#, are nodes. **/
-  class NodeHandle : public int { };
-  /// Adds a new node with those specific coordinates
-  NodeHandle add(double *coords);
-  /// Remove that nodes
-  void remove(NodeHandle node);
-  
- private:
-  /// The dimension of the space
-  int ndim;
-  /* Stores the coordinates.  A node is identified by an index in
-     `node_list'.  All indices in range #[0,max-1)# that are not in
-     #invalid#, are nodes. **/
-  dvector<double> coords;
-  /// Max node added so far
-  int max;
-  /// These are `holes' in the container
-  set<int> invalid;
-};
-#endif
-
-#if 0
 class Mesh {
- public:
-  class Object {};
-  
- private:
-  dvector<double> xnod();
-  dvector<int> icone();
+public:
+  class iterator {
+    
+  };
+  virtual void get_adacency(GeomObject &g1,list<GeomObject *>
 };
-#endif
 
 #endif
