effc1f8475a710eff6c02e4f6cc0181cb4a97a1a
mstorti <mstorti>
2004-11-20 21:16:27 +0000

    Working on `femref'.

--- tools/femref/femref.cpp
+++ tools/femref/femref.cpp
@@ -1,11 +1,58 @@
 //__INSERT_LICENSE__
-// $Id: femref.cpp,v 1.4 2004/11/18 23:34:05 mstorti Exp $
+// $Id: femref.cpp,v 1.5 2004/11/20 21:16:27 mstorti Exp $
 
 #include <string>
 
 using namespace std;
 
 #include "./femref.h"
+
+GeomObject::GeomObject() : gobj(NULL) { }
+
+void GeomObject::init(int sz,const int *nodes,
+		      GeomObject::Type t) { 
+  clear();
+#define CTOR(CLASS) \
+else if(t==CLASS##T) gobj = new CLASS(sz,nodes)
+  if (t==NULL_TYPE) assert(0);
+  CTOR(OrientedEdge);
+  CTOR(Edge);
+  CTOR(OrientedTri);
+  CTOR(Tri);
+  else assert(0);
+}
+
+bool GeomObject::equal(GeomObject &go) { 
+  return gobj->equal(*go.gobj); 
+}
+
+int GeomObject::dim() { 
+  return gobj->dim(); 
+}
+
+int GeomObject::csum() { 
+  return gobj->csum(); 
+}
+
+int GeomObject::size(GeomObject::Type t) {
+  return gobj->size(t); 
+}
+
+int GeomObject::size() {
+  return gobj->size(); 
+}
+
+int GeomObject::Type GeomObject::type() {
+  return gobj->type(); 
+}
+
+void GeomObject::clear() { 
+  if (gobj) delete gobj; gobj=NULL; 
+}
+
+GeomObject::~GeomObject() { 
+  clear(); 
+}
 
 int main() { 
   Mesh mesh(2,3);
