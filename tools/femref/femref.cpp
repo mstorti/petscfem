95bda517dcb756b8d713ff6a08286151ac640547
mstorti <mstorti>
2004-11-21 23:43:09 +0000

    Working on `femref'.

--- tools/femref/femref.cpp
+++ tools/femref/femref.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-// $Id: femref.cpp,v 1.6 2004/11/21 20:21:53 mstorti Exp $
+// $Id: femref.cpp,v 1.7 2004/11/21 23:43:09 mstorti Exp $
 
 #include <string>
 #include <limits.h>
@@ -8,85 +8,160 @@ using namespace std;
 
 #include "./femref.h"
 
-GeomObject::GeomObject() : gobj(NULL) { }
+GeomObject::GeomObject(Type t,const int *nodes_a) 
+  :  canonical(0), go_template(NULL) {
 
-void GeomObject::init(int sz,const int *nodes,
-		      GeomObject::Type t) { 
-  clear();
-#define CTOR(CLASS) \
-else if(t==CLASS##T) gobj = new CLASS(sz,nodes)
+#define SET_TEMPL(CLASS) \
+else if(t==CLASS##T) go_template = &CLASS##Template
+
   if (t==NULL_TYPE) assert(0);
-  CTOR(OrientedEdge);
-  CTOR(Edge);
-  CTOR(OrientedTri);
-  CTOR(Tri);
+  // SET_TEMPL(OrientedEdge);
+  SET_TEMPL(Edge);
+  // SET_TEMPL(OrientedTri);
+  SET_TEMPL(Tri);
+  SET_TEMPL(OrientedTetra);
   else assert(0);
+
+  int sz = size();
+  nodes.mono(sz);
+  cs = 0;
+  for (int j=0; j<sz; j++) {
+    int node = nodes_a[j];
+    cs += node;
+    nodes.ref(j) = node;
+  }
+
 }
 
-bool GeomObject::equal(GeomObject &go) { 
-  return gobj->equal(*go.gobj); 
+void GeomObject::make_canonical() {
+  // iterate through all possible perms. and
+  // remain with lowest (in lexicographical order)
+  // numbering
+  int np = nperms();
+  int sz = size();
+  dvector<int> aux;
+  aux.mono(sz);
+  int *cmin = &aux.ref(0);
+  int *nodesp = &nodes.ref(0);
+#define DBG
+#ifdef DBG
+  printf("entered: ");
+  for (int kk=0; kk<sz; kk++) 
+    printf(" %d",nodesp[kk]);
+  printf("\n");
+#endif
+  for (int kk=0; kk<sz; kk++) 
+    cmin[kk] = nodesp[kk];
+  for (int j=0; j<np; j++) {
+    // Go constructing the permutation. 
+    // If it is higher skip. Else replain current min
+    const int *permv = perm(j);
+#if 0 && defined DBG
+    printf("perm %d: ",j);
+#endif
+    for (int k=0; k<sz; k++) {
+      int perm_node = nodesp[permv[k]];
+      int node = cmin[k];
+      if (perm_node < node) {
+	// Permuted numeration is lower (lexicographically)
+	for (int kk=0; kk<sz; kk++) 
+	  cmin[kk] = nodesp[permv[kk]];
+	break;
+      } else if (perm_node>node) 
+	// Permuted numeration is higher (lexicographically)
+	// -> continue with next permutation
+	break;
+    }
+#if 0 && defined DBG
+    for (int kk=0; kk<sz; kk++) 
+      printf(" %d",cmin[kk]);
+    printf("\n");
+#endif
+  }
+  for (int kk=0; kk<sz; kk++) 
+    nodesp[kk] = cmin[kk];
+#if 1
+  printf("canonical form: ");
+  for (int kk=0; kk<sz; kk++) 
+    printf(" %d",nodesp[kk]);
+  printf("\n");
+#endif
 }
 
-int GeomObject::dim() { 
-  return gobj->dim(); 
+bool GeomObject::equal(GeomObject &go) {
+  if (type() != go.type()) return false;
+  if (csum() != go.csum()) return false;
+  make_canonical();
+  go.make_canonical();
+  
 }
 
-int GeomObject::csum() { 
-  return gobj->csum(); 
-}
+int GeomObject::NULL_NODE = INT_MAX;
 
-int GeomObject::size(GeomObject::Type t) {
-  return gobj->size(t); 
-}
-
-int GeomObject::size() {
-  return gobj->size(); 
-}
-
-
-int GeomObject::nperms() { 
-  return gobj->nperms();
-}
-
-const int* GeomObject::perm(int perm_indx) {
-  return gobj->perm(perm_indx);
-}
-
-GeomObject::Type GeomObject::type() {
-  return gobj->type(); 
-}
-
-void GeomObject::clear() { 
-  if (gobj) delete gobj; gobj=NULL; 
-}
-GeomObject::~GeomObject() { 
-  clear(); 
-}
-
-GeomObject Mesh::id2obj(GeomObjectId id) {
-  //  GeomObject 
-}
-
-int Edge::perms_v[2] = {1,0};
-
-int GeomObjectSeq::NULL_NODE = INT_MAX;
-
-GeomObjectSeq::GOTemplate
-::GOTemplate(int sz,GeomObject::Type t,
+GeomObject::Template
+::Template(int sz,GeomObject::Type t,
 	     int dim_a,int nperms_a,const int *perms) 
   : size_m(sz), type(t), dim_m(dim_a), nperms_m(nperms_a) {
   perms_v.resize(nperms_m*size_m);
-  int *q = perms;
+  const int *q = perms;
   for (int j=0; j<nperms_m*size_m; j++)
     perms_v.e(j) = *q++;
+  assert(*q==GeomObject::NULL_NODE);
   perms_v.reshape(2,nperms_m,size_m);
 }
 
-static GeomObjectSeq::GOTemplate 
-EdgeTemplate(2,GeomObject::EdgeT,1,1,{1,0}) { }
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+static int EdgeTemplatePerm_v[] = {1,0,GeomObject::NULL_NODE};
+GeomObject::Template 
+GeomObject::EdgeTemplate(2,GeomObject::EdgeT,
+			 1,1,EdgeTemplatePerm_v);
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+static int TriTemplatePerm_v[] = {1,2,0,
+				  2,0,1,
+				  0,2,1,
+				  2,1,0,
+				  1,0,2,GeomObject::NULL_NODE};
+GeomObject::Template 
+GeomObject::TriTemplate(3,GeomObject::TriT,
+			 2,5,TriTemplatePerm_v);
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+static int OrientedTetraTemplate_v[] = {1,2,0,3,
+					2,0,1,3,
+					0,3,1,2,
+					1,0,3,2,
+					3,1,0,2,
+					1,3,2,0,
+					3,2,1,0,
+					2,1,3,0,
+					0,2,3,1,
+					2,3,0,1,
+					3,0,2,1,GeomObject::NULL_NODE};
+GeomObject::Template 
+GeomObject::OrientedTetraTemplate(4,GeomObject::OrientedTetraT,
+				  3,11,OrientedTetraTemplate_v);
 
 int main() { 
-  Mesh mesh(2,3);
-  mesh.read("coord.dat","icone.dat");
+  int v1[] = {0,1};
+  GeomObject edge(GeomObject::EdgeT,v1);
+  int v2[] = {23,35,2};
+  GeomObject tri(GeomObject::TriT,v2);
+  tri.make_canonical();
+  dvector<int> nodes;
+  nodes.mono(4);
+  nodes.ref(0) = 34;
+  nodes.ref(1) = 98;
+  nodes.ref(2) = 91;
+  nodes.ref(3) = 76;
+  GeomObject tetra(GeomObject::OrientedTetraT,nodes.buff());
+  tetra.make_canonical();
+#if 0
+  while(1) {
+    for (int j=0; j<4; j++) 
+      nodes.ref(j) = rand() % 100;
+    GeomObject tetra(GeomObject::OrientedTetraT,nodes.buff());
+    tetra.make_canonical();
+  }
+#endif
 }
-
--- tools/femref/femref.h
+++ tools/femref/femref.h
@@ -1,6 +1,6 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: femref.h,v 1.15 2004/11/21 21:22:42 mstorti Exp $
+// $Id: femref.h,v 1.16 2004/11/21 23:43:09 mstorti Exp $
 #ifndef PETSCFEM_FEMREF_H
 #define PETSCFEM_FEMREF_H
 
@@ -10,101 +10,26 @@
 #include <src/linkgraph.h>
 #include <src/generror.h>
 
-#if 0
-typedef unsigned int Node;
-
-class GeomObjectBasic;
-class GeomObjectId;
-
 class GeomObject {
-private:
-  GeomObjectBasic *gobj;
 public:
   /// Types of objects
-  enum Type { NULL_TYPE=0, OrientedEdgeT, EdgeT, OrientedTriT, TriT };
-  /// Ctor of empty object
-  GeomObject();
-  /// Set object to especific instance
-  void init(int sz,const int *nodes,Type t);
-  /// Constructor
-  GeomObject(int sz,const int *nodes,
-	     Type t) { init(sz,nodes,t); }
-  /// Compare two objects
-  bool equal(GeomObject &go);
-  /// Dimension of the object
-  int dim();
-  /** Check sum of the object, may be used as 
-      a weaker comparison operator. */ 
-  int csum();
-  /// Number of objects of type #t# adjacent to this object. 
-  int size(Type t);
-  /// Number of nodes adjacent to this object. 
-  int size();
-  /** Number of permutations that leave 
-      this object invariant. */ 
-  int nperms();
-  /** Returns the #perm_indx# permutatation for 
-      this kinf of object. */
-  const int* perm(int perm_indx);
-  /// Type of this object. 
-  Type type();
-  /// 
-  void get_adjacency(const GeomObjectId &id1,
-		     GeomObject::Type t,
-		     std::list<GeomObjectId> &adjacent) { }
-  /// Bring the node sequence to canonical order
-  void make_canonical();
-  /// Clears the object and set to the empty object. 
-  void clear();
-  /// Dtor. 
-  ~GeomObject();
-};
-
-/** Geometric objects are stored via a kind of
-    smart pointer. This is the real object stored 
-    via an indirect pointer in a #GeomObject#. */ 
-class GeomObjectBasic {
-public:
-  /// Compares two objects
-  virtual bool equal(GeomObjectBasic &)=0;
-  /// Returns the dimension of the object
-  virtual int dim()=0;
-  /// Returns the a fast check sum for the object. 
-  virtual int csum()=0;
-  /// Returns the number of nodes connected to this object. 
-  virtual int size();
-  /** Returns the number of object of type #t#
-      connected to #*this#. */ 
-  virtual 
-  int size(GeomObject::Type t) {return 0; }
-  /// Type of this object. 
-  virtual GeomObject::Type type() { 
-    return GeomObject::NULL_TYPE; 
-  }
-  /** Number of permutations that leave 
-      this object invariant. */ 
-  virtual int nperms() { return 0; }
-  /** Returns the #perm_indx# permutatation for 
-      this kinf of object. */
-  virtual const int* perm(int perm_indx);
-  /// Creates objects on demand. 
-  static GeomObjectBasic 
-  *factory(int sz,const int *nodes, GeomObject::Type t);
-};
-#endif
-
-class GeomObjectSeq : public GeomObjectBasic {
+  enum Type { NULL_TYPE=0, 
+	      OrientedEdgeT, EdgeT, 
+	      OrientedTriT, TriT,
+	      OrientedTetraT, TetraT};
+  static int NULL_NODE;
 protected:
   class Template {
-    friend class GeomObjectSeq;
-    static int NULL_NODE;
+    friend class GeomObject;
     dvector<int> perms_v;
     int size_m,dim_m,nperms_m;
     GeomObject::Type type;
     Template(int sz,GeomObject::Type t,
-	       int dim_a,int nperms_a,const int *perms);
+	       int dim_a,int nperms_a,
+	     const int *perms);
   };
-  static Template EdgeTemplate;
+  static Template EdgeTemplate, TriTemplate, 
+    OrientedTetraTemplate;
   Template *go_template;
 private:
   int cs, canonical;
@@ -117,23 +42,11 @@ public:
   const int* perm(int perm_indx) {
     return &(go_template->perms_v.e(perm_indx,0)); 
   }
-  GeomObject::Type type() { return go_template->type; }
-  GeomObjectSeq(GeomObject::Type t,const int *nodes_a);
+  Type type() { return go_template->type; }
+  GeomObject(Type t,const int *nodes_a);
   void make_canonical();
   /// Compare two objects
   bool equal(GeomObject &go);
 };
-
-#if 0
-#define GO_CLASS(TYPE)					\
-class TYPE : public GeomObjectSeq {			\
-public:							\
-  TYPE(const int *nodes_a)				\
-    : GeomObjectSeq(&GeomObjectSeq::TYPE##Template,	\
-		    nodes_a) { }			\
-}
-
-GO_CLASS(Edge);
-#endif
 
 #endif
