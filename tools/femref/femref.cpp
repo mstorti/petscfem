b246f89769648748aea6293c8f6c8099144c66ec
mstorti <mstorti>
2004-11-23 19:50:55 +0000

    Added Doc++ and comments to femref.

--- tools/femref/femref.cpp
+++ tools/femref/femref.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-// $Id: femref.cpp,v 1.16 2004/11/23 13:05:37 mstorti Exp $
+// $Id: femref.cpp,v 1.17 2004/11/23 19:50:55 mstorti Exp $
 
 #include <string>
 #include <limits.h>
@@ -53,22 +53,13 @@ void GeomObject::init(Type t,const int *nodes_a) {
 
   clear();
 
-#if 0
-#define SET_TEMPL(CLASS) \
-else if(t==CLASS##T) go_template = &CLASS##Template
-
-  if (t==NULL_TYPE) assert(0);
-  // SET_TEMPL(OrientedEdge);
-  // SET_TEMPL(Edge);
-  SET_TEMPL(OrientedTri);
-  // SET_TEMPL(Tri);
-  SET_TEMPL(OrientedTetra);
-  else assert(0);
-#endif
-
+  // Set template ptr from type
   go_template = get_template(t);
+  // resize node array
   int sz = size();
   nodes_m.mono(sz);
+  // Copy node array in internal vector
+  // and compute check-sum
   if (nodes_a) {
     cs = 0;
     for (int j=0; j<sz; j++) {
@@ -98,27 +89,44 @@ GeomObject::Template* GeomObject::get_template(Type t) {
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void GeomObject::make_canonical() {
-  // iterate through all possible perms. and
+  // Iterate through all possible perms. and
   // remain with lowest (in lexicographical order)
   // numbering
+  
+  // Apply all permutations and compare
+  // with current minimum.
+  // The comparison is done at the same time
+  // the permutation is being built, so that
+  // if the first node is higher than the
+  // first node of th current minimum then
+  // we skip to the next permutation. 
+
+  // Number of permutations for current geometry
   int np = nperms();
+  // Number of nodes in object
   int sz = size();
+  // aux/cmin will be the current minimum
+  // ordering of nodes
   dvector<int> aux;
   aux.mono(sz);
   int *cmin = &aux.ref(0);
+  // Ptr to internal nodes
   int *nodesp = &nodes_m.ref(0);
-#define DBG
+  //#define DBG
 #ifdef DBG
   printf("entered: ");
   for (int kk=0; kk<sz; kk++) 
     printf(" %d",nodesp[kk]);
   printf("\n");
 #endif
+  // Make current minimum = current ordering
   for (int kk=0; kk<sz; kk++) 
     cmin[kk] = nodesp[kk];
+  // Loop over permutations that leave
+  // invariant this shape
   for (int j=0; j<np; j++) {
     // Go constructing the permutation. 
-    // If it is higher skip. Else replain current min
+    // If it is higher skip. Else replace current min
     const int *permv = perm(j);
 #if 0 && defined DBG
     printf("perm %d: ",j);
@@ -142,9 +150,10 @@ void GeomObject::make_canonical() {
     printf("\n");
 #endif
   }
+  // Copy lowest ordering to internal ordering
   for (int kk=0; kk<sz; kk++) 
     nodesp[kk] = cmin[kk];
-#if 1
+#if 0
   printf("canonical form: ");
   for (int kk=0; kk<sz; kk++) 
     printf(" %d",nodesp[kk]);
@@ -154,10 +163,13 @@ void GeomObject::make_canonical() {
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 bool GeomObject::equal(GeomObject &go) {
+  // Objects should have the same type and check-sum
   if (type() != go.type()) return false;
   if (csum() != go.csum()) return false;
+  // Bring both of them to canonical ordering
   make_canonical();
   go.make_canonical();
+  // Compare node sequences
   int *np = nodes_m.buff();
   int *gonp = go.nodes_m.buff();
   for (int j=0; j<go_template->size_m; j++)
@@ -175,11 +187,14 @@ GeomObject::Template
 	   const char *label_a) 
   : size_m(sz), type(t), dim_m(dim_a), nperms_m(nperms_a),
     label(label_a) {
+  // Copy external array to internal
   perms_v.resize(nperms_m*size_m);
   const int *q = perms;
   for (int j=0; j<nperms_m*size_m; j++)
     perms_v.e(j) = *q++;
+  // Check end of sequence
   assert(*q==GeomObject::NULL_NODE);
+  // reshape internal array
   perms_v.reshape(2,nperms_m,size_m);
 }
 
@@ -199,59 +214,58 @@ void GeomObject::print(const char*s) const {
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void GeomObject
 ::set(Type t,int j,GeomObject &go) const {
+  // Set #go# to internal subobject #j of type #t#. 
+  // Template of subobject
   Template * go_tmpl = get_template(t);
+  // Size (number of nodes) of subobject
   int go_sz = go_tmpl->size_m;
+  // List of nodes for internal subobject
   dvector<int> go_nodes;
   go_nodes.mono(go_sz);
+  // Nodes of subobject (local to large object)
   const int *local_nodes = go_template->nodes(t,j);
   const int *my_nodes = nodes();
+  // Define sub-object nodes
   for (int k=0; k<go_sz; k++) 
     go_nodes.e(k) = my_nodes[local_nodes[k]];
+  // Create sub-object
   go.init(t,go_nodes.buff());
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 int GeomObject::find(GeomObject &go) const {
+  // Find sub-object go in large object *this. Returns
+  // number of subobject in large object. Pay attention to
+  // the fact that the sub-object numbering is relative to
+  // the current large-object node numbering. 
+
+  // Shape of sub-object
   Type typ = go.type();
-  // Template *templ = get_template(t);
+  // Nbr of nodes in subobject
   int n_so = size(typ);
+  // Internal sub-object of the given type
   GeomObject subobj;
   for (int j=0; j<n_so; j++) {
+    // create j-th sub-object
     set(typ,j,subobj);
+    // compare objects
     if (go.equal(subobj)) return j;
   }
   return -1;
 }
 
-#if 0
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-#define TEMPLATE(TYPE,size,dim,nperms)				\
-GeomObject::Template						\
-GeomObject::TYPE##Template(size,GeomObject::TYPE##T,		\
-			 dim,nperms,TYPE##TemplatePerm_v,#TYPE)
-
-static int EdgeTemplatePerm_v[] = {1,0,GeomObject::NULL_NODE};
-TEMPLATE(Edge,2,1,1);
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
-static int 
-TriTemplatePerm_v[] = {1,2,0,
-		       2,0,1,
-		       0,2,1,
-		       2,1,0,
-		       1,0,2,GeomObject::NULL_NODE};
-TEMPLATE(Tri,3,2,5);
-#endif
-
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void UniformMesh::set(iterator it,GeomObject &go) {
   // assert(it.t == tmpl->type);
+  // check that large-object number is OK.
   assert(0 <= it.obj && it.obj<nelem);
+  // Number of sub-objects of type t in large-object
   int sz = tmpl->size(it.t);
   assert(it.subobj < sz);
-  // go.init(it.t);
+  // Shape of the sub-object
   GeomObject::Template *so_tmpl = 
     GeomObject::get_template(it.t);
+  // Nodes in local numbering
   const int *local_nodes = tmpl->nodes(it.t,it.subobj);
   // Nodes of the subobject
   dvector<int> so_nodes;
@@ -265,6 +279,7 @@ void UniformMesh::set(iterator it,GeomObject &go) {
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void 
 UniformMesh::find(GeomObject &go,list<iterator> &its) {
+  // This is a wrapper, returns the list
   iterator it;
   its.clear();
   find(go,&its,it);
@@ -273,6 +288,7 @@ UniformMesh::find(GeomObject &go,list<iterator> &its) {
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 Mesh::iterator 
 UniformMesh::find(GeomObject &go) { 
+  // This is a wrapper, returns the first iterator
   iterator it;
   find(go,NULL,it);
   return it;
@@ -281,10 +297,15 @@ UniformMesh::find(GeomObject &go) {
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 void UniformMesh::
 find(GeomObject &go,list<iterator> *its,iterator &it) {
+  // Finds the elements connected to #go#
+  // by performing the intersection of the sets of
+  // large-objects connected to the nodes in #go#. 
   int sz = go.size();
   assert(sz>0);
-  dvector<int> p, pe, vals;
-  p.mono(sz); pe.mono(sz); vals.mono(sz);
+  // `p' is an array of cursors. `[p(j),pe(j))' is the 
+  // set of elements connected to node `j' in `go'. 
+  dvector<int> p, pe;
+  p.mono(sz); pe.mono(sz);
   const int *nodes_p = go.nodes();
   int emin, emax, jmin;
   go.print("searching face: ");
@@ -292,9 +313,11 @@ find(GeomObject &go,list<iterator> *its,iterator &it) {
     int node = nodes_p[j];
     p.e(j) = n2e_ptr.e(node);
     pe.e(j) = n2e_ptr.e(node+1);
+    // Normally, each node should have at least one element
+    // connected to it
     assert(pe.e(j)>p.e(j));
   }
-#if 1
+#if 0
   for (int j=0; j<sz; j++) {
     printf("ngbrs elems for node %d: ",nodes_p[j]);
     for (int k=p.e(j); k<pe.e(j); k++) {
@@ -304,25 +327,30 @@ find(GeomObject &go,list<iterator> *its,iterator &it) {
   }
 #endif
   while (1) {
-    // Search for min and max elements
-    printf("elems in ptrs: ");
+    // Search for min and max of elements pointed
+    // by current position of cursors. 
+    // printf("elems in ptrs: ");
     for (int j=0; j<sz; j++) {
       int pp = p.e(j);
       int ele = n2e.e(pp);
-      printf("%d ",ele);
+      // printf("%d ",ele);
       if (j==0) emin = emax = ele; 
       if (ele < emin) emin = ele;
       else if (ele > emax) emax = ele;
     }
-    printf("\n");
-    // If all elems are equal 
+    // printf("\n");
+    // If all elems are equal, then this element
+    // is a candidate to contain the object
     int indx;
     if (emin==emax) {
-      printf("candidate elem %d\n",emin);
-      // GeomObject(Type t,const int *nodes_a=NULL);
+      // printf("candidate elem %d\n",emin);
+      // Construct the large-object
       GeomObject super(tmpl->type,&connec.e(emin,0));
+      // Look for `go' in this large-object
       indx = super.find(go);
       if (indx>=0) {
+	// If `its==NULL' then the value is returned via `it',
+	// else it is appended to `*its' and process continue. 
 	if (!its) {
 	  it = iterator(emin,go.type(),indx);
 	  return;
@@ -331,20 +359,31 @@ find(GeomObject &go,list<iterator> *its,iterator &it) {
 	}
       }
     } 
+    // Advance all cursors that are pointing to the lowest
+    // element.
     for (int j=0; j<sz; j++) {
       int pp = p.e(j);
       if (n2e.e(pp) == emin) {
+	// We reached the end for at least one
+	// of the list of elements. 
 	if (++p.e(j) >= pe.e(j)) {
+	  // If we are returning the list of iterators,
+	  // then return the empty list. If not, return
+	  // the empty iterator. 
 	  if (!its) it = iterator();
 	  return;
 	} 
       }
     }
   }
-  assert(0); // we should never reach here
+  // we should never reach here
+  assert(0); 
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+// Generates a random choice of `N' elements
+// from the range `[0,M)', with `M<=N'. If `M=N'
+// then this is a random permutation of the integers in `[0,N)'. 
 void rand_perm(vector<int> &perm,int N,int M=-1) {
   if (M<0) M=N;
   vector<int> aux;
