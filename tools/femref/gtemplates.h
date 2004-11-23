9948ff5c91ddd795be5b47028c4f11d632c93ad1
mstorti <mstorti>
2004-11-23 18:41:10 +0000

    *** empty log message ***

--- tools/femref/gtemplates.h
+++ tools/femref/gtemplates.h
@@ -1,6 +1,6 @@
 // -*- mode: c++ -*-
 //__INSERT_LICENSE__
-// $Id: gtemplates.h,v 1.2 2004/11/22 19:35:22 mstorti Exp $
+// $Id: gtemplates.h,v 1.3 2004/11/23 18:41:10 mstorti Exp $
 #ifndef PETSCFEM_GTEMPLATES_H
 #define PETSCFEM_GTEMPLATES_H
 
@@ -43,5 +43,25 @@ public:
 
 extern OrientedTriTemplateClass 
 OrientedTriTemplate;
+
+#if 0
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+#define TEMPLATE(TYPE,size,dim,nperms)				\
+GeomObject::Template						\
+GeomObject::TYPE##Template(size,GeomObject::TYPE##T,		\
+			 dim,nperms,TYPE##TemplatePerm_v,#TYPE)
+
+static int EdgeTemplatePerm_v[] = {1,0,GeomObject::NULL_NODE};
+TEMPLATE(Edge,2,1,1);
+
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+static int 
+TriTemplatePerm_v[] = {1,2,0,
+		       2,0,1,
+		       0,2,1,
+		       2,1,0,
+		       1,0,2,GeomObject::NULL_NODE};
+TEMPLATE(Tri,3,2,5);
+#endif
 
 #endif
