0727d723cb3f3a73d9923293f42b892aee62fcc7
mstorti <mstorti>
2004-12-29 00:34:09 +0000

    *** empty log message ***

--- tools/femref/tryme2.cpp
+++ tools/femref/tryme2.cpp
@@ -1,6 +1,7 @@
 //__INSERT_LICENSE__
-// $Id: tryme2.cpp,v 1.3 2004/12/28 23:27:02 mstorti Exp $
+// $Id: tryme2.cpp,v 1.4 2004/12/29 00:34:09 mstorti Exp $
 
+#include <ctime>
 #include <cstdio>
 #include <cassert>
 #include <cmath>
@@ -55,6 +56,7 @@ lower_boundb_aux(vector<int> &v,
 
 int lower_boundb(vector<int> &v, 
 		 int x, int j1, int j2) {
+  // printf("j1 %d, j2 %d\n",j1,j2);
   while (j2-j1 > 1) {
     its++;
     int j = j1 + (j2-j1)/2;
@@ -74,14 +76,14 @@ int lower_boundbl(vector<int> &v, int x) {
     j2 = v.size()-1,
     v2 = v[j2];
   if (x > v2) return v.size();
-  if (x == v2 || j2-j1 <= 1) 
-    return lower_boundb_aux(v,x,j2);
+  if (x < v1) return 0;
+  if (v1 == v2) return 0;
   j = j1 + int((double(x-v1)*double(j2-j1))/double(v2-v1));
-  int dj = int(sqrt(double(j)));
+  int dj = int(pow(double(j),0.5));
   int jj1 = j-dj;
   if (jj1>=0) {
     int vjj1 = v[jj1];
-    if (vjj1 <= x) {
+    if (vjj1 < x) {
       v1=vjj1;
       j1=jj1;
     }
@@ -89,7 +91,7 @@ int lower_boundbl(vector<int> &v, int x) {
   int jj2 = j + dj;
   if (jj2<v.size()) {
     int vjj2 = v[jj2];
-    if (vjj2 > x) {
+    if (vjj2 >= x) {
       v2 = vjj2;
       j2=jj2;
     }
@@ -99,22 +101,56 @@ int lower_boundbl(vector<int> &v, int x) {
 
 int lower_boundb(vector<int> &v, int x) {
   its = 0;
-  if (v[0]>x) return 0;
+  if (v[0]>=x) return 0;
+  // printf("size %d\n",v.size());
   return lower_boundb(v,x,0,v.size());
 }
 
 int main() {
-  int N=10000;
+  time_t start, end;
+  int N=10000000, ntries=10000000;
   vector<int> v(N);
-  for (int j=0; j<N; j++)
+  for (int j=0; j<N; j++) 
     v[j] = rand();
   sort(v.begin(),v.end());
 
-  for (int k=0; k<10; k++) {
+#if 0
+  for (int j=0; j<1000; j++) 
+    printf("v[%d]=%d\n",j,v[j]);
+#endif
+  
+  start = time(NULL);
+  char *method = NULL;
+  int iters = 0;
+  for (int k=0; k<ntries; k++) {
     int j = rand() % N;
-    // int jj = lower_boundb(v,v[j]);
+#if 0
+    if (j == 0) {
+      printf("here\n");
+    }
+#endif
+
+#if 1
+    method = "binary search";
+    int jj = lower_boundb(v,v[j]);
+#else
+    method = "linear+binary search";
     int jj = lower_boundbl(v,v[j]);
-    assert(v[jj]==v[j]);
-    printf("%d its, bin %d\n",its,ilog2(N));
+#endif
+    iters += its;
+    if (v[jj]!=v[j]) {
+      printf("v[j=%d]=%d, v[jj=%d]=%d, k %d\n",
+	     j,v[j],jj,v[jj],k);
+    }
+    // printf("%d its, bin %d\n",its,ilog2(N));
   }
+  double elaps = difftime(time(NULL),start);
+  printf("Stats for %s method:\n" 
+	 "%d tries, %d elements\n"
+	 "%d its, rate %fits/try\n"
+	 "%d secs, rate %g secs/T-elem-try\n",
+	 method, ntries, N, iters, 
+	 double(iters)/double(ntries),
+	 int(elaps), 
+	 elaps/(double(N)*double(ntries))*1e12);
 }
