c6673f07968185bfc3f22a973855fb28c42c8aab
mstorti <mstorti>
2005-01-12 16:07:07 +0000

    *** empty log message ***

--- tools/femref/mkcube.m
+++ tools/femref/mkcube.m
@@ -1,9 +1,10 @@
-N=1;
+N=10;
 
-w=zhomo([0 1 0 1],N+1,N+1);
+w=zhomo([-1 1 -1 1],N+1,N+1);
 [x2,i2] = pfcm2fem(w);
 
 [xnod,icone] = extrude(x2,i2,N,1/N);
+xnod(:,3) = 2*xnod(:,3)-1;
 
 asave("cube.con-hexa.tmp",icone);
 asave("cube.nod.tmp",xnod);
@@ -13,5 +14,9 @@ iconet = aload("cube.con.tmp");
 asave("cube.con.tmp",iconet-1);
 
 nnod = rows(xnod);
-asave("cube.state.tmp",[xnod,zeros(nnod,1)]);
+v = pvec(xnod,[0,0,1]);
+rho = l2(v);
+v = leftscal(1./(0.05+rho).^2,v);
+
+asave("cube.state.tmp",[v,zeros(nnod,1)]);
 
