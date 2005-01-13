8096fd388c5f990c0a1270688727f9aa50f86612
mstorti <mstorti>
2005-01-13 16:01:44 +0000

    *** empty log message ***

---
+++ tools/femref/data.pl
@@ -0,0 +1,14 @@
+require "$ENV{'PETSCFEM_DIR'}/test/eperlini.pl";
+
+$N = 10; 
+$nnod = ($N+1)**3; 
+$nelem = $N**3*5;
+$nfaces = $nelem*4;
+$nfaces_ext = 2*6*$N**2;
+
+@vars = qw(N);
+octave_export_vars(">data.m.tmp",@vars);
+
+system "octave -qH mkcube.m >/dev/null";
+
+1;
--- tools/femref/mkcube.m
+++ tools/femref/mkcube.m
@@ -1,4 +1,4 @@
-N=30;
+source("data.m.tmp");
 h = 2/N;
 dd = 0.5;
 
@@ -13,10 +13,13 @@ asave("cube.nod.tmp",xnod);
 
 system("../hexasplit.bin -i cube.con-hexa.tmp -o cube.con.tmp");
 
+icone = aload("cube.con.tmp");
+asave("cube.con0.tmp",icone-1);
+
 nnod = rows(xnod);
 v = pvec(xnod,[0,0,1]);
 rho = l2(v);
-v = -leftscal(1./(dd^2+rho.^2),v);
+v = leftscal(1./(dd^2+rho.^2),v);
 
 asave("cube.state.tmp",[v,zeros(nnod,1)]);
 
@@ -26,7 +29,7 @@ grad_u = aload("cube.grad-u.tmp");
 
 w = [+grad_u(:,10)-grad_u(:,7), \
      -grad_u(:,3)+grad_u(:,9), \
-     +grad_u(:,5)-grad_u(:,2)]/2;
+     +grad_u(:,5)-grad_u(:,2)];
 aw=l2(w);
 
 asave("cube.aw.tmp",aw);
@@ -35,7 +38,7 @@ surf_con = aload("cube.surf-con.tmp")+1;
 xe = pfnd2ele(xnod,surf_con,xnod);
 
 rhoe=l2(xe(:,1:2));
-awt = (dd^2-rhoe.^2)./(dd^2+rhoe.^2).^2;
+awt = 2*dd^2./(dd^2+rhoe.^2).^2;
 
 ## indx = find(xe(:,2)<h & xe(:,2)>0 & xe(:,3)>0 & abs(xe(:,1))<0.99);
 indx = find(abs(xe(:,1)-xe(:,2))<1e-4 & xe(:,3)>0);
