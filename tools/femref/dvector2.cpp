4aa0aa7386fc43b3d5ddee96b29bdf84fe6d40fb
mstorti <mstorti>
2005-01-16 23:58:59 +0000

    *** empty log message ***

---
+++ tools/femref/dvector2.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <libguile.h>
+
+#include <src/dvector.h>
+
+void scmlist2vec(SCM s_list,vector<int> &v) {
+  SCM_ASSERT(scm_list_p(s_list),
+	     s_list, SCM_ARG1, __FUN__);
+  v.clear();
+  SCM q = s_list;
+  while(!SCM_NULLP(q)) {
+    SCM item = SCM_CAR(q);
+    SCM_ASSERT(scm_integer_p(item),item,SCM_ARG1,"scmlist2vec");
+    int w = SCM_INUM(item);
+    printf("%d ",w);
+    v.push_back(w);
+    q = SCM_CDR(q);
+  }
+  printf("\n");
+}
