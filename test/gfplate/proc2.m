f4ce6d6fafbc36abbf6e61afeb06dda02a2a8f88
mstorti <mstorti>
2005-01-21 15:25:25 +0000

    *** empty log message ***

--- test/gfplate/proc2.m
+++ test/gfplate/proc2.m
@@ -1,15 +1,22 @@
-## $Id: proc2.m,v 1.2 2005/01/21 03:14:30 mstorti Exp $
+## $Id: proc2.m,v 1.3 2005/01/21 15:25:25 mstorti Exp $
 
 source("data.m.tmp");
 
-U = aload("gfabso.some-rslt.tmp");
+if 1
+  U = aload("gfabso.some-rslt.tmp");
+  rem(rows(U),Nx+1)==0 || error("not correct size");
+  nt = rows(U)/(Nx+1);
+  u=reshape(U(:,3),Nx+1,nt);
+  nnod = rows(u);
+  u = u(1:nnod-2,:);
+  x = aload("gfabso.nod.tmp");
+  x = x(1:Nx-1,1);
+endif
 
-rem(rows(U),Nx+1)==0 || error("not correct size");
-
-nt = rows(U)/(Nx+1);
-u=reshape(U(:,3),Nx+1,nt);
-nnod = rows(u);
-u = u(1:nnod-2,:);
-
-plot(u)
+axis([0 Lx min(min(u)) max(max(u))])
+m=1;
+for k=1:m:columns(u)
+  plot(x,u(:,k));
+  pause(0.2);
+endfor
 
