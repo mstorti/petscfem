a17ba388873314e0154f3e0967fb6675e2392de7
mstorti <mstorti>
2005-01-23 23:08:31 +0000

    Tested `gasflow_abso' in a 3D strip with lateral velocity in the mean flow. Perturbation in lateral velocity.

--- test/gfplate/mkgfabso.m
+++ test/gfplate/mkgfabso.m
@@ -1,4 +1,4 @@
-## $Id: mkgfabso.m,v 1.17 2005/01/23 23:01:59 mstorti Exp $
+## $Id: mkgfabso.m,v 1.18 2005/01/23 23:08:31 mstorti Exp $
 source("data.m.tmp");
 
 poutlet = pref;
@@ -71,7 +71,7 @@ nnod2 = size(xnod,1);
 Uini = Uref(ones(nnod2,1),:);
 Uini(nnod+[1,3],:) = 0;		# lagrange multipliers to 0
 
-dw = 0.2*[0 1 0 0 0]; ## perturbation
+dw = 0.5*[0 0 1 0 0]; ## perturbation
 dw(2:4) = dw(2:4)*Orot';
 
 dfx = exp(-((x-Lx/2)/sigma).^2);
