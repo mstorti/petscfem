6d6adecfba6233d60a6957f358c1f96ac18327ac
mstorti <mstorti>
2005-02-16 01:22:56 +0000

    *** empty log message ***

--- tools/femref/test5.scm
+++ tools/femref/test5.scm
@@ -1,8 +1,47 @@
-;;; $Id: test5.scm,v 1.1 2005/02/16 00:25:58 mstorti Exp $
+;;; $Id: test5.scm,v 1.2 2005/02/16 01:22:56 mstorti Exp $
 
+(use-modules (srfi srfi-1))
+
+(define (sum l) (fold (lambda (a b) (+ a b)) 0 l))
+
+;;; ================================================================
 ;;; Generate all partitions of 'n'
 (define (partition n)
-  (let loop ((partial-list '())
+  ;;; Completes one partition
+  (define (complete-1 part min-elem remain)
+    (let ((kmax (min min-elem remain)))
+      (cond ((zero? remain) (list part))
+	    (#t 
+	     (let loop ((k 1)
+			(completed-parts '()))
+	       (cond ((= k kmax) completed-parts)
+		     (#t 
+		      (loop (+ k 1) 
+			    (map (lambda (q) 
+				   (append q part))
+				 (append
+				  completed-parts 
+				  (complete-1 (cons k part) k (- n k))))))))))))
+  (complete-1 '() n n))
+
+#!
+;;; ================================================================
+;;; Generate all partitions of 'n'
+(define (partition2 n)
+  (let (part-list '())		; List of partial partitions
+    (let loop ((q part-list)
+	       (completed-lists '()))
+      (cond ((null? q) completed-lists)
+	    (#t (append 
+		 completed-lists 
+		 ;; Given a `partial list' returns the partitions
+		 ;; originated in from it. 
+		 (let loop2 ((partial-list (car q))
+			     (result '()))
+		   (let ((min 
+
+
+  (let loop ((part-list '(()))
 	     (min-elem n)
 	     (remain n))
     (cond ((= remain 0) partial-list)
@@ -21,5 +60,7 @@
 				       (append q partial-list))
 				     (loop (cons k partial-list) k (- remain k))))
 			(+ k 1)))))))))
+!#
 
-(partition 4)
+(let ((n 4))
+  (format #t "part ~A: ~A" n (partition n)))
