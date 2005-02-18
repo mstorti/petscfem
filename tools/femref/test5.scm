17b0317727a8d23e50fa27139875215316372b9a
mstorti <mstorti>
2005-02-18 00:46:41 +0000

    *** empty log message ***

--- tools/femref/test5.scm
+++ tools/femref/test5.scm
@@ -1,4 +1,4 @@
-;;; $Id: test5.scm,v 1.11 2005/02/18 00:29:11 mstorti Exp $
+;;; $Id: test5.scm,v 1.12 2005/02/18 00:46:41 mstorti Exp $
 
 (use-modules (srfi srfi-1))
 
@@ -53,6 +53,8 @@
 			     ;; `complete-1' with rest `remain-k'. 
 			     (complete-1 (cons k part) k (- remain k))))))))))))
 
+(define (nparts2 n) (length (partition n)))
+
 ;;---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 ;; Computes the number of partitions of `n'
 (define (nparts n)
@@ -68,65 +70,54 @@
 	  ((= m 0) 1)
 	  ((= p 1) 1)
 	  (#t 
-	   (format #t "(nparts-prtl ~A ~A)\n" m p)
 	   (let ((k-max (min p m)))
 	     (let loop ((q 0)
 			(k 1))
 	       (cond ((> k k-max) q)
 		     (#t (loop (+ q (nparts-prtl (- m k) k)) (+ k 1))))))))))
 
-(nparts 4)
-(define (nparts2 n) (length (partition n)))
-
-#!
-(let* ((n 7)
-       (parts (partition n)))
-  (format #t "part ~A: total ~A partitions.\n" n (length parts))
-;  (format #t "partitions: ~A\n" parts)
-  (format #t "(nparts n) -> ~A\n" (nparts n n)))
-!#
-
-#!
-(let loop ((n 1)
-	   (p 1))
-  (cond ((> p n) (format #t "\n") (loop (+ n 1) 1))
-	((> n 5))
-	(#t (format #t "(~A ~A -> ~A ~A) " n p (nparts n p))  
-	    (loop n (+ p 1)))))
-!#
-
+;;---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
+;; This is the more efficient algorithm, constructing the table.
+;; In this form the algorithm is `n^3' (I think). 
 (define (nparts3 n)
-  (let* ((table (make-array 0 n n))
+  (let* (
+	 ;; This is the table that stores the previous values.
+	 (table (make-array 0 n n))
+	 ;; Auxiliary function that retrieves the value stored in the table
+	 ;; for the pair `(n p)'
 	 (np-ref (lambda (n p) 
 		   (cond ((<= n 1) 1)
 			 ((= p 1) 1)
 			 (#t (array-ref table (- n 1) (- p 1))))))
+	 ;; Auxiliary function that sets the entry in the table
+	 ;; for the pair `(n p)'
 	 (np-set! (lambda (val n p) 
 		    (array-set! table val 
-				(- n 1) (- p 1)))))
+				(- n 1) (- p 1))))
+	 ;; Computes one value in the table from previous values
+	 (compute-1 (lambda (m p)
+		      (let loop ((sub-parts 0)
+				 (k 1))
+			(cond ((> k (min p m)) sub-parts)
+			      (#t (loop (+ sub-parts (np-ref (- m k) k)) (+ k 1))))))))
+    ;; Loops over pairs `(m p)' increasing `p' first, until `(n n)' is
+    ;; reached.
     (let loop ((m 1)
 	       (p 1))
-      (format #t "m ~A, p ~A, table ~A\n" m p table)
+      ;; (format #t "m ~A, p ~A, table ~A\n" m p table)
       (cond ((> p m) (loop (+ m 1) 1))
-	    ((> m n) 
-	     (format #t "m ~A, n~A\n" m n)
-	     (np-ref n n))
+	    ((> m n) (np-ref n n))
 	    (#t 
-	     (np-set!
-	      (let loop2 ((sub-parts 0)
-			  (k 1))
-		(cond ((> k (min p m)) sub-parts)
-		      (#t (loop2 (+ sub-parts (np-ref (- m k) k)) (+ k 1)))))
-			  m p)
+	     (np-set! (compute-1 m p) m p)
 	     (loop m (+ p 1)))))))
 
+#!
 (let loop ((n 1))
   (cond ((> n 15))
 	(#t (format #t "(~A -> ~A ~A ~A)\n" 
 		    n (nparts n) (nparts2 n) (nparts3 n))
 	    (loop (+ n 1)))))
 
-#!
 (let ((n 5))
       (format #t "(nparts3 ~A) ~A\n" n (nparts3 n)))
 
