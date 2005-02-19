d6d7065ea543c2b3e1f0d3b4d859ad22d2a15daf
mstorti <mstorti>
2005-02-19 16:30:35 +0000

    *** empty log message ***

--- test/gfplate/compshock.m
+++ test/gfplate/compshock.m
@@ -13,17 +13,29 @@ function U2 = compshock (U1,du,ga);
   c1 = sqrt(ga*p1/rho1);
   M1 = u1/c1;
 
+  ## Solution is found by considering that 
+  ## (Ma+1/(ga*Ma))^2/(0.5*Ma^2+1/ga1) is constant
+  ## across the shock (Rankine-Hugoniot relations)
   ga1 = ga-1;
   C  = (M1+1/(ga*M1))^2/(0.5*M1^2+1/ga1);
 
+  ## Solves polynomial equation 
   coef = [C/2-1,C/ga1-2/ga,-1/ga^2];
   M2 = sqrt(roots(coef));
 
+  ## This should have two solutions, one
+  ## is M1 and the other should be M2
   [bid,indx] = min(abs(M2-M1));
   abs(M2(indx)-M1)<1e-10 || error("inconsistent result");
 
   ## M2 is Mach downstream
   M2(indx) = [];
+
+  ## Take care that, for a given `gamma' there is a minimum
+  ## downstream Mach possible (1/sqrt(7)=0.37796 for gamma=1.4).
+  ## If the 'M1' is lower than this value, then you can get complex
+  ## solutions to the equation. 
+  abs(imag(M2))<1e-10 || error("can't find a real solution");
 
   ## Values downstream
   u2 = u1*sqrt((0.5+1/M1^2/ga1)/(0.5+1/M2^2/ga1));
@@ -31,6 +43,7 @@ function U2 = compshock (U1,du,ga);
   rho2 = u1*rho1/u2;
   p2 = c2^2*rho2/ga;
 
+  ## State downstream
   U2 = [rho2,u2,p2];
 
 endfunction
--- test/gfplate/tryme3.m
+++ test/gfplate/tryme3.m
@@ -1,14 +1,23 @@
-## $Id: tryme3.m,v 1.1 2005/02/19 15:45:14 mstorti Exp $
+## $Id: tryme3.m,v 1.2 2005/02/19 16:30:35 mstorti Exp $
 ga = 1.4;
 ga1 = ga-1;
 
-M1 = 0.4;
+M1 = 1/sqrt(7)+0.001;
 rho1=1;
 p1=1/ga;
 
 c1 = sqrt(ga*p1/rho1);
 u1 = c1*M1;
 U1 = [rho1,u1,p1];
+
+U2 = compshock([rho1,u1,p1]');
+rho2 = U2(1);
+u2 = U2(2);
+p2 = U2(3);
+c2 = sqrt(ga*p2/rho2);
+M2 = u2/c2
+M2a = sqrt((M1^2+5)/(7*M1^2-1))
+return
 
 M2 = (0.1:0.01:20)';
 
@@ -18,7 +27,6 @@ u2 = sqrt(ht_1./(0.5+1./(M2.^2*ga1)));
 c2 = u2./M2;
 rho2 = rho1*u1./u2;
 p2 = c2.^2.*rho2/ga;
-
 
 flux1 = [rho1*u1, \
 	 rho1*u1^2+p1, \
