bf7c836ef76c924b40a2f79b50d6a46f9cb498e7
mstorti <mstorti>
2005-02-24 17:50:14 +0000

    *** empty log message ***

--- tools/project/interp2.m
+++ tools/project/interp2.m
@@ -22,8 +22,10 @@ nelem1 = rows(ico1);
 
 C = zeros(ndim);
 C2 = C;
-for j=1:nnod2
-  xe = x2(j,:);			# point to project
+xeh=[];
+xprojh=[];
+for j=1:100
+  xe = 3*rand(1,2)-1;			# point to project
   for k=1:nelem1
     nodes = ico1(k,:);
     C = [x1(nodes,:)';
@@ -42,7 +44,12 @@ for j=1:nnod2
       L2(indx) = 0;
       xproj = (C(1:2,:)*L2)';
     endif
-    xproj
+    xprojh=[xprojh;
+	    xproj];
+    xeh=[xeh;
+	 xe];
   endfor
 endfor
-  
+
+plot(xeh(:,1),xeh(:,2),'og',xprojh(:,1),xprojh(:,2),'or',\
+     [xeh(:,1)';xprojh(:,1)'], [xeh(:,2)';xprojh(:,2)'],b);
