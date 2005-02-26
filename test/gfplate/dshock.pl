cd26de29408507cc209ef42bc92e00b203fb6f3b
mstorti <mstorti>
2005-02-26 18:23:08 +0000

    Runs good example with pout=0.001*pin, low shocap. Forms shock at outlet.

--- test/gfplate/dshock.pl
+++ test/gfplate/dshock.pl
@@ -28,23 +28,24 @@ if (0) {
     $pout = 143; 
     $Tout = 262;
 } else { 
-    $pout = 0.01*$pin; 
+    $pout = 0.001*$pin; 
     $Tout = 262;
     $rhoout = $pout/($Rgas*$Tout);
 }
 $pout0 = $pout/$pref;
 $rhoout0 = $rhoout/$rhoref;
 
-$Co = 0.5;
+$Co = 1;
 $h = 1/$Nx;
 $Dt = $Co*$h/($uin0+1);
-$tramp = 10*$Dt;
+$tramp = 50*$Dt;
 
 @vars = qw(Dt ga Lx Nx Rgas pin0 rhoin0 uin0 pout0 rhoout0 tramp);
 octave_export_vars(">data.m.tmp",@vars);
 doc_vals(@vars);
 
 $mkmesh = $ENV{'mkmesh'};
+$mkmesh = 0 if !defined $mkmesh;
 if (!defined $mkmesh || $mkmesh) {
     system "octave -qH mkgfshock.m";
 }
