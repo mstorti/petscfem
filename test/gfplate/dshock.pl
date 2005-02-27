0ed712efc833a3b224e85b41c4ca24c691e1f781
mstorti <mstorti>
2005-02-27 02:37:34 +0000

    Runs shock/tuyere problem at pout=143Pa.

--- test/gfplate/dshock.pl
+++ test/gfplate/dshock.pl
@@ -28,7 +28,7 @@ if (0) {
     $pout = 143; 
     $Tout = 262;
 } else { 
-#    $pout = 0.001*$pin; 
+## 0.00023833 is the target $pin/$pout
     $pout = 143;
     $Tout = 262;
     $rhoout = $pout/($Rgas*$Tout);
@@ -36,17 +36,17 @@ if (0) {
 $pout0 = $pout/$pref;
 $rhoout0 = $rhoout/$rhoref;
 
-$Co = 0.1;
+$Co = 0.5;
 $h = 1/$Nx;
 $Dt = $Co*$h/($uin0+1);
-$tramp = 200*$Dt;
+$tramp = 20*$Dt;
 
 @vars = qw(Dt ga Lx Nx Rgas pin0 rhoin0 uin0 pout0 rhoout0 tramp);
 octave_export_vars(">data.m.tmp",@vars);
 doc_vals(@vars);
 
 $mkmesh = $ENV{'mkmesh'};
-$mkmesh = 0 if !defined $mkmesh;
+$mkmesh = 1 if !defined $mkmesh;
 if (!defined $mkmesh || $mkmesh) {
     system "octave -qH mkgfshock.m";
 }
