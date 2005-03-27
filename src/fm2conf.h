7c8545d39febdb2272b4ed1163b62bce80e5b70f
mstorti <mstorti>
2005-03-27 21:50:14 +0000

    *** empty log message ***

---
+++ src/fm2conf.h
@@ -0,0 +1,9 @@
+// -*- mode: c++ -*-
+//__INSERT_LICENSE__
+// $Id: fm2conf.h,v 1.1.2.1 2005/03/27 21:50:14 mstorti Exp $
+#ifndef PETSCFEM_FM2CONF_H
+#define PETSCFEM_FM2CONF_H
+
+//#define DO_FM2_STAT
+
+#endif
