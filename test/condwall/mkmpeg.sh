2d365690ed5bc91da5617420d33a60fd231f5134
mstorti <mstorti>
2005-03-30 01:17:56 +0000

    Runs example `condwall/movwall' with `T=10'.

--- test/condwall/mkmpeg.sh
+++ test/condwall/mkmpeg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#$Id: mkmpeg.sh,v 1.1 2005/03/29 21:05:26 mstorti Exp $
+#$Id: mkmpeg.sh,v 1.2 2005/03/30 01:17:56 mstorti Exp $
 
 mpeg=condwall.mpeg
 
 if [ -f $mpeg ] ; then rrm $mpeg ; fi
 if [ -f $mpeg ] ; then echo "$mpeg exists" ; exit 1 ; fi
 
-mkmpeg2.sh | ffmpeg -r 16/1 -s 640x480 -f rawvideo -i - -b 4000 $mpeg
+mkmpeg2.sh | ffmpeg -r 20/1 -s 640x480 -f rawvideo -i - -b 8000 $mpeg
---
+++ test/condwall/proc2.m
@@ -0,0 +1,9 @@
+# $Id: proc2.m,v 1.1 2005/03/30 01:17:56 mstorti Exp $
+
+# h=readconv("Residual_norm","nohup.log");
+# semilogy(h);
+# pause
+
+h2=readconv("norm_res","nohup.log");
+semilogy(h2);
+
