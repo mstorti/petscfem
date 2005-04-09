97860ee1027f0d822cf89c3fcf18d76776202e48
mstorti <mstorti>
2005-04-09 21:45:32 +0000

    Fixed error in `BasicObject' initialization.

--- src/pfobject.cpp
+++ src/pfobject.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-// $Id: pfobject.cpp,v 1.9 2005/04/09 21:38:26 mstorti Exp $
+// $Id: pfobject.cpp,v 1.10 2005/04/09 21:45:35 mstorti Exp $
 
 #include <src/pfobject.h>
 #include <src/texthash.h>
@@ -16,9 +16,13 @@ BasicObject::~BasicObject() {}
 BasicObject *BasicObject::factory(string &type) {
   if (0) {} // tricky!!
   // else if (type=="interpolation") return new interpolation;
+
   // BasicObject types specific for this application
-  else if (BasicObject_application_factory) {
-    return (*BasicObject_application_factory)(type);
-  } else return NULL;
+  if (BasicObject_application_factory) {
+    BasicObject *obj = (*BasicObject_application_factory)(type);
+    if (obj) return obj;
+  } 
+
+  return NULL;
 }
 
