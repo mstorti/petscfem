9d539784eae3c46e8478380e43c62edcd920fd7e
mstorti <mstorti>
2005-04-28 21:14:11 +0000

    *** empty log message ***

--- tools/project/project4.cpp
+++ tools/project/project4.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-// $Id: project4.cpp,v 1.5 2005/03/09 21:23:23 mstorti Exp $
+// $Id: project4.cpp,v 1.6 2005/04/28 21:14:11 mstorti Exp $
 
 #include <cstdio>
 #include <src/fastmat2.h>
@@ -49,7 +49,7 @@ int main() {
 #define XNOD2 "square2.nod.tmp"
 #endif
 
-#if 1
+#if 0
 #define DATA_DIR "./fluent"
 #define XNOD1 DATA_DIR "/fluent.nod"
 #define STATE1 DATA_DIR "/fluent.forces"
@@ -68,14 +68,23 @@ int main() {
 #if 0
 #define XNOD1  "./mesh3.nod"
 #define ICONE1 "./mesh3.con"
-#define XNOD2  "./mesh3.nod"
 #define STATE1 "./mesh3.nod"
+#define XNOD2  "./mesh3.nod"
 #endif
 
-  int ndim = 3;
+#if 1
+#define DATA_DIR "/u/mstorti/PETSC/COMP-CORNER/euler-mach-10-run2"
+#define XNOD1  DATA_DIR "/comp_corner_Ma_10_Euler.nod.tmp"
+  // #define ICONE1 DATA_DIR "/comp_corner_Ma_10_Euler.con.tmp"
+#define ICONE1 DATA_DIR "/comp-corner-tri.con.tmp"
+#define STATE1 DATA_DIR "/comp_corner_Ma_10_Euler.state.tmp"
+#define XNOD2  DATA_DIR "/test-points.dat"
+#endif
+
+  int ndim = 2;
   int ndimel = 2;
   int nel = 3;
-  int ndof = 3;
+  int ndof = 4;
 
   dvector<double> xnod1, xnod2, u1, u2,
     area1, area2;
@@ -85,11 +94,13 @@ int main() {
 	    u1,STATE1, u2,ico1,ICONE1,
 	    ndim,ndimel,nel,ndof);
 
+#if 0 // Si los datos vienen `concentrados' por nodos.
   nod_vol(xnod1,ico1,area1);
   int nnod = xnod1.size(0);
   for (int j=0; j<nnod; j++)
     for (int k=0; k<ndof; k++)
       u1.e(j,k) /= area1.e(j);
+#endif
 
   FemInterp fem_interp;
   fem_interp.init(10,ndof,ndimel,xnod1,ico1);
@@ -97,6 +108,7 @@ int main() {
   fem_interp.interp(xnod2,u1,u2);
   u2.print(DATA_DIR "/u2-interp.dat");
 
+#if 0
   dvector<int> ico2;
   ico2.cat(ICONE2).defrag();
   assert(ico2.size() % nel ==0);
@@ -110,5 +122,5 @@ int main() {
     for (int k=0; k<ndof; k++)
       u2.e(j,k) *= area2.e(j);
   u2.print("u2n.dat");
-  
+#endif
 }
