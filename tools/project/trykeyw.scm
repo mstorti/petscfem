74ed9180f084951b0ae3720aa7e742d316c8deb9
mstorti <mstorti>
2005-05-19 11:48:05 +0000

    *** empty log message ***

--- tools/project/trykeyw.scm
+++ tools/project/trykeyw.scm
@@ -1,10 +1,19 @@
-;;; $Id: trykeyw.scm,v 1.1 2005/05/17 16:29:39 mstorti Exp $
+;;; $Id: trykeyw.scm,v 1.2 2005/05/19 11:48:05 mstorti Exp $
 (use-modules (ice-9 format))
 (use-modules (ice-9 optargs))
 
+(define (remove-keys ls)
+  (let loop ((ls ls) (acc '()))
+    (if (null? ls)
+        (reverse! acc)
+        (let ((kw? (keyword? (car ls))))
+          (loop ((if kw? cddr cdr) ls)
+                (if kw? acc (cons (car ls) acc)))))))
+
 (define* (f x y #:key a b . rest)
-	   (format #t "x ~A, y ~A, a ~A, b ~A, rest ~A\n"
-		   x y a b rest))
+  (set! rest (remove-keys rest))
+  (format #t "x ~A, y ~A, a ~A, b ~A, rest ~A\n"
+	  x y a b rest))
 
 (f 1 2 3)
 (f 1 2 3 #:a 4)
@@ -15,3 +24,5 @@
 
 (g #:a 1 #:b 2)
 (g #:b 1 #:a 2)
+
+
