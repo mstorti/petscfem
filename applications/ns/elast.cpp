5dddcc642f092343c21e95be13a780e5078a34ef
mstorti <mstorti>
2005-05-23 18:36:26 +0000

    Merged from Beto. Doesn't compile yet.

--- applications/ns/elast.cpp
+++ applications/ns/elast.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: elast.cpp,v 1.16 2004/12/07 19:28:24 mstorti Exp $
+//$Id: elast.cpp,v 1.16.30.1 2005/05/23 18:36:26 mstorti Exp $
 
 #include <src/fem.h>
 #include <src/utils.h>
@@ -50,6 +50,14 @@ void elasticity::init() {
   assert(!(rec_Dt>0. && rho==0.));
   assert(ndof==ndim);
 
+  //o exponent for stiffen deforming elements (Tezduyar paper)
+  TGETOPTDEF(thash,double,stiffness_exponent,0.);
+  Jaco_pow = stiffness_exponent;
+
+  //o minimum det(Jaco) value for deformed mesh to stiffen deformed elements
+  TGETOPTDEF(thash,double,detJaco_minimum_value,1.0e-10);
+  detJaco_min = detJaco_minimum_value;
+
   G_body.resize(1,ndim).set(0.);
   const char *line;
   vector<double> G_body_v;
@@ -67,8 +75,11 @@ void elasticity::init() {
   B.resize(2,ntens,nen).set(0.);
   C.resize(2,ntens,ntens).set(0.);
   Jaco.resize(2,ndim,ndim);
+  Jaco_def.resize(2,ndim,ndim);
   G.resize(2,ndim,ndim);
   dshapex.resize(2,ndim,nel);  
+  dJaco.resize(2,nel,ndim);
+  tmp_elast.resize(1,nel);
 
   my_fun2.init(G);
   // Plane strain
@@ -99,25 +110,36 @@ void elasticity::element_connector(const FastMat2 &xloc,
 				   const FastMat2 &state_old,
 				   const FastMat2 &state_new,
 				   FastMat2 &res,FastMat2 &mat){
-  B.reshape(3,ntens,nel,ndof);
 
+  B.reshape(3,ntens,nel,ndof);
+  
+  // Levi-Civita tensor generation
+  epsilon_LC.eps_LC();
+  double J1,J2,coef;
+  
   // loop over Gauss points
   for (int ipg=0; ipg<npg; ipg++) {
     
-    // x_new.set(xloc).add(state_new);
     x_new.set(xloc);
+    //**    x_new.set(xloc).add(state_new);
     dshapexi.ir(3,ipg+1); // restriccion del indice 3 a ipg+1
-    // Jaco.prod(dshapexi,xloc,1,-1,-1,2);
+    //x_def.set(xloc).add(state_old);
+    x_def.set(xloc).add(state_new);
+    Jaco_def.prod(dshapexi,x_def,1,-1,-1,2);
     Jaco.prod(dshapexi,x_new,1,-1,-1,2);
     G.prod(Jaco,Jaco,-1,1,-1,2);
     my_fun2.apply(G,fG);
     
     double detJaco = Jaco.det();
+    double detJaco_def = Jaco_def.det();
+    //**    if (detJaco_0<=0.) {
     if (detJaco<=0.) {
       detj_error(detJaco,elem);
       set_error(1);
     }
+    detJaco_def = (detJaco_def>detJaco_min ? detJaco_def : detJaco_min);
     double wpgdet = detJaco*wpg.get(ipg+1);
+    wpgdet = wpgdet*pow(detJaco/detJaco_def,Jaco_pow);
     iJaco.inv(Jaco);
     dshapex.prod(iJaco,dshapexi,1,-1,-1,2);
     dshapex_scaled.prod(fG,dshapex,1,-1,-1,2);
@@ -157,12 +179,76 @@ void elasticity::element_connector(const FastMat2 &xloc,
     // Residual computation
     res_pg.prod(B,stress,-1,1,2,-1);
     res.axpy(res_pg,-wpgdet);
-    
+
     // Jacobian computation
     mat_pg1.prod(C,B,1,-1,-1,2,3);
     mat_pg2.prod(B,mat_pg1,-1,1,2,-1,3,4);
     mat.axpy(mat_pg2,wpgdet);
     
+    dJaco.set(0.);
+    if (ndim==2) {
+      J1 = Jaco_def.get(2,2);
+      dshapexi.ir(1,1);
+      tmp_elast.set(dshapexi).scale(J1);
+      dJaco.ir(2,1).set(tmp_elast).rs();
+
+      J1 = Jaco_def.get(1,2);
+      dshapexi.ir(1,2);
+      tmp_elast.set(dshapexi).scale(-J1);
+      dJaco.ir(2,1).add(tmp_elast).rs();
+
+      J1 = Jaco_def.get(1,1);
+      dshapexi.ir(1,2);
+      tmp_elast.set(dshapexi).scale(J1);
+      dJaco.ir(2,2).set(tmp_elast).rs();
+
+      J1 = Jaco_def.get(2,1);
+      dshapexi.ir(1,1);
+      tmp_elast.set(dshapexi).scale(-J1);
+      dJaco.ir(2,2).add(tmp_elast).rs();
+      
+    } else if (ndim==3) {
+      for (int p=1; p<=3; p++) {
+	for (int q=1; q<=3; q++) {
+	  for (int r=1; r<=3; r++) {
+	    //epsilon_LC.ir(1,p).ir(2,q).ir(3,r);
+	    coef = epsilon_LC.get(p,q,r);
+	    // first term
+	    J1 = Jaco_def.get(q,2);
+	    J2 = Jaco_def.get(r,3);
+	    dshapexi.ir(1,p);
+	    //tmp_elast.set(dshapexi).scale(J1*J2*double(epsilon_LC));
+	    tmp_elast.set(dshapexi).scale(J1*J2*coef);
+            dJaco.ir(2,1).add(tmp_elast).rs();
+
+	    // second term
+	    J1 = Jaco_def.get(p,1);
+	    J2 = Jaco_def.get(r,3);
+	    dshapexi.ir(1,q);
+	    //	    tmp_elast.set(dshapexi).scale(J1*J2*double(epsilon_LC));
+	    tmp_elast.set(dshapexi).scale(J1*J2*coef);
+	    dJaco.ir(2,2).add(tmp_elast).rs();
+
+	    // third term
+	    J1 = Jaco_def.get(p,1);
+	    J2 = Jaco_def.get(q,2);
+	    dshapexi.ir(1,r);
+	    //	    tmp_elast.set(dshapexi).scale(J1*J2*double(epsilon_LC));
+	    tmp_elast.set(dshapexi).scale(J1*J2*coef);
+	    dJaco.ir(2,3).add(tmp_elast).rs();
+	    
+	    //	    epsilon_LC.rs();
+
+	  }
+	}
+      }
+      //      dshapexi.rs();
+    }
+    dshapexi.rs();
+
+    mat_pg3.prod(res_pg,dJaco,1,2,3,4).scale(-Jaco_pow/detJaco_def);
+    mat.axpy(mat_pg3,wpgdet);
   }
     
 }
+
--- applications/ns/elast.h
+++ applications/ns/elast.h
@@ -1,6 +1,6 @@
 // -*- mode: C++ -*-
 /*__INSERT_LICENSE__*/
-//$Id: elast.h,v 1.7 2004/12/07 19:28:24 mstorti Exp $
+//$Id: elast.h,v 1.7.30.1 2005/05/23 18:36:26 mstorti Exp $
 
 #ifndef ELASTICITY_H
 #define ELASTICITY_H
@@ -13,6 +13,8 @@ public:
   int ntens,nen;
   FastMat2 B,C,Jaco,iJaco,strain,stress,res_pg,mat_pg1,mat_pg2,
     x_new,G,fG,dshapex_scaled,G_body;
+  FastMat2 x_def,Jaco_def,dJaco,tmp_elast,mat_pg3,epsilon_LC;
+  double detJaco_min, Jaco_pow;
   void init();
   void element_connector(const FastMat2 &xloc,
 			 const FastMat2 &state_old,
