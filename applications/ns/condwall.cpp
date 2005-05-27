3b337fccdd7186a0ffe56e662649672e4d6c6338
mstorti <mstorti>
2005-05-27 02:11:26 +0000

    *** empty log message ***

--- applications/ns/condwall.cpp
+++ applications/ns/condwall.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-// $Id: condwall.cpp,v 1.14 2005/05/19 16:45:51 mstorti Exp $
+// $Id: condwall.cpp,v 1.15 2005/05/27 02:11:26 mstorti Exp $
 
 #include "./condwall.h"
 extern int MY_RANK,SIZE;
@@ -64,48 +64,38 @@ res(int k,FastMat2 &U,FastMat2 &r,
     // printf("[%d] elem %d R %f\n",MY_RANK,k,R);
     // printf("k %d, R %f\n",k,R);
   }
-  if (R>0) {
-    u1.set(0.0);
-    u2.set(0.0);
-    if (data_p && data_p->u1.size()>0) {
-      if (k==0) {
-	assert(data_p->u1.size()==ndim*size());
-	assert(data_p->u2.size()==ndim*size());
-      }
-      u1.set(&data_p->u1.e(k,0));
-      u2.set(&data_p->u2.e(k,0));
-      // printf("k %d, v1 %f, v2 %f\n",k,u1.get(2),u2.get(2));
+  double p1 = U1.get(ndof);
+  double p2 = U2.get(ndof);
+  double dp_fac;
+  // Restricted to flux parallel to x axis, so far
+  u1.set(0.0).setel(1.0,1).scale((p1-p2)/R);
+  if (0 && data_p && data_p->u1.size()>0) {
+    if (k==0) {
+      assert(data_p->u1.size()==ndim*size());
+      assert(data_p->u2.size()==ndim*size());
     }
-    // Closed
-    r.set(0.0);
-    U1.is(1,1,ndim);
-    r.is(1,1,ndim).set(U1).rest(u1).rs();
-    U1.rs();
-    U2.is(1,1,ndim);
-    r.rs().is(1,ndof+1,ndof+ndim)
-      .set(U2).rest(u2).rs();
-    U2.rs();
-
-    w.set(0.);
-    w.is(2,1,ndim).ir(1,1).is(3,1,ndim).eye().rs();
-    w.is(2,1,ndim).ir(1,2).is(3,ndof+1,ndof+ndim).eye().rs();
-
-    jac.set(0.).is(1,1,ndim).ir(2,1)
-      .is(3,1,ndim).eye().rs();
-    jac.is(1,ndof+1,ndof+ndim).ir(2,2)
-      .is(3,1,ndim).eye().rs();
-  } else {
-    // Open
-    r.set(0.).is(1,1,ndof).set(U1).rest(U2)
-      .rs().scale(alpha);
-    w.set(0.).is(3,1,ndof)
-      .ir(1,1).eye()
-      .ir(1,2).eye(-1.0).rs();
-    jac.set(0.)
-      .is(1,1,ndof).ir(2,1).eye()
-      .ir(2,2).eye(-1.)
-      .rs().scale(alpha);
+    u1.set(&data_p->u1.e(k,0));
+    u2.set(&data_p->u2.e(k,0));
+    // printf("k %d, v1 %f, v2 %f\n",k,u1.get(2),u2.get(2));
   }
+  // Closed
+  r.set(0.0);
+  U1.is(1,1,ndim);
+  r.is(1,1,ndim).set(U1).rest(u1).rs();
+  U1.rs();
+  U2.is(1,1,ndim);
+  r.rs().is(1,ndof+1,ndof+ndim)
+    .set(U2).rest(u1).rs();
+  U2.rs();
+
+  w.set(0.);
+  w.is(2,1,ndim).ir(1,1).is(3,1,ndim).eye().rs();
+  w.is(2,1,ndim).ir(1,2).is(3,ndof+1,ndof+ndim).eye().rs();
+
+  jac.set(0.).is(1,1,ndim).ir(2,1)
+    .is(3,1,ndim).eye().rs();
+  jac.is(1,ndof+1,ndof+ndim).ir(2,2)
+    .is(3,1,ndim).eye().rs();
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
