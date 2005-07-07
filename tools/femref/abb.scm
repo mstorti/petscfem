898b89d8403c139da390066a5759ad4daa069da2
mstorti <mstorti>
2005-07-07 02:53:30 +0000

    *** empty log message ***

--- tools/femref/abb.scm
+++ tools/femref/abb.scm
@@ -44,12 +44,41 @@
     (cond ((= j n) q)
 	  (else (loop (cons (random m) q) (+ j 1))))))
 
+;; returns bool min max
+(define (abb?-aux a)
+  (cond ((null? a) (values #t #f #f))
+	(else (let ((root (car a))
+		    (left (cadr a))
+		    (right (caddr a)))
+		(receive (isl minl maxl) (abb?-aux left)
+;			 (format #t "left ~A, isl ~A, minl ~A, maxl ~A\n"
+;				 left isl minl maxl)
+			 (cond ((not isl) (values #f #f #f))
+			       ((and maxl (> maxl root)) 
+;				(format #t "maxl ~A, root ~A\n" maxl root)
+				(values #f #f #f))
+			       (else 
+				(receive (isr minr maxr) (abb?-aux right)
+;					 (format #t "right ~A, isr ~A, minr ~A, maxr ~A\n"
+;						 right isr minr maxr)
+					 (cond ((not isr) (values #f #f #f))
+					       ((and minr (< minr root)) (values #f #f #f))
+					       (else 
+;						(format #t "abb ~A, returns ~A ~A ~A\n" 
+;							a #t minl maxr)
+						(values #t 
+							(cond (minl minl)
+							      (else root))
+							(cond (maxr maxr)
+							      (else root)))))))))))))
+
 (define (abb? a)
   (cond ((null? a) #t)
 	(else (let ((root (car a))
 		    (left (cadr a))
 		    (right (caddr a)))
-		(cond ((and (not (null? left)) (< (car left) root)) #f)
+		(cond ((and (not (null? left)) (< (car left) root)) 
+		       #f)
 		      ((and (not (null? right)) (< (car right) root)) #f)
 		      (else #t))))))
 
