3b86a6be465d7babd82627e3529e2fcef6cf19de
mstorti <mstorti>
2005-08-22 19:42:41 +0000

    *** empty log message ***

--- applications/advdif/advdfgth.cpp
+++ applications/advdif/advdfgth.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: advdfgth.cpp,v 1.3 2003/07/03 04:32:11 mstorti Exp $
+//$Id: advdfgth.cpp,v 1.4 2005/08/22 19:42:41 mstorti Exp $
 
 #include <src/fem.h>
 #include <src/utils.h>
@@ -21,6 +21,9 @@ void flow_rate_integrator::init() {
   assert(ndimel==ndim-1);
   assert(gather_length==1);
   Q.resize(0);
+
+  //o Give mass rate or volumetric rate
+  TGETOPTDEF(thash,int,use_mass_rate,1); 
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
@@ -29,12 +32,8 @@ void flow_rate_integrator::set_pg_values(vector<double> &pg_values,FastMat2 &u,
 				     double wpgdet,double time) {
   double rho = u.get(1); 
   u.is(1,2,ndim_m+1); 
-  Q.prod(n,u,-1,-1).scale(rho*wpgdet);
+  double w = (use_mass_rate ? rho*wpgdet : wpgdet);
+  Q.prod(n,u,-1,-1).scale(w);
   u.rs();
   Q.export_vals(&*pg_values.begin());
-#if 0
-#define SHM(m) m.print(#m ": ")
-  SHM(u);
-  SHM(Q);
-#endif
 }
--- applications/advdif/advdfgth.h
+++ applications/advdif/advdfgth.h
@@ -1,6 +1,6 @@
 // -*- mode: C++ -*-
 /*__INSERT_LICENSE__*/
-//$Id: advdfgth.h,v 1.2 2005/02/23 01:40:33 mstorti Exp $
+//$Id: advdfgth.h,v 1.3 2005/08/22 19:42:41 mstorti Exp $
 #ifndef PETSCFEM_ADVDIF_GATHERER_H
 #define PETSCFEM_ADVDIF_GATHERER_H
 
@@ -11,7 +11,7 @@
 class flow_rate_integrator : public gatherer {
 private:
   FastMat2 Q;
-  int ndim_m;
+  int ndim_m, use_mass_rate;
 public:
   /// perform several checks and initialization
   void init();
