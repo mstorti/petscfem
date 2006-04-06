4f6b39d2f7d3f6b03af1c775d4b8256c5cec7843
mstorti <mstorti>
2006-04-06 16:38:14 +0000

    *** empty log message ***

--- src/dvecpar.cpp
+++ src/dvecpar.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-// $Id: dvecpar.cpp,v 1.2 2006/04/06 16:37:44 mstorti Exp $
+// $Id: dvecpar.cpp,v 1.3 2006/04/06 16:38:14 mstorti Exp $
 
 #include <src/dvecpar.h>
 
@@ -10,10 +10,3 @@ DVECTOR_MPI_TYPE_DEF(int,INT)
 DVECTOR_MPI_TYPE_DEF(double,DOUBLE) 
 DVECTOR_MPI_TYPE_DEF(char,CHAR) 
 DVECTOR_MPI_TYPE_DEF(float,FLOAT) 
-
-#if 0
-MPI_Datatype dvector_mpi_type(int) { return MPI_INT; }
-MPI_Datatype dvector_mpi_type(double) { return MPI_DOUBLE; }
-MPI_Datatype dvector_mpi_type(char) { return MPI_CHAR; }
-MPI_Datatype dvector_mpi_type(float) { return MPI_FLOAT; }
-#endif
