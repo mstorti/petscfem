bc944eb2fa31c308739d3992e90c004838126ded
mstorti <mstorti>
2006-04-08 21:35:17 +0000

    Implements `dvector_read_parallel' for general structures using `sizeof()' and making the broadcast with MPI_CHAR type.

--- src/dvecpar.h
+++ src/dvecpar.h
@@ -1,6 +1,6 @@
 // -*- mode: C++ -*- 
 /*__INSERT_LICENSE__*/
-// $Id: dvecpar.h,v 1.3 2006/04/08 20:37:15 mstorti Exp $
+// $Id: dvecpar.h,v 1.4 2006/04/08 21:35:17 mstorti Exp $
 #ifndef PETSCFEM_DVECPAR_H
 #define PETSCFEM_DVECPAR_H
 
@@ -9,41 +9,29 @@
 #include <petsc.h>
 #include <src/dvector.h>
 
-extern int MY_RANK,SIZE;
 
-template<class T>
-class dvector_mpi_type {
-public:
-  MPI_Datatype type();
-};
-
+/** Clones a dvector object from the #root# processor to all
+    the others (like #MPI_Bcast()#). 
+    @param w (input/output) the dvector to be broadcasted. As 
+    a side effect it is defragmented in the root procesor and
+    is created as defragmented in all others.
+    @param root (input) the processor from which the dvector 
+    is boradcasted */ 
 template<class T>
 void 
-dvector_clone_parallel(dvector<T> &w,int root=0) {
-  int size = w.size();
-  MPI_Datatype t = dvector_mpi_type<T>().type();
-  int ierr = MPI_Bcast(&size,1,t,
-		       root,PETSC_COMM_WORLD);
-  assert(!ierr);
+dvector_clone_parallel(dvector<T> &w,int root=0);
 
-  if (MY_RANK!=root) w.resize(size).defrag();
-  ierr = MPI_Bcast(w.buff(), size, MPI_DOUBLE, 
-		   root,PETSC_COMM_WORLD);
-  assert(!ierr);
-}
-
+/** Reads a vector in the #root# processor and broadcasts it to 
+    all the others. The final vectors are defragmented. 
+    @param file (input) The name of the file
+    @param w (input/output) the dvector to be broadcasted. As 
+    a side effect it is defragmented in the root procesor and
+    is created as defragmented in all others.
+    @param root (input) the processor from which the dvector 
+    is boradcasted */ 
 template<class T>
 void 
 dvector_read_parallel(const char *file,
-		      dvector<T> &w,int root=0) {
-  if (MY_RANK==root) {
-    if (w.size()==0) {
-      w.cat(file).defrag();
-    } else {
-      w.read(file).defrag();
-    }
-  }
-  dvector_clone_parallel(w,root);
-}
+		      dvector<T> &w,int root=0);
 
 #endif
---
+++ src/dvecpar2.h
@@ -0,0 +1,64 @@
+// -*- mode: C++ -*- 
+/*__INSERT_LICENSE__*/
+// $Id: dvecpar2.h,v 1.1 2006/04/08 21:35:17 mstorti Exp $
+#ifndef PETSCFEM_DVECPAR2_H
+#define PETSCFEM_DVECPAR2_H
+
+#include <cassert>
+#include <mpi.h>
+#include <petsc.h>
+
+#include <src/dvector.h>
+#include <src/debug.h>
+
+extern int MY_RANK,SIZE;
+
+#define MPI_TYPE_UNDEFINED INT_MAX
+
+extern Debug *GLOBAL_DEBUG;
+
+template<class T>
+class dvector_mpi_type {
+public:
+  MPI_Datatype type() { return MPI_TYPE_UNDEFINED; }
+};
+
+template<class T>
+void 
+dvector_clone_parallel(dvector<T> &w,int root=0) {
+  int size = w.size();
+  int ierr = MPI_Bcast(&size,1,MPI_INT,
+		       root,PETSC_COMM_WORLD);
+  assert(!ierr);
+  
+  if (MY_RANK!=root) w.resize(size);
+  w.defrag();
+  MPI_Datatype t = dvector_mpi_type<T>().type();
+  GLOBAL_DEBUG->trace("en clone, antes de Bcast");
+  if (t!=MPI_TYPE_UNDEFINED) {
+    ierr = MPI_Bcast(w.buff(), size, MPI_DOUBLE, 
+		     root,PETSC_COMM_WORLD);
+    assert(!ierr);
+  } else {
+    ierr = MPI_Bcast(w.buff(), size*sizeof(T), MPI_CHAR, 
+		     root,PETSC_COMM_WORLD);
+    assert(!ierr);
+  }
+  GLOBAL_DEBUG->trace("en clone, despues de Bcast");
+}
+
+template<class T>
+void 
+dvector_read_parallel(const char *file,
+		      dvector<T> &w,int root=0) {
+  if (MY_RANK==root) {
+    if (w.size()==0) {
+      w.cat(file).defrag();
+    } else {
+      w.read(file).defrag();
+    }
+  }
+  dvector_clone_parallel(w,root);
+}
+
+#endif
