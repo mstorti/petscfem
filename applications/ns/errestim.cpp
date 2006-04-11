04926f3ff67a9563c430be19870a67eb70d6d0d6
mstorti <mstorti>
2006-04-11 01:31:00 +0000

    Writing `error_estimator' elemset.

--- applications/ns/errestim.cpp
+++ applications/ns/errestim.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: errestim.cpp,v 1.2 2006/04/11 00:48:10 mstorti Exp $
+//$Id: errestim.cpp,v 1.3 2006/04/11 01:31:00 mstorti Exp $
 
 #include <src/fem.h>
 #include <src/utils.h>
@@ -20,6 +20,7 @@ elemset_init() {
   TGETOPTDEF_ND(thash,double,norm_expo,2.);
   tmp.resize(1,ndof);
   du.resize(1,ndof);
+  G.resize(1,ndof);
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
@@ -32,12 +33,17 @@ pg_connector(const FastMat2 &xpg,
 	     FastMat2 &res_pg,FastMat2 &mat_pg) {
   assert(ndof==1);
   du.set(state_new_pg).rest(state_old_pg).scale(rec_Dt);
-  // tmp.set(G); // G will be the error estimator
-  tmp.print("tmp: ");
+  double g = grad_H.sum_square_all();
+  G.setel(g,1);
+  tmp.set(G); // G will be the error estimator
+  // tmp.print("tmp: ");
   tmp.rest(du);
   res_pg.prod(shape(),tmp,1,2);
   mat_pg.set(0.0);
+#if 0
   xpg.print("xpg:");
   H.print("H: ");
   grad_H.print("grad_H: ");
+#endif
+  
 }
--- applications/ns/errestim.h
+++ applications/ns/errestim.h
@@ -1,13 +1,13 @@
 // -*- mode: C++ -*- 
 /*__INSERT_LICENSE__*/
-// $Id: errestim.h,v 1.2 2006/04/11 00:48:10 mstorti Exp $
+// $Id: errestim.h,v 1.3 2006/04/11 01:31:00 mstorti Exp $
 #ifndef ERRESTIM_H
 #define ERRESTIM_H
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 class error_estimator : public adaptor_pg {
   double norm_expo;
-  FastMat2 du, tmp;
+  FastMat2 du, tmp, G;
 public:
   void elemset_init();
   void pg_connector(const FastMat2 &xpg,
