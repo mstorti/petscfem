f79d078ab471e4cf9e778c880b0a194dd9f87a85
mstorti <mstorti>
2006-04-24 01:17:38 +0000

    *** empty log message ***

--- tools/femref/hashedann.cpp
+++ tools/femref/hashedann.cpp
@@ -14,7 +14,12 @@ using namespace std;
 class hashed_coords_t {
 private:
   vector<double> coords;
-  map<unsigned int,unsigned int> table;
+  typedef 
+  multimap<unsigned int,unsigned int> map_t;
+  typedef 
+  pair<map_t::const_iterator,map_t::const_iterator> pair_iter_t;
+  typedef pair<unsigned int,unsigned int> pair_t;
+  map_t table;
   int npoints, nint;
   double tol;
   int ndim;
@@ -22,6 +27,53 @@ private:
   vector<double> bounds;
   double & bound(int ndim, int which) {
     return bounds[3*ndim+which];
+  }
+  void get_candidates(const vector<int> &intervals,
+		      const vector<int> &incs, 
+		      vector<int> &candidates) {
+    hasher.reset();
+    for (int j=0; j<ndim; j++) 
+      hasher.hash(intervals[j]+incs[j]);
+    int h = hasher.val();
+    pair_iter_t p = table.equal_range(h);
+    for (map_t::const_iterator q = p.first; q!=p.second; q++)
+      candidates.push_back(q->second);
+  }
+  void get_candidates(const double *x,
+		      vector<int> &candidates) {
+    vector<int> intervals(ndim);
+    for (int k=0; k<ndim; k++) {
+      intervals[k] = 
+	(unsigned int)(nint*(x[k]-bound(k,0))/bound(k,2));
+    }
+    vector<int> incs(ndim);
+    for (int k=0; k<ndim; k++) incs[k] = -1;
+    while (1) {
+      int ok=0;
+      for (int j=0; j<ndim; j++) {
+	if (incs[j]<1) { ok=1; incs[j]++; break; } 
+	else { incs[j]=-1; }
+      }
+      if (!ok) break;
+      get_candidates(intervals,incs,candidates);
+    }
+  }
+  void get_candidates(const vector<double> &x,
+		      vector<int> &candidates) {
+    assert(x.size() % ndim == 0);
+    int np = x.size()/ndim;
+    candidates.clear();
+    for (int j=0; j<np; j++) 
+      get_candidates(&x[j*ndim],candidates);
+  }
+  double dist(int k,const vector<double> &x) {
+    double d = 0.0;
+    for (int j=0; j<ndim; j++) {
+      double dx = coords[k*ndim+j]-x[j];
+      d += dx*dx;
+    }
+    d = sqrt(d);
+    return d;
   }
 public:
   hashed_coords_t(int ndim_a, double tol_a=NAN)
@@ -34,49 +86,62 @@ public:
       bound(j,2) = 1.0;
     }
   }
-  int add(const vector<double> &x) {
-    assert(x.size()%ndim==0);
+  int add(const vector<double> &x,double tol_a=NAN) {
+    if (isnan(tol_a)) tol_a=tol;
+    assert(tol_a<=tol);
+    assert(x.size() % ndim == 0);
     int np = x.size()/ndim;
     int ok = 0;
     for (int j=0; j<np; j++) {
+      vector<int> candidates;
+      vector<double> xx(ndim);
+      for (int k=0; k<ndim; k++) 
+	xx[k] = x[j*ndim+k];
+      get_candidates(xx,candidates);
+      int ncand = candidates.size();
+      for (int j=0; j<ncand; j++) {
+	double d = dist(candidates[j],x);
+	if (d<tol_a) continue;
+      }
+      ok++;
       hasher.reset();
       for (int k=0; k<ndim; k++) {
-	double xx = x[j*ndim+k];
-	coords.push_back(xx);
-	unsigned int w = 
-	  (unsigned int)(nint*(xx-bound(k,0))/bound(k,2));
-	// printf(" %x",w);
-	hasher.hash(w);
+	coords.push_back(xx[k]);
+	int interv = 
+	  (unsigned int)(nint*(xx[k]-bound(k,0))/bound(k,2));
+	hasher.hash(interv);
       }
-      // printf("\n");
-      int h = hasher.val();
-//       printf("adding point j %d, (%f,%f,%f), hash %x\n",
-// 	     j,x[j*ndim],x[j*ndim+1],x[j*ndim+2],h);
-      if (table.find(h)==table.end()) {
-	table[h] = npoints++;
-	ok ++;
-      } 
+      int w = hasher.val();
+      table.insert(pair_t(w,npoints++));
     }
     return ok;
   }
-
+  int size() { return npoints; }
   int get(const vector<double> &x,
-	  vector<double> &ngbrs) {
-    int np = x.size()/ndim;
-    for (int j=0; j<np; j++) {
-      hasher.reset();
-      for (int k=0; k<ndim; k++) {
-	double xx = x[j*ndim+k];
-	int w = int(nint*(xx-bound(j,0))/bound(j,2));
-	hasher.hash(w);
+	  vector<double> &ngbrs,double tol_a=NAN) {
+    if (isnan(tol_a)) tol_a=tol;
+    assert(tol_a<=tol);
+    assert(x.size()==ndim);
+    vector<int> candidates;
+    get_candidates(x,candidates);
+    int ncand = candidates.size();
+    int OK=0;
+    for (int k=0; k<ncand; k++) {
+      int c = candidates[k];
+      double d = dist(c,x);
+      if (d<=tol_a) {
+	OK++;
+	for (int j=0; j<ndim; j++) 
+	  ngbrs.push_back(coords[c*ndim+j]);
       }
-      int h = hasher.val();
-      if (table.find(h)!=table.end()) {
-	int indx = table[h];
-	for (int j=0; j<ndim; j++) {
-	  ngbrs.push_back(coords[indx*ndim+j]);
-	}
-      }
+    }
+    return OK;
+  }
+  void print() {
+    map_t::iterator q = table.begin();
+    while (q!=table.end()) {
+      printf("%d -> %d\n",q->first,q->second);
+      q++;
     }
   }
 };
@@ -88,24 +153,25 @@ double drand() {
 // Tries to solve the ANN problem (or related)
 // through hashing
 int main() {
-  int N=10000000, ndim=3;			// Number of points to be added
+  int N=10, ndim=3;			// Number of points to be added
   hashed_coords_t hashed_coords(ndim,1e-10);
   vector<double> coords;
   for (int j=0; j<N*ndim; j++)
     coords.push_back(drand());
-  int ok = hashed_coords.add(coords);
-  printf("tried %d, ok %d\n",N,ok);
+  int npoints = hashed_coords.add(coords);
+  printf("tried %d, OK %d\n",N,npoints);
   int bad=0;
+  hashed_coords.print();
   for (int j=0; j<N; j++) {
     vector<double> xtry;
     int q = rand() % 2;
     if (q) {
-      // printf("random generation\n");
+      printf("random generation\n");
       for (int j=0; j<ndim; j++) 
 	xtry.push_back(drand());
     } else {
       int k = rand()%N;
-      // printf("probing point %d\n",k);
+      printf("probing point %d\n",k);
       for (int j=0; j<ndim; j++) 
 	xtry.push_back(coords[k*ndim+j]);
     }
