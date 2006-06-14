bca1e62d8a7188286cb5390bacdb9836aaa68599
mstorti <mstorti>
2006-06-14 01:10:42 +0000

    *** empty log message ***

--- applications/advdif/advdife.cpp
+++ applications/advdif/advdife.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: advdife.cpp,v 1.113 2006/06/13 22:31:54 mstorti Exp $
+//$Id: advdife.cpp,v 1.114 2006/06/14 01:10:42 mstorti Exp $
 extern int comp_mat_each_time_step_g,
   consistent_supg_matrix_g,
   local_time_step_g;
@@ -569,16 +569,14 @@ void NewAdvDif::new_assemble(arg_data_list &arg_data_v,const Nodedata *nodedata,
     }
 
     if (0){
-      // DEBUG
       int kk,ielhh;
       element.position(kk,ielhh);
-      if((kk==264)){
+      if (rand()%1==0){
 	printf("Element %d \n",kk);
 	xloc.print(" xloc^(n+1) :");
 	Hloc.print(" Hloc^(n+1) :");
 	vloc_mesh.print(" vloc_mesh^(n+1) :");
       }
-      // END DEBUG
     }
     
     if(use_low_gpdata) {
