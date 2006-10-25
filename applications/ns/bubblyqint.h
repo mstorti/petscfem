b220db895c0af638f6913e8cc7bb9b56d540de6b
mstorti <mstorti>
2006-10-25 19:09:03 +0000

    *** empty log message ***

--- applications/ns/bubblyqint.h
+++ applications/ns/bubblyqint.h
@@ -1,13 +1,13 @@
 // -*- mode: C++ -*-
 /*__INSERT_LICENSE__*/
-//$Id: bubblyqint.h,v 1.2 2006/09/04 17:49:42 mstorti Exp $
+//$Id: bubblyqint.h,v 1.3 2006/10/25 19:09:03 mstorti Exp $
 #ifndef PETSCFEM_BUBBLYQINT_H
 #define PETSCFEM_BUBBLYQINT_H
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 class bubbly_flow_rate_integrator : public SurfGatherer {
 private:
-  FastMat2 vslip_user_vp, tmp, ;
+  FastMat2 vslip_user_vp, tmp;
   int nphases, ndim, g_dir;
 public:
   void init();
