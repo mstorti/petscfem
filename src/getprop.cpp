83524639e67367a8d20a36298e8f90a18f398dbd
mstorti <mstorti>
2007-01-31 16:03:13 +0000

    *** empty log message ***

--- src/getprop.cpp
+++ src/getprop.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: getprop.cpp,v 1.16 2007/01/30 19:03:44 mstorti Exp $
+//$Id: getprop.cpp,v 1.17 2007/01/31 16:03:13 mstorti Exp $
   
 #include "fem.h"
 #include "readmesh.h"
@@ -159,8 +159,8 @@ int get_string(const TextHashTable *thash,const char *name,
   }
 
   string value = string(value_);
-  int ierr = get_string_from_string(value,ret);
-  return ierr;
+  get_string_from_string(value,ret);
+  return 0;
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
