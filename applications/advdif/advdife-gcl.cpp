cda3a7ec01ce39249b200562b466a66fb48ba9ee
mstorti <mstorti>
2007-02-06 16:35:07 +0000

    *** empty log message ***

--- applications/advdif/advdife-gcl.cpp
+++ applications/advdif/advdife-gcl.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: advdife-gcl.cpp,v 1.12 2007/02/04 14:10:09 mstorti Exp $
+//$Id: advdife-gcl.cpp,v 1.12.2.1 2007/02/06 16:35:07 mstorti Exp $
 extern int comp_mat_each_time_step_g,
   consistent_supg_matrix_g,
   local_time_step_g;
@@ -853,7 +853,7 @@ new_assemble_GCL_compliant(arg_data_list &arg_data_v,const Nodedata *nodedata,
 	if (!lumped_mass) {
 	  adv_diff_ff->enthalpy_fun
 	    ->comp_W_Cp_N(N_Cp_N,SHAPE,SHAPE,
-			  detJaco_new*WPG*rec_Dt_m);
+			  0*detJaco_new*WPG*rec_Dt_m);
 	  matlocf.add(N_Cp_N);
 	}
 
