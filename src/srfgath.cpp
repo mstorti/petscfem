7a7fd25c27eabc85c925046bb3c89174885c7e44
mstorti <mstorti>
2007-02-19 17:59:20 +0000

    *** empty log message ***

--- src/srfgath.cpp
+++ src/srfgath.cpp
@@ -1,5 +1,5 @@
 //__INSERT_LICENSE__
-//$Id: srfgath.cpp,v 1.16 2007/01/30 19:03:44 mstorti Exp $
+//$Id: srfgath.cpp,v 1.16.8.1 2007/02/19 17:59:24 mstorti Exp $
 
 #include <src/fem.h>
 #include <src/utils.h>
@@ -258,7 +258,7 @@ int SurfGatherer::assemble(arg_data_list &arg_data_v,Nodedata *nodedata,
 
   int ja = 0;
   double *locst = arg_data_v[ja++].locst;
-  //double *locst2 = arg_data_v[ja++].locst;
+  double *locst2 = arg_data_v[ja++].locst;
   //int options = arg_data_v[ja].options;
   vector<double> *values = arg_data_v[ja++].vector_assoc;
   int nvalues = values->size();
