445cd3701778988bb568921cfcc76d1208391ed9
mstorti <mstorti>
2007-03-13 02:40:21 +0000

    *** empty log message ***

---
+++ applications/ns/vand.h
@@ -0,0 +1,12 @@
+// -*- mode: c++ -*-
+//__INSERT_LICENSE__
+// $Id: vand.h,v 1.1.4.1 2007/03/13 02:40:21 mstorti Exp $
+#ifndef PETSCFEM_VAND_H
+#define PETSCFEM_VAND_H
+
+extern int vd_dump_flag;
+extern dvector<int> vd_elems_loc,vd_ebuff,vd_elems;
+extern dvector<double> vd_data_loc,vd_dbuff,vd_data;
+#define VD_DATA_SIZE 4
+
+#endif
