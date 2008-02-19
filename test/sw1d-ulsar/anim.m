990f1243304d6614686a82032b6fcbeec028a17e
Rodrigo R. PAZ <rodrigop@intec.unl.edu.ar>
2008-02-19 10:40:20 -0200

    *** empty log message ***

--- test/sw1d-ulsar/anim.m
+++ test/sw1d-ulsar/anim.m
@@ -20,27 +20,3 @@ for i=0:1000
   end
   pause;
 end
-if (0)
-  for i=0:1
-    i
-    fil=strcat({'STEPS/plano.'},int2str(i),{'.tmp'});
-    u=load(char(fil));
-    pdesurf([x y]', con', u(1:nnod,field));
-				%  axis([0 1 0 0.20 0.1 0.9]);%view(2);
-    view([0.7133    0.7009   -0.0000   -0.7071;
-	  -0.4315    0.4391    0.7880   -0.3978;
-	  -0.5523    0.5620   -0.6157    8.9632;
-          0         0         0    1.0000]);
-				%  caxis([0.41 0.55]);
-    caxis([0. 0.15]);
-    colormap('default');
-    colorbar;
-    anot=char(strcat({'real time: '},num2str(i*dt),{' secs. '}));
-    text(0,0,0.3,anot);
-    title('Hidraulic head vs time');
-    xlabel('x [m]'); ylabel('y [m]'); zlabel('height [m]');
-    box
-    fig=char(strcat({'YUV/plano.'},int2str(i),{'.tiff'}));
-    print('-dtiff',fig);
-  end
-end
