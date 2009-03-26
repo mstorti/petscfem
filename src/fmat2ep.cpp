ff4e249d6865ab4a64e44255cfbb1cc69fbca813
Mario Storti <mario.storti@gmail.com>
2009-03-26 20:01:11 -0300

    FastMat2+OpenMP: reverted changes, will test on a separate code

--- src/fmat2ep.cpp
+++ src/fmat2ep.cpp
@@ -5,7 +5,6 @@
 //$Id merge-with-petsc-233-50-g0ace95e Fri Oct 19 17:49:52 2007 -0300$
 #include <math.h>
 #include <stdio.h>
-#include <omp.h>
 
 #include <src/fem.h>
 #include <src/fastmat2.h>
@@ -507,8 +506,6 @@ FastMat2 & FastMat2::__NAME__(__FUN_ARGS__) {
 _//>
 //< in_place_all(); //>
 
-extern vector<int> fastmat2_pocessed_rows;
-
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>---: 
 //<$prod = <<'//EOF';
 FastMat2 & FastMat2::prod(const FastMat2 & A,const FastMat2 & B,
@@ -697,11 +694,8 @@ FastMat2 & FastMat2::prod(const FastMat2 & A,const FastMat2 & B,
   int 
     nlines = cache->nlines,
     mm=cache->line_size;
-#pragma omp parallel for schedule(dynamic,5)
+  double **pa,**pb,**pa_end,sum,*paa,*pbb,*paa_end;
   for (int j=0; j<nlines; j++) {
-    int tid = omp_get_thread_num();
-    fastmat2_pocessed_rows[tid]++;
-    double **pa,**pb,**pa_end,sum,*paa,*pbb,*paa_end;
     LineCache *lc = cache->line_cache_start+j;
     pa = lc->starta;
     pb = lc->startb;
