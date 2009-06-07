583d1eab513d0da5cb0577dcd123e7ff87846414
Mario Storti <mario.storti@gmail.com>
2009-06-07 19:47:16 -0300

    Adding test for conv with analytuc jacobian in burgers

--- test/burgers/jacotest/data.pl
+++ test/burgers/jacotest/data.pl
@@ -3,6 +3,8 @@ require "math.pl";
 
 $check_jac = 1 unless defined $check_jac;
 $N = ($check_jac? 1 : 50);
+$steady = ($check_jac? 0 : 1);
+$diffusivity = ($check_jac? 0.01 : 0.1);
 $L=1;
 
 @vars = qw(N L);
