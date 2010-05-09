602f43f7be7b5a082a4267c2a4a88df40d8af1cc
Mario Storti <mario.storti@gmail.com>
2010-05-08 21:13:20 -0300

    Writing FastMat2::prod(a,b,c,...) (Multiprod)

--- src/fastmat2c.cpp
+++ src/fastmat2c.cpp
@@ -30,7 +30,7 @@
 #include "./mproddef.h"
 
 #define OLD 0
-#define NEW 1
+#define TMP 1
 #define UNKNOWN 2
 
 #define INACTIVE 0
@@ -38,11 +38,33 @@
 #define UNDEF -1
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
+// We store a vector of these structures in the cache, and
+// then `make_prod()' has the stored the order
+// in which the products must be done, and which matrices
+// are involved.
+// The `mat_info's are stored for both the
+// original matrices, and the temporaries that
+// are created. So if we have `n' matrices we make
+// `n-1' products, and we need a mat_info for the result
+// so we have `2*n-1' mat_infos. 
+// From the `n-1' results, the first `n-2' are temporaries,
+// and the last one goes to the final output result. 
 struct mat_info {
   // Pointers to old matrices should be `const'
   FastMat2 *Ap;
+  // The vector that indicates the contractions
+  // to be performed (the args to the low-level
+  // prod())
   vector<int> contract;
+  // The dims of the involved matrices
   vector<int> dims;
+  // type: may be OLD, TMP or UNKNOWN
+  // is_active: when we make a product the two involved
+  //            matrices are marked as INACTIVE and the new
+  //            inserted to ACTIVE
+  // position: stores the position in the matrix list.
+  //           We try to preserve the position so that
+  //           the process is more clear. 
   int type, is_active, position;
   mat_info() : Ap(NULL), 
                type(UNKNOWN),
@@ -50,6 +72,7 @@ struct mat_info {
 };
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
+// Searches for `x' in vector `v'
 static int vfind(vector<int> &v,int x) {
   int n=v.size();
   for (int j=0; j<n; j++)
@@ -58,6 +81,8 @@ static int vfind(vector<int> &v,int x) {
 }
 
 #if 0
+//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
+// Auxiliary matrix to print th information for the product
 static void 
 print_mat_info(mat_info_cont_t::iterator q,
                const char *s=NULL) {
@@ -77,35 +102,45 @@ print_mat_info(mat_info_cont_t::iterator q,
 #endif
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
+// Computes and returns the operation count for a
+// possible product. Also returns the product of the free
+// indices in q and r, and of the contracted indices.
+// (BTW the opcount is qfree*rfree*
 static int compute_opcount(mat_info &qmi,mat_info &rmi,
-                           int &qfree,int &rfree,int &qr1) {
+                           int &qfree,int &rfree,int &qctr) {
   vector<int> 
     &qc = qmi.contract,
     &qd = qmi.dims,
     &rc = rmi.contract,
     &rd = rmi.dims;
   int 
-    qr2=1,
+    rctr=1,
     qrank = qd.size(),
     rrank = rd.size(),
     k,dim;
   rfree=1;
   qfree=1;
-  qr1=1;
+  qctr=1;
   for (int j=0; j<qrank; j++) {
     k = qc[j];
     dim = qd[j];
+    // The free indices are those that
+    // are positive or negative but they are
+    // not in the other matrix. There may be negative
+    // indices because they are contracted in other
+    // product.
     if (k>0 || !vfind(rc,k)) qfree *= dim;
-    else qr1 *= dim;
+    else qctr *= dim;
   }
   for (int j=0; j<rrank; j++) {
     k = rc[j];
     dim = rd[j];
+    // Same as before
     if (k>0 || !vfind(qc,k)) rfree *= dim;
-    else qr2 *= dim;
+    else rctr *= dim;
   }
-  assert(qr1==qr2);
-  return qfree*rfree*qr1;
+  assert(qctr==rctr);
+  return qfree*rfree*qctr;
 }
 
 // Just for debugging makes the first
@@ -113,37 +148,55 @@ static int compute_opcount(mat_info &qmi,mat_info &rmi,
 int fastmat_multiprod_use_first=0;
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
+// This is the cache for the product operation
 class multiprod_subcache_t : public FastMatSubCache {
 public:
+  // Number of matrices involved in this product.
+  // Must be >=2
   int nmat;
+  // A vector of structures containing information
+  // for each involved matrix (including temporaries)
   vector<mat_info> mat_info_cont;
+  // A table that stores in which orders must peformed
+  // the products
   vector<int> order;
   multiprod_subcache_t(FastMatCache *cache_a) { }
   ~multiprod_subcache_t();
+  // This makes the product when cached
   void make_prod();
 };
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
 multiprod_subcache_t::~multiprod_subcache_t() {
+  // Deallocates the temporary matrices
   int n = mat_info_cont.size();
   for (int j=0; j<n; j++) {
     mat_info &mi = mat_info_cont[j];
-    if (mi.type == NEW && mi.Ap) delete mi.Ap;
+    if (mi.type == TMP && mi.Ap) delete mi.Ap;
   }
 }
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
 void multiprod_subcache_t::make_prod() {
+  // The `order' table contained for each product
+  // 3 integers: the destination matrix, and the 2
+  // input matrix
   int nprod = nmat-1,
     qkey, rkey, skey;
   char line[100];
   for (int j=0; j<nprod; j++) {
     mat_info 
+      // Destination matrices
       &smi = mat_info_cont[order[3*j]],
+      // Source matrices
       &qmi = mat_info_cont[order[3*j+1]],
       &rmi = mat_info_cont[order[3*j+2]];
+
+    // Makes the product, calling prod(a,b,indxa,indxb)
     smi.Ap->prod(*qmi.Ap,*rmi.Ap,qmi.contract,rmi.contract);
 
+    //#define VERBOSE
+#ifdef VERBOSE
     sprintf(line,"q[%d]: ",order[3*j+1]);
     qmi.Ap->print(line);
 
@@ -152,27 +205,22 @@ void multiprod_subcache_t::make_prod() {
 
     sprintf(line,"s[%d]: ",order[3*j]);
     smi.Ap->print(line);
+#endif
   }
 }
 
-#if 0
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
-struct compare_position_t : 
-  public binary_function<double, double, bool> {
-  static vector<mat_info> *mat_info_cont_p;
-  bool operator()(double j, double k) { 
-    return (*mat_info_cont_p)[j].position 
-      < (*mat_info_cont_p)[k].position;
-  }
-};
-
-vector<mat_info> *compare_position_t::mat_info_cont_p = NULL;
-#endif
-
-//---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
+// We keep a set of this pairs for the active matrices.
+// Each pair contains the key (an index in `mat_info_cont'
+// and the position in the active matrix set).
+// This last one should be irrelevant and is kept only
+// in order to make the computation more readable.
+// Also could have an impact in performance because
+// the product may be done with `dgemm' or not. 
 class active_mat_info_t {
 public:
   int key,position;
+  // Comparison function
   bool operator<(const active_mat_info_t y) const {
     if (position != y.position) 
       return position < y.position;
@@ -184,7 +232,11 @@ public:
 };
 
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*>
-// General case
+// General case. Receives a list of pointers to matrices
+// and a vector of indices. The instances for 2,3,4,... matrices
+// are wrappers to this. In turn this routine only computes the
+// best order to do the products and calls the version
+// for two matrices
 FastMat2 & 
 FastMat2::prod(vector<const FastMat2 *> &mat_list,
                Indx &indx) {
@@ -217,7 +269,8 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
       nindx += mat_list[j]->n();
     PETSCFEM_ASSERT(nindx==indx.size(),"Not correct nbr of indices. "
                     "Sum of ranks %d. nbr of indices %d",nindx,indx.size());  
-    // For contracted indices (negeative) count how many times
+
+    // For contracted indices (negative) count how many times
     // they appear (should be 2).
     // For the free indices they should appear once and
     // in the range [1,rank] where rank is the
@@ -263,20 +316,29 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
     vector<mat_info> &mat_info_cont = mpsc->mat_info_cont;
     mat_info_cont.resize(nmat);
     vector<int> &order = mpsc->order;
+    assert(order.empty());
      
+    // This stores the information for the active matrices
     typedef std::set<active_mat_info_t> active_mat_info_cont_t;
     active_mat_info_cont_t active_mat_info_cont;
+
+    // Load the input matrices in the container
     int j=0;
     for (int k=0; k<nmat; k++) {
-      FastMat2 &A = *(FastMat2 *)mat_list[k];
-      mat_info &m = mat_info_cont[k];
+      // Insert in the active list
       active_mat_info_cont
         .insert(active_mat_info_t(k,k));
+
+      // Pointer to the matrix
+      FastMat2 &A = *(FastMat2 *)mat_list[k];
+      mat_info &m = mat_info_cont[k];
       m.Ap = &A;
       m.type = OLD;
       m.position = k;
       int rank = A.n();
+      // This stores the indices to be contracted
       m.contract.resize(rank);
+      // This stores the dims
       m.dims.resize(rank);
       for (int l=0; l<rank; l++) {
         m.contract[l] = indx[j++];
@@ -284,17 +346,32 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
       }
     }
 
+    // The created temporaries are assigned
+    // this key (position in `mat_info_cont')
     int new_mat_indx = nmat;
+    // Each time we run over all pairs of active matrices
+    // and find which one has the lower op. count.
+    // q,r, iterate and qmin,rmin store the lowest combination
     active_mat_info_cont_t
       ::iterator q,r,qmin,rmin;
+    // nbr of active matrices
     int nact;
     int qfree,rfree,qr1,qr2,nopsmin,nops,
       qrank,rrank,k,dim,qkey,rkey,nopscount=0;
+#ifdef VERBOSE
     printf("fastmat_multiprod_use_first %d\n",
            fastmat_multiprod_use_first);
+#endif
     while (1) {
+      // For each execution of this loop we
+      // look for the lowest opcount. The the
+      // two corresponding matrices become inactive
+      // and the new temporary is created. 
+
+      // Nbr of current active matrices
       nact = active_mat_info_cont.size();
-#if 1
+
+#ifdef VERBOSE
       // Print current matrices 
       q = active_mat_info_cont.begin();
       while (q!=active_mat_info_cont.end()) {
@@ -313,27 +390,37 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
 #endif
       if (active_mat_info_cont.size()<2) break;
 
-      printf("nbr of matrices %zu\n",active_mat_info_cont.size());
+      // printf("nbr of matrices %zu\n",active_mat_info_cont.size());
 
       // search for the product with lowest number
       // of operations
+      // q,r, iterate, qmin,rmin keep the lowest combination
       q = active_mat_info_cont.begin();
       nopsmin=-1;
       while (q != active_mat_info_cont.end()) {
+        // we identify the matrix by his position
+        // in `mat_info_cont' (the `key')
         qkey = q->key;
         mat_info &qmi = mat_info_cont[qkey];
         r = q; r++;
         while (r != active_mat_info_cont.end()) {
+          // Same for `r'
           rkey = r->key;
           mat_info &rmi = mat_info_cont[rkey];
-#if 0
+#ifdef VERBOSE 
           print_mat_info(q,"q: ");
           print_mat_info(r,"r: ");
 
           printf("qfree %d, rfree %d, qr1 %d, qr2 %d \n",
                  qfree,rfree,qr1,qr2);
 #endif        
+          // Computes the number of operations
           nops = compute_opcount(qmi,rmi,qfree,rfree,qr1);
+          // `fastmat_multiprod_use_first' is just for debugging.
+          // If activated it correspond to do the first product
+          // that does a contraction.
+          // Beware it may fail. in case there is no contraction!!
+          // FOr instance a.prod(b,c,1,2);
           if (fastmat_multiprod_use_first?
               qr1>1 
               : (nopsmin<0 || nops<nopsmin)) {
@@ -348,34 +435,54 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
         if (fastmat_multiprod_use_first && nopsmin>0) break;
       }
       assert(nopsmin>0);
+      // Keeps the total number of operations performed
       nopscount += nopsmin;
 
+      // Insert a new `mat_info' corresponding to
+      // the temporary to be created
       mat_info_cont.push_back(mat_info());
-      // Insert new entry in 
       int skey = new_mat_indx++;
       mat_info &smi = mat_info_cont[skey];
+      // For the firs products except the last one
+      // we reorder the free indices
+      // so that they are in range [1,n].
+      // For the last one this is not needed because
+      // it should end in this way automatically.
+      // And the order should be preserved becuase
+      // otherwise the elements in the resulting
+      // matrix would be in an order not desired. 
       int reo=1;
       if (nact>2) {
+        // This is for all the products except the last one
         smi.Ap = new FastMat2;
-        smi.type = NEW;
+        smi.type = TMP;
       } else {
+        // In this case the output matrix is *this
         smi.Ap = this;
+        // It is NOT a temporary
         smi.type = OLD;
+        // Do not reorder the indices
         reo = 0;
       }
+      // This new matrix is active
       smi.is_active = ACTIVE;
       vector<int> 
         &sc = smi.contract,
         &sd = smi.dims;
 
+      // Check that the active matrices are ordered FIRST
+      // by position 
       assert(qmin->position<=rmin->position);
 
+      // Product to be done is S = Q * R
+      // get info for Q matrix
       mat_info &qmi = mat_info_cont[qmin->key];
       qmi.is_active = INACTIVE;
       vector<int> 
         &qc = qmi.contract,
         &qd = qmi.dims;
 
+      // get info for R matrix
       mat_info &rmi = mat_info_cont[rmin->key];
       rmi.is_active = INACTIVE;
       vector<int> 
@@ -385,6 +492,7 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
       qrank = qd.size();
       rrank = rd.size();
       int sindx = 1;
+      // Compute the new contraction indices for Q
       for (int j=0; j<qrank; j++) {
         k = qc[j];
         dim = qd[j];
@@ -394,6 +502,7 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
           sd.push_back(dim);
         }
       }
+      // Compute the new contraction indices for R
       for (int j=0; j<rrank; j++) {
         k = rc[j];
         dim = rd[j];
@@ -403,20 +512,28 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
           sd.push_back(dim);
         }
       }
+      // Insert the `keys' for this product in `order'
       order.push_back(skey);
       smi.position = qmi.position;
       order.push_back(qmin->key);
       order.push_back(rmin->key);
-
+      
+      // Mark the Q and R matrices as inactive,
+      // and the result S as active
       active_mat_info_cont.erase(qmin);
       active_mat_info_cont.erase(rmin);
       active_mat_info_cont
         .insert(active_mat_info_t(skey,smi.position));
 
+#ifdef VERBOSE
       int n=order.size();
       printf("contracts a%d,a%d\n",qmin->key,rmin->key);
+#endif
     }
+
+#ifdef VERBOSE
     printf("total ops count %d\n",nopscount);
+#endif
     assert(order.size() == (unsigned int)(3*(nmat-1)));
 
     // Now for each product to be done checks that
@@ -430,7 +547,11 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
         &qc = qmi.contract,
         &rc = rmi.contract;
 
+      // new_neg maps the `old' contraction indices
+      // to the `new' indices
       map<int,int> new_neg;
+      // this is the next new negative indices to
+      // be assigned
       int new_neg_free=-1;
       int k,
         qrank = qc.size(),
@@ -438,11 +559,16 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
       for (int l=0; l<qrank; l++) {
         k = qc[l];
         if (k<0) {
+          // If another negative index appears
+          // we assign a new index
           if (new_neg.find(k)==new_neg.end()) 
             new_neg[k] = new_neg_free--;
+          // replace the old indices with the
+          // new indices
           qc[l] = new_neg[k];
         }
       }
+      // Same for the q indices
       for (int l=0; l<rrank; l++) {
         k = rc[l];
         if (k<0) {
@@ -451,7 +577,7 @@ FastMat2::prod(vector<const FastMat2 *> &mat_list,
           rc[l] = new_neg[k];
         }
       }
-#if 1
+#ifdef VERBOSE
       printf("prod #%d [qc:",j);
       for (int l=0; l<qrank; l++) 
         printf("%d,",qc[l]);
