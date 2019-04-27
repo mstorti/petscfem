49fa605bd9d2feacbb55675caea827f9c5e9fd5c
Mario Storti (@galileo) <mario.storti@gmail.com>
2019-04-27 18:20:06 +0200

    CHIMERA-ADV: Cleaning MMVFORCE hook and CHIMERA stuff

--- applications/advdif/mmvforce.cpp
+++ applications/advdif/mmvforce.cpp
@@ -29,20 +29,39 @@ mmv_force_hook_t::mmv_force_hook_t() : coords_buff(NULL) { }
 //---:---<*>---:---<*>---:---<*>---:---<*>---:---<*> 
 void mmv_force_hook_t::init(Mesh &mesh_a,Dofmap &dofmap_a,
 		 const char *name) {
+  // Get the parameters of the mesh
   int ierr;
+  // Dimension of the problem. 
   TGETOPTDEF_ND(GLOBAL_OPTIONS,int,ndim,0);
   assert(ndim>0);
 
+  // Save the mesh coordinates each NSAVEROT steps
   TGETOPTDEF_ND(GLOBAL_OPTIONS,int,nsaverot,0);
+  // Use XALEPATTERN template to build the name for the
+  // coordinates save file. It must contain a "%d" part to
+  // store the frame.
   TGETOPTDEF_S_ND(GLOBAL_OPTIONS,string,xalepattern,"");
-  
+
+  // Internal pointer to the coordinates
   coords_buff = mesh_a.nodedata->nodedata;
   nnod = mesh_a.nodedata->nnod;
   nu = mesh_a.nodedata->nu;
+  // Normally in ALE problems we should have the one slot
+  // for the t{n} coordinates (i.e. columns [0,ndim) and
+  // another slot for the t{n+1} coords (columns
+  // [ndim,2*ndim)). But in fact we could have more
+  // coordinates that are auxiliary properties or fields, so
+  // this assert is not totally correct. 
   assert(nu==2*ndim);
 
+  // COORDS0 will store the reference coordinates
   coords0.a_resize(2,nnod,2*ndim).set(coords_buff);
+  // COORDS will store the actual (t{n} and t{n+1}) coords. 
   coords.clone(coords0);
+  // This is an index to store the last frame that has been
+  // saved in XALEPATTERN file. If NSAVEROT=1 then FRAME is
+  // the same as time step, but if NSAVEROT>1 then they do
+  // not coincide.
   frame=0;
 
   // Call the hook for the user
@@ -60,9 +79,6 @@ void mmv_force_hook_t::time_step_pre(double time,int step) {
       coords.e(j,ndim+k) = coords.e(j,k);
     }
   }
-
-  // Call the hook for the user
-  time_step_pre_user(time,step);
 
   // Call the user function to compute the new coords
   // Shift the coords at t[n] from slot 0 to slot 1.
